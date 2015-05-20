1654fd124be28ebbe077d5d4f613857a96ebfcb3
Adnan Abdulhussein <adnan@bitnami.com>
2015-05-20 16:25:09 -0700

    Fix mariadb /conf volume

--- containers/mariadb/entrypoint.sh
+++ containers/mariadb/entrypoint.sh
@@ -10,11 +10,18 @@ fi
 if [ "$1" = 'mysqld.bin' ]; then
   set -- "$@" --defaults-file=/opt/bitnami/mysql/my.cnf --basedir=/opt/bitnami/mysql --datadir=/opt/bitnami/mysql/data --plugin-dir=/opt/bitnami/mysql/lib/plugin --user=mysql --socket=/opt/bitnami/mysql/tmp/mysql.sock$EXTRA_OPTIONS
 
+  if [ ! -f "/conf/my.cnf" ]; then
+    echo "Copying default configuration to /conf/my.cnf..."
+    echo ""
+    cp /opt/bitnami/mysql/my.cnf.default /conf/my.cnf
+  fi
+
   if [ ! "$(ls -A /data)" ]; then
     if [ -z "$MYSQL_PASSWORD" ]; then
       MYSQL_PASSWORD=$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c12)
     fi
 
+    echo "Initializing MySQL database..."
     echo ""
     echo "#########################################################################"
     echo "#                                                                       #"
@@ -23,13 +30,9 @@ if [ "$1" = 'mysqld.bin' ]; then
     echo "#########################################################################"
     echo ""
 
+    chown mysql:mysql /data
     su mysql -c "sh /opt/bitnami/mysql/scripts/myscript.sh /opt/bitnami/mysql $MYSQL_PASSWORD"
     /opt/bitnami/mysql/scripts/ctl.sh stop mysql > /dev/null
-  fi
-
-  if [ ! -f "/config/my.cnf" ]; then
-    mv /opt/bitnami/mysql/my.cnf /config/my.cnf
-    ln -s /config/my.cnf /opt/bitnami/mysql/my.cnf
   fi
 fi
 
