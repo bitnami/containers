8c7b6cb1730cf433067b2a47006533843d95aec8
Adnan Abdulhussein <adnan@bitnami.com>
2015-05-26 14:45:38 -0700

    Switch to utility script and /usr/local/bitnami prefix

--- containers/mariadb/Dockerfile
+++ containers/mariadb/Dockerfile
@@ -5,15 +5,15 @@ ENV BITNAMI_APP_NAME mariadb
 ENV BITNAMI_APP_VERSION 5.5.42-0
 ENV BITNAMI_APP_DIRNAME mysql
 
-ADD install.sh /tmp/install.sh
-ADD post-install.sh /tmp/post-install.sh
+ADD https://storage.googleapis.com/bitnami-artifacts/install.sh?GoogleAccessId=432889337695-e1gggo94k5qubupjsb35tajs91bdu0hg@developer.gserviceaccount.com&Expires=1434934078&Signature=QNkAu%2F8E2RlalSQy4n1sxMhsGKF%2FVltr6zu65HU6A9H0HKOgl6u9etqy9w6OwD4DsLMxYuy2uymOK3iDc5RbfkAMncKI1zJpxcwRQ4Mt43Oe8PBXKbQYcZ7mQaYPtpnjYblDs1S2p12Pu5NTDJHK2hJ1MrIUYwBup5n60R6OJRI%3D /tmp/install.sh
+COPY post-install.sh /tmp/post-install.sh
 
 # We need to specify a mysql password since the installer initializes the database, but it is
 # removed in the post install and re-initialized at runtime.
-RUN bash /tmp/install.sh\
+RUN sh /tmp/install.sh\
     --base_password bitnami --mysql_password bitnami --mysql_allow_all_remote_connections 1 --disable-components common
 
-ENV PATH /opt/bitnami/mysql/bin:$PATH
+ENV PATH /usr/local/bitnami/mysql/bin:$PATH
 EXPOSE 3306
 VOLUME ["/data", "/conf", "/logs"]
 
--- containers/mariadb/entrypoint.sh
+++ containers/mariadb/entrypoint.sh
@@ -9,11 +9,11 @@ fi
 if [ ! "$(ls -A /conf)" ]; then
   echo "Copying default configuration to /conf/my.cnf..."
   echo ""
-  cp -r /opt/bitnami/mysql/conf.defaults/* /opt/bitnami/mysql/conf
+  cp -r /usr/local/bitnami/mysql/conf.defaults/* /usr/local/bitnami/mysql/conf
 fi
 
 if [ "$1" = 'mysqld.bin' ]; then
-  set -- "$@" --defaults-file=/opt/bitnami/mysql/my.cnf --log-error=/opt/bitnami/mysql/logs/mysqld.log --basedir=/opt/bitnami/mysql --datadir=/opt/bitnami/mysql/data --plugin-dir=/opt/bitnami/mysql/lib/plugin --user=mysql --socket=/opt/bitnami/mysql/tmp/mysql.sock "--lower-case-table-names=1" $EXTRA_OPTIONS
+  set -- "$@" --defaults-file=/usr/local/bitnami/mysql/my.cnf --log-error=/usr/local/bitnami/mysql/logs/mysqld.log --basedir=/usr/local/bitnami/mysql --datadir=/usr/local/bitnami/mysql/data --plugin-dir=/usr/local/bitnami/mysql/lib/plugin --user=mysql --socket=/usr/local/bitnami/mysql/tmp/mysql.sock "--lower-case-table-names=1" $EXTRA_OPTIONS
 
   if [ ! "$(ls -A /data)" ]; then
     if [ -z "$MYSQL_PASSWORD" ]; then
@@ -29,8 +29,8 @@ if [ "$1" = 'mysqld.bin' ]; then
     echo "Initializing MySQL database..."
     echo ""
 
-    /opt/bitnami/mysql/scripts/mysql_install_db --port=3306 --socket=/opt/bitnami/mysql/tmp/mysql.sock --basedir=/opt/bitnami/mysql --datadir=/opt/bitnami/mysql/data > /dev/null
-    chown -R mysql:mysql /opt/bitnami/mysql/data
+    /usr/local/bitnami/mysql/scripts/mysql_install_db --port=3306 --socket=/usr/local/bitnami/mysql/tmp/mysql.sock --basedir=/usr/local/bitnami/mysql --datadir=/usr/local/bitnami/mysql/data > /dev/null
+    chown -R mysql:mysql /usr/local/bitnami/mysql/data
 
     echo "UPDATE mysql.user SET Password=PASSWORD('$MYSQL_PASSWORD') WHERE User='root';" >> /tmp/init_mysql.sql
     echo "FLUSH PRIVILEGES;" >> /tmp/init_mysql.sql
@@ -43,7 +43,7 @@ if [ "$1" = 'mysqld.bin' ]; then
     set -- "$@" --init-file=/tmp/init_mysql.sql
   fi
 
-  chown -R mysql:mysql /opt/bitnami/mysql/logs
+  chown -R mysql:mysql /usr/local/bitnami/mysql/logs
 fi
 
 exec "$@"
