b18f032253247c2a4617ad189f5b24eb861c18ea
Miguel Martinez <miguel@bitnami.com>
2015-06-05 15:31:18 -0700

    Redis multiple options and namespacing

--- bitnami/redis/post-install.sh
+++ bitnami/redis/post-install.sh
@@ -3,9 +3,6 @@
 INSTALL_DIR=$1
 cd $INSTALL_DIR
 
-# remove unneeded files
-rm -rf $INSTALL_DIR/scripts
-
 # set up default config
 mkdir $INSTALL_DIR/etc/conf.defaults
 mv $INSTALL_DIR/etc/redis.conf $INSTALL_DIR/etc/conf.defaults
@@ -15,15 +12,14 @@ ln -s $INSTALL_DIR/etc/conf/redis.conf $INSTALL_DIR/etc/redis.conf
 mkdir $INSTALL_DIR/var/data
 
 # symlink mount points at root to install dir
-ln -s $INSTALL_DIR/etc/conf /conf
-ln -s $INSTALL_DIR/var/data /data
-ln -s $INSTALL_DIR/var/log /logs
+ln -s $INSTALL_DIR/etc/conf $BITNAMI_VOL_PREFIX/conf
+ln -s $INSTALL_DIR/var/data $BITNAMI_VOL_PREFIX/data
+ln -s $INSTALL_DIR/var/log $BITNAMI_VOL_PREFIX/logs
 
 # set up logging to stdout
 #ln -sf /dev/stdout /logs/redis-server.log
 
 # Conf modifications
-sed -i -e 's:\(dir '$INSTALL_DIR'/var\):\1/data:' $INSTALL_DIR/etc/conf.defaults/redis.conf
 sed -i -e 's:\(daemonize\) yes:\1 no:' $INSTALL_DIR/etc/conf.defaults/redis.conf
 
 
