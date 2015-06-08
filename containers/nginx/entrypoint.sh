b5013f70b5251237ed81d6c51a246ea552bc7dd8
Adnan Abdulhussein <adnan@bitnami.com>
2015-06-08 14:58:31 -0700

    nginx: namespacing volumes and cp without overwrite everytime

--- containers/nginx/entrypoint.sh
+++ containers/nginx/entrypoint.sh
@@ -2,18 +2,16 @@
 set -e
 SERVICE_USER=daemon
 
-if [ ! "$(ls -A /conf)" ]; then
-  cp -r $BITNAMI_APP_DIR/conf.defaults/* $BITNAMI_APP_DIR/conf
-fi
+cp -nr $BITNAMI_APP_DIR/conf.defaults/* $BITNAMI_APP_DIR/conf
 
 if [ ! "$(ls -A /app)" ]; then
   cp -r $BITNAMI_APP_DIR/html.defaults/* $BITNAMI_APP_DIR/html
 fi
 
-chown -R $SERVICE_USER:$SERVICE_USER /logs/ /conf/ /app/
+chown -R $SERVICE_USER:$SERVICE_USER $BITNAMI_VOL_PREFIX/logs/ $BITNAMI_VOL_PREFIX/conf/ /app/
 
 if [ "$1" = 'nginx' ]; then
-  gosu $SERVICE_USER:$SERVICE_USER tail -f /logs/* &
+  gosu $SERVICE_USER:$SERVICE_USER tail -f $BITNAMI_VOL_PREFIX/logs/* &
 fi
 
 echo "#########################################################################"
