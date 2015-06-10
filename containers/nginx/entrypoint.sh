3a4a2eba4dd1be9cbdc56abef94e389935f0fcbc
Miguel Martinez <miguel@bitnami.com>
2015-06-09 20:22:55 -0700

    Do not crash if chown error

--- containers/nginx/entrypoint.sh
+++ containers/nginx/entrypoint.sh
@@ -10,7 +10,7 @@ if [ ! "$(ls -A /app)" ]; then
   cp -r $BITNAMI_APP_DIR/html.defaults/* $BITNAMI_APP_DIR/html
 fi
 
-chown -R $BITNAMI_APP_USER:$BITNAMI_APP_USER $BITNAMI_APP_VOL_PREFIX/logs/ $BITNAMI_APP_VOL_PREFIX/conf/ /app/
+chown -R $BITNAMI_APP_USER:$BITNAMI_APP_USER $BITNAMI_APP_VOL_PREFIX/logs/ $BITNAMI_APP_VOL_PREFIX/conf/ /app/ || true
 
 if [ "$1" = 'nginx' ]; then
   gosu $BITNAMI_APP_USER:$BITNAMI_APP_USER tail -f $BITNAMI_APP_VOL_PREFIX/logs/* &
