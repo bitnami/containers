897fe27ceec9e69c22e709f4f805f5ef8b8ff198
Miguel Martinez <miguel@bitnami.com>
2015-06-10 12:12:23 -0700

    Replacing BITNAMI_DIR, Refs T4065

--- bitnami/mariadb/post-install.sh
+++ bitnami/mariadb/post-install.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-
-INSTALL_DIR=$1
-cd $INSTALL_DIR
+cd $BITNAMI_APP_DIR
 
 # set up logging to stdout
 ln -s /dev/stdout logs/mysqld.log
@@ -9,9 +7,9 @@ ln -s /dev/stdout logs/mysqld.log
 # set up default config
 mkdir conf.defaults
 mv my.cnf conf.defaults/
-ln -s $INSTALL_DIR/conf/my.cnf my.cnf
+ln -s $BITNAMI_APP_DIR/conf/my.cnf my.cnf
 
 # symlink mount points at root to install dir
-ln -s $INSTALL_DIR/conf $BITNAMI_APP_VOL_PREFIX/conf
-ln -s $INSTALL_DIR/data $BITNAMI_APP_VOL_PREFIX/data
-ln -s $INSTALL_DIR/logs $BITNAMI_APP_VOL_PREFIX/logs
+ln -s $BITNAMI_APP_DIR/conf $BITNAMI_APP_VOL_PREFIX/conf
+ln -s $BITNAMI_APP_DIR/data $BITNAMI_APP_VOL_PREFIX/data
+ln -s $BITNAMI_APP_DIR/logs $BITNAMI_APP_VOL_PREFIX/logs
