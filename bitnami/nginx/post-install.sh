ffa26fad7288e35d8c3683db1f4b7c1013861c9b
Miguel Martinez <miguel@bitnami.com>
2015-06-10 12:12:23 -0700

    Replacing BITNAMI_DIR, Refs T4065

--- bitnami/nginx/post-install.sh
+++ bitnami/nginx/post-install.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
-
-INSTALL_DIR=$1
-cd $INSTALL_DIR
+cd $BITNAMI_APP_DIR
 
 # Backup default conf/html
 mv conf conf.defaults
 mv html html.defaults
 
 # Setup mount point symlinks
-ln -s $INSTALL_DIR/conf $BITNAMI_APP_VOL_PREFIX/conf
-ln -s $INSTALL_DIR/logs $BITNAMI_APP_VOL_PREFIX/logs
-ln -s $INSTALL_DIR/html /app
+ln -s $BITNAMI_APP_DIR/conf $BITNAMI_APP_VOL_PREFIX/conf
+ln -s $BITNAMI_APP_DIR/logs $BITNAMI_APP_VOL_PREFIX/logs
+ln -s $BITNAMI_APP_DIR/html /app
