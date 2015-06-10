afaf7ba73630465aa0d7b6ed1aded22a2937b5f3
Miguel Martinez <miguel@bitnami.com>
2015-06-10 12:12:23 -0700

    Replacing BITNAMI_DIR, Refs T4065

--- containers/redis/post-install.sh
+++ containers/redis/post-install.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 
-INSTALL_DIR=$1
-cd $INSTALL_DIR
+cd $BITNAMI_APP_DIR
 
 # set up default config
-mkdir $INSTALL_DIR/etc/conf.defaults
-mv $INSTALL_DIR/etc/redis.conf $INSTALL_DIR/etc/conf.defaults
-ln -s $INSTALL_DIR/etc/conf/redis.conf $INSTALL_DIR/etc/redis.conf
+mkdir $BITNAMI_APP_DIR/etc/conf.defaults
+mv $BITNAMI_APP_DIR/etc/redis.conf $BITNAMI_APP_DIR/etc/conf.defaults
+ln -s $BITNAMI_APP_DIR/etc/conf/redis.conf $BITNAMI_APP_DIR/etc/redis.conf
 
 # symlink mount points at root to install dir
-ln -s $INSTALL_DIR/etc/conf $BITNAMI_APP_VOL_PREFIX/conf
-ln -s $INSTALL_DIR/var/data $BITNAMI_APP_VOL_PREFIX/data
-ln -s $INSTALL_DIR/var/log $BITNAMI_APP_VOL_PREFIX/logs
+ln -s $BITNAMI_APP_DIR/etc/conf $BITNAMI_APP_VOL_PREFIX/conf
+ln -s $BITNAMI_APP_DIR/var/data $BITNAMI_APP_VOL_PREFIX/data
+ln -s $BITNAMI_APP_DIR/var/log $BITNAMI_APP_VOL_PREFIX/logs
 
 # set up logging to stdout
 #ln -sf /dev/stdout /logs/redis-server.log
 
 # Conf modifications
-sed -i -e 's:\(daemonize\) yes:\1 no:' $INSTALL_DIR/etc/conf.defaults/redis.conf
+sed -i -e 's:\(daemonize\) yes:\1 no:' $BITNAMI_APP_DIR/etc/conf.defaults/redis.conf
 
 
