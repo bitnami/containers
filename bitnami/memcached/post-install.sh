5be7adf245473eb2ae20a9e5f93660a5dcab711d
Miguel Martinez <miguel@bitnami.com>
2015-06-10 12:12:23 -0700

    Replacing BITNAMI_DIR, Refs T4065

--- bitnami/memcached/post-install.sh
+++ bitnami/memcached/post-install.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
-
-INSTALL_DIR=$1
-cd $INSTALL_DIR
+cd $BITNAMI_APP_DIR
 
 # set up logging to stdout
 mkdir logs
 ln -s /dev/stdout logs/memcached.log
 
 # symlink mount points at root to install dir
-ln -s $INSTALL_DIR/logs $BITNAMI_APP_VOL_PREFIX/logs
+ln -s $BITNAMI_APP_DIR/logs $BITNAMI_APP_VOL_PREFIX/logs
