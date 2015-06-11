f406b6ed25ddf936d8d7b36856b2c6f8130206c7
Miguel Martinez <miguel@bitnami.com>
2015-06-11 14:12:09 -0700

    Adding integrity check

--- containers/redis/post-install.sh
+++ containers/redis/post-install.sh
@@ -11,11 +11,3 @@ ln -s $BITNAMI_APP_DIR/etc/conf/redis.conf $BITNAMI_APP_DIR/etc/redis.conf
 ln -s $BITNAMI_APP_DIR/etc/conf $BITNAMI_APP_VOL_PREFIX/conf
 ln -s $BITNAMI_APP_DIR/var/data $BITNAMI_APP_VOL_PREFIX/data
 ln -s $BITNAMI_APP_DIR/var/log $BITNAMI_APP_VOL_PREFIX/logs
-
-# set up logging to stdout
-#ln -sf /dev/stdout /logs/redis-server.log
-
-# Conf modifications
-sed -i -e 's:\(daemonize\) yes:\1 no:' $BITNAMI_APP_DIR/etc/conf.defaults/redis.conf
-
-
