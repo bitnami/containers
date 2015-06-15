1397fbbac09136b43284e15201121936ceb27e96
Miguel Martinez <miguel@bitnami.com>
2015-06-15 14:10:16 -0700

    Adding volumes and conf generation

--- bitnami/php-fpm/post-install.sh
+++ bitnami/php-fpm/post-install.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 mkdir /app
-chown -R $BITNAMI_APP_USER:$BITNAMI_APP_USER /app/
+# set up default config
+mkdir $BITNAMI_APP_DIR/etc/conf.defaults
+mkdir $BITNAMI_APP_DIR/etc/conf
+mv $BITNAMI_APP_DIR/etc/php-fpm.conf $BITNAMI_APP_DIR/etc/conf.defaults
+ln -s $BITNAMI_APP_DIR/etc/conf/php-fpm.conf $BITNAMI_APP_DIR/etc/php-fpm.conf
+
+chown -R $BITNAMI_APP_USER:$BITNAMI_APP_USER /app/ $BITNAMI_APP_DIR/etc/conf $BITNAMI_APP_DIR/var/log
+ln -s $BITNAMI_APP_DIR/var/log $BITNAMI_APP_VOL_PREFIX/logs
+ln -s $BITNAMI_APP_DIR/etc/conf $BITNAMI_APP_VOL_PREFIX/conf
