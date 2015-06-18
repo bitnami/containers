9bd416ad93271ddd4063261c710b5a02e6d13c8c
Miguel Martinez <miguel@bitnami.com>
2015-06-18 13:37:59 -0700

    1.4.24-0 version + tests

--- bitnami/memcached/entrypoint.sh
+++ bitnami/memcached/entrypoint.sh
@@ -9,13 +9,14 @@ if [ "${1:0:1}" = '-' ]; then
 fi
 
 if [ "$1" = 'memcached' ]; then
-  set -- "$@" -u $BITNAMI_APP_USER -S -l 0.0.0.0
+  set -- "$@" -u $BITNAMI_APP_USER -l 0.0.0.0
 fi
 
 if [ "$MEMCACHED_PASSWORD" ]; then
+  set -- "$@" -S
   echo "Setting password..."
   $BITNAMI_APP_DIR/bnconfig --userpassword $MEMCACHED_PASSWORD
-  print_app_credentials $BITNAMI_APP_NAME **none** $MEMCACHED_PASSWORD
+  print_app_credentials $BITNAMI_APP_NAME user $MEMCACHED_PASSWORD
 fi
 
 exec "$@" >> $BITNAMI_APP_VOL_PREFIX/logs/memcached.log 2>&1
