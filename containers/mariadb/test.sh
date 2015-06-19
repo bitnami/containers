763ba3216e13b1b2e3f4b123051d5670eb9ec650
Miguel Martinez <miguel@bitnami.com>
2015-06-18 19:40:16 -0800

    Disabling TTY for Jenkins CI

--- containers/mariadb/test.sh
+++ containers/mariadb/test.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bats
-
-CONTAINER_NAME=mariadb
+CONTAINER_NAME=bitnami-mariadb-test
 IMAGE_NAME=bitnami/mariadb
 SLEEP_TIME=5
 MARIADB_DATABASE=test_database
@@ -20,7 +19,7 @@ teardown() {
 }
 
 cleanup_volumes_content() {
-  docker run -it --rm\
+  docker run --rm\
     -v $HOST_VOL_PREFIX/data:$VOL_PREFIX/data\
     -v $HOST_VOL_PREFIX/conf:$VOL_PREFIX/conf\
     -v $HOST_VOL_PREFIX/logs:$VOL_PREFIX/logs\
@@ -28,11 +27,16 @@ cleanup_volumes_content() {
 }
 
 create_basic_container(){
-  docker run -itd --name $CONTAINER_NAME $IMAGE_NAME; sleep $SLEEP_TIME
+  docker run -d --name $CONTAINER_NAME $IMAGE_NAME; sleep $SLEEP_TIME
+}
+
+# $1 is the command
+mysql_client(){
+  docker run --rm --link $CONTAINER_NAME:$CONTAINER_NAME $IMAGE_NAME mysql -h $CONTAINER_NAME "$@"
 }
 
 create_full_container(){
-  docker run -itd --name $CONTAINER_NAME\
+  docker run -d --name $CONTAINER_NAME\
    -e MARIADB_USER=$MARIADB_USER\
    -e MARIADB_DATABASE=$MARIADB_DATABASE\
    -e MARIADB_PASSWORD=$MARIADB_PASSWORD $IMAGE_NAME
@@ -40,7 +44,7 @@ create_full_container(){
 }
 
 create_full_container_mounted(){
-  docker run -itd --name $CONTAINER_NAME\
+  docker run -d --name $CONTAINER_NAME\
    -e MARIADB_USER=$MARIADB_USER\
    -e MARIADB_DATABASE=$MARIADB_DATABASE\
    -e MARIADB_PASSWORD=$MARIADB_PASSWORD\
@@ -53,65 +57,57 @@ create_full_container_mounted(){
 
 @test "Root user created without password" {
   create_basic_container
-  docker exec -it $CONTAINER_NAME mysql -e "select User from mysql.user where User=\"root\"\G" | {
-    run grep 'User: root'
-    [ $status = 0 ]
-  }
+  run mysql_client -e "select User from mysql.user where User=\"root\"\G"
+  [[ "$output" =~ "User: root" ]]
 }
 
 @test "Root user created with password" {
-  docker run -itd --name $CONTAINER_NAME -e MARIADB_PASSWORD=$MARIADB_PASSWORD $IMAGE_NAME
+  docker run -d --name $CONTAINER_NAME -e MARIADB_PASSWORD=$MARIADB_PASSWORD $IMAGE_NAME
   sleep $SLEEP_TIME
   # Can not login as root
-  run docker exec -it $CONTAINER_NAME mysql -e 'show databases\G'
+  run mysql_client -e 'show databases\G'
   [ $status = 1 ]
-  run docker exec -it $CONTAINER_NAME mysql -p$MARIADB_PASSWORD -e 'show databases\G'
+  run mysql_client -p$MARIADB_PASSWORD -e 'show databases\G'
   [ $status = 0 ]
 }
 
 @test "Root user has access to admin database" {
   create_basic_container
-  docker exec -it $CONTAINER_NAME mysql -e 'show databases\G' | {
-    run grep 'Database: mysql'
-    [ $status = 0 ]
-  }
+  run mysql_client -e 'show databases\G'
+  [[ "$output" =~ 'Database: mysql' ]]
 }
 
 @test "Custom database created" {
-  docker run -itd --name $CONTAINER_NAME -e MARIADB_DATABASE=$MARIADB_DATABASE $IMAGE_NAME
+  docker run -d --name $CONTAINER_NAME -e MARIADB_DATABASE=$MARIADB_DATABASE $IMAGE_NAME
   sleep $SLEEP_TIME
-  docker exec -it $CONTAINER_NAME mysql -e 'show databases\G' | {
-    run grep "Database: $MARIADB_DATABASE"
-    [ $status = 0 ]
-  }
+  run mysql_client -e 'show databases\G'
+  [[ "$output" =~ "Database: $MARIADB_DATABASE" ]]
 }
 
 @test "Can't create a custom user without database" {
-  run docker run -it --name $CONTAINER_NAME -e MARIADB_USER=$MARIADB_USER $IMAGE_NAME
+  run docker run --name $CONTAINER_NAME -e MARIADB_USER=$MARIADB_USER $IMAGE_NAME
   [[ "$output" =~ "you need to provide the MARIADB_DATABASE" ]]
   [ $status = 255 ]
 }
 
 @test "Create custom user and database without password" {
-  docker run -itd --name $CONTAINER_NAME\
+  docker run -d --name $CONTAINER_NAME\
    -e MARIADB_USER=$MARIADB_USER\
    -e MARIADB_DATABASE=$MARIADB_DATABASE $IMAGE_NAME
   sleep $SLEEP_TIME
   # Can not login as root
-  run docker exec -it $CONTAINER_NAME mysql -e 'show databases\G'
+  run mysql_client -e 'show databases\G'
   [ $status = 1 ]
-  docker exec -it $CONTAINER_NAME mysql -u $MARIADB_USER -e  'show databases\G' | {
-    run grep "Database: $MARIADB_DATABASE"
-    [ $status = 0 ]
-  }
+  run mysql_client -u $MARIADB_USER -e  'show databases\G' 
+  [[ "$output" =~ "Database: $MARIADB_DATABASE" ]]
 }
 
 @test "Create custom user and database with password" {
   create_full_container
   # Can not login as root
-  run docker exec -it $CONTAINER_NAME mysql -u $MARIADB_USER -e 'show databases\G'
+  run mysql_client -u $MARIADB_USER -e 'show databases\G'
   [ $status = 1 ]
-  run docker exec -it $CONTAINER_NAME mysql -u $MARIADB_USER -p$MARIADB_PASSWORD -e 'show databases\G'
+  run mysql_client -u $MARIADB_USER -p$MARIADB_PASSWORD -e 'show databases\G'
   [ $status = 0 ]
 }
 
@@ -125,7 +121,7 @@ create_full_container_mounted(){
   run docker logs $CONTAINER_NAME
   [[ "$output" =~ "The credentials were set on first boot." ]]
 
-  run docker exec -it $CONTAINER_NAME mysql -u $MARIADB_USER -p$MARIADB_PASSWORD -e 'show databases\G'
+  run mysql_client -u $MARIADB_USER -p$MARIADB_PASSWORD -e 'show databases\G'
   [ $status = 0 ]
 }
 
@@ -135,13 +131,13 @@ create_full_container_mounted(){
 
   docker rm -fv $CONTAINER_NAME
 
-  docker run -itd --name $CONTAINER_NAME\
+  docker run -d --name $CONTAINER_NAME\
    -v $HOST_VOL_PREFIX/data:$VOL_PREFIX/data\
    -v $HOST_VOL_PREFIX/conf:$VOL_PREFIX/conf\
    $IMAGE_NAME
   sleep $SLEEP_TIME
 
-  run docker exec -it $CONTAINER_NAME mysql -u $MARIADB_USER -p$MARIADB_PASSWORD -e 'show databases\G'
+  run mysql_client -u $MARIADB_USER -p$MARIADB_PASSWORD -e 'show databases\G'
   [ $status = 0 ]
   cleanup_volumes_content
 }
@@ -149,19 +145,17 @@ create_full_container_mounted(){
 @test "Port 3306 exposed and accepting external connections" {
   create_basic_container
 
-  run docker run -it --rm --name 'linked' --link $CONTAINER_NAME:$CONTAINER_NAME $IMAGE_NAME\
+  run docker run --rm --name 'linked' --link $CONTAINER_NAME:$CONTAINER_NAME $IMAGE_NAME\
     mysql -h $CONTAINER_NAME -P 3306 -e 'show databases\G'
   [ $status = 0 ]
 }
 
 @test "All the volumes exposed" {
   create_basic_container
-  docker inspect $CONTAINER_NAME | {
-    run grep "\"Volumes\":" -A 3
-    [[ "$output" =~ "$VOL_PREFIX/data" ]]
-    [[ "$output" =~ "$VOL_PREFIX/conf" ]]
-    [[ "$output" =~ "$VOL_PREFIX/logs" ]]
-  }
+  run docker inspect $CONTAINER_NAME
+  [[ "$output" =~ "$VOL_PREFIX/data" ]]
+  [[ "$output" =~ "$VOL_PREFIX/conf" ]]
+  [[ "$output" =~ "$VOL_PREFIX/logs" ]]
 }
 
 @test "Data gets generated in conf and data if bind mounted in the host" {
@@ -170,4 +164,3 @@ create_full_container_mounted(){
   [ $status = 0 ]
   cleanup_volumes_content
 }
-
