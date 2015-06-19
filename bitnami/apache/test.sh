6b0ab46fa59126dfc94d06096030bf9c7b95c249
Miguel Martinez <miguel@bitnami.com>
2015-06-19 11:19:03 -0700

    Tests cleanup

--- bitnami/apache/test.sh
+++ bitnami/apache/test.sh
@@ -4,21 +4,23 @@ CONTAINER_NAME=bitnami-apache-test
 IMAGE_NAME=bitnami/apache
 SLEEP_TIME=2
 VOL_PREFIX=/bitnami/apache
-HOST_VOL_PREFIX=/tmp/bitnami/$CONTAINER_NAME
 
-# Check config override from host
-setup() {
-  mkdir -p $HOST_VOL_PREFIX
-}
-
-teardown() {
+cleanup_running_containers() {
   if [ "$(docker ps -a | grep $CONTAINER_NAME)" ]; then
     docker rm -fv $CONTAINER_NAME
   fi
 }
 
+setup() {
+  cleanup_running_containers
+}
+
+teardown() {
+  cleanup_running_containers
+}
+
 create_container(){
-  docker run -itd --name $CONTAINER_NAME \
+  docker run -d --name $CONTAINER_NAME \
    --expose 81 $IMAGE_NAME
   sleep $SLEEP_TIME
 }
@@ -34,47 +36,40 @@ add_vhost() {
 
 @test "We can connect to the port 80 and 443" {
   create_container
-  docker run --link $CONTAINER_NAME:apache --rm bitnami/apache curl -L -i http://apache:80 | {
-    run grep "200 OK"
-    [ $status = 0 ]
-  }
+  run docker run --link $CONTAINER_NAME:apache --rm bitnami/apache curl -L -i http://apache:80
+  [[ "$output" =~  "200 OK" ]]
+  [ $status = 0 ]
 
-  docker run --link $CONTAINER_NAME:apache --rm bitnami/apache curl -L -i -k https://apache:443 | {
-    run grep "200 OK"
-    [ $status = 0 ]
-  }
+  run docker run --link $CONTAINER_NAME:apache --rm bitnami/apache curl -L -i -k https://apache:443
+  [[ "$output" =~  "200 OK" ]]
+  [ $status = 0 ]
 }
 
 @test "Returns default page" {
   create_container
-  docker run --link $CONTAINER_NAME:apache --rm bitnami/apache curl -L -i http://apache:80 | {
-    run grep "It works!"
-    [ $status = 0 ]
-  }
+  run docker run --link $CONTAINER_NAME:apache --rm bitnami/apache curl -L -i http://apache:80
+  [[ "$output" =~  "It works!" ]]
+  [ $status = 0 ]
 
-  docker run --link $CONTAINER_NAME:apache --rm bitnami/apache curl -L -i -k https://apache:443 | {
-    run grep "It works!"
-    [ $status = 0 ]
-  }
+  run docker run --link $CONTAINER_NAME:apache --rm bitnami/apache curl -L -i -k https://apache:443
+  [[ "$output" =~  "It works!" ]]
+  [ $status = 0 ]
 }
 
 @test "Logs to stdout" {
   create_container
   docker run --link $CONTAINER_NAME:apache --rm bitnami/apache curl -L -i http://apache:80
-  docker logs $CONTAINER_NAME | {
-    run grep "GET / HTTP/1.1"
-    [ $status = 0 ]
-  }
+  run docker logs $CONTAINER_NAME
+  [[ "$output" =~  "GET / HTTP/1.1" ]]
+  [ $status = 0 ]
 }
 
 @test "All the volumes exposed" {
   create_container
-  docker inspect $CONTAINER_NAME | {
-    run grep "\"Volumes\":" -A 3
-    [[ "$output" =~ "$VOL_PREFIX/logs" ]]
-    [[ "$output" =~ "$VOL_PREFIX/conf" ]]
-    [[ "$output" =~ "/app" ]]
-  }
+  run docker inspect $CONTAINER_NAME
+  [[ "$output" =~ "$VOL_PREFIX/logs" ]]
+  [[ "$output" =~ "$VOL_PREFIX/conf" ]]
+  [[ "$output" =~ "/app" ]]
 }
 
 @test "Vhosts directory is imported" {
