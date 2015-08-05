4c243661ae739ad16fea6b51f80e1ba49543f1d3
Sameer Naik <sameer@damagehead.com>
2015-08-05 20:16:40 +0530

    nullify `ctl.sh` script administered by `bnconfig`

--- containers/mongodb/post-install.sh
+++ containers/mongodb/post-install.sh
@@ -13,5 +13,8 @@ ln -s $BITNAMI_APP_DIR/conf $BITNAMI_APP_VOL_PREFIX/conf
 ln -s $BITNAMI_APP_DIR/data $BITNAMI_APP_VOL_PREFIX/data
 ln -s $BITNAMI_APP_DIR/log $BITNAMI_APP_VOL_PREFIX/logs
 
+# nullify `ctl.sh` script administered by `bnconfig`
+echo "# Disabled" > $BITNAMI_APP_DIR/scripts/ctl.sh
+
 # Temp fix for disabling authentication
 sed -i 's/^auth/# auth/' $BITNAMI_APP_DIR/conf.defaults/mongodb.conf
