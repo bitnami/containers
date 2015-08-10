b5b94b5b5f7ba814150a5c2ee1a68c4a69909978
Sameer Naik <sameer@damagehead.com>
2015-08-10 23:42:16 +0530

    added `conf` volume

--- containers/postgresql/post-install.sh
+++ containers/postgresql/post-install.sh
@@ -3,7 +3,9 @@ cd $BITNAMI_APP_DIR
 
 mkdir -p $BITNAMI_APP_DIR/data
 mkdir -p $BITNAMI_APP_DIR/logs
+mkdir -p $BITNAMI_APP_DIR/conf
 
 # symlink mount points at root to install dir
 ln -s $BITNAMI_APP_DIR/data $BITNAMI_APP_VOL_PREFIX/data
 ln -s $BITNAMI_APP_DIR/logs $BITNAMI_APP_VOL_PREFIX/logs
+ln -s $BITNAMI_APP_DIR/conf $BITNAMI_APP_VOL_PREFIX/conf
