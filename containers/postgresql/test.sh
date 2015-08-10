0a01706fb2aaa90e36aa845c2d254c05525bdbdf
Sameer Naik <sameer@damagehead.com>
2015-08-10 22:24:52 +0530

    "Create custom user and database with password"

--- containers/postgresql/test.sh
+++ containers/postgresql/test.sh
@@ -31,6 +31,14 @@ psql_client(){
   docker run --rm --link $CONTAINER_NAME:$CONTAINER_NAME -e PGPASSWORD=$POSTGRESQL_PASSWORD $IMAGE_NAME psql -h $CONTAINER_NAME "$@"
 }
 
+create_full_container(){
+  docker run -d --name $CONTAINER_NAME\
+   -e POSTGRESQL_USER=$POSTGRESQL_USER\
+   -e POSTGRESQL_DATABASE=$POSTGRESQL_DATABASE\
+   -e POSTGRESQL_PASSWORD=$POSTGRESQL_PASSWORD $IMAGE_NAME
+  sleep $SLEEP_TIME
+}
+
 @test "Port 5432 exposed and accepting external connections" {
   create_container -d
 
@@ -65,4 +73,15 @@ psql_client(){
 @test "Can't create a custom user without database" {
   run create_container -it -e POSTGRESQL_USER=$POSTGRESQL_USER
   [[ "$output" =~ "you need to provide the POSTGRESQL_DATABASE" ]]
+}
+
+@test "Create custom user and database with password" {
+  create_full_container
+  # Can not login as postgres
+  run docker run --rm --link $CONTAINER_NAME:$CONTAINER_NAME $IMAGE_NAME psql -h $CONTAINER_NAME -U $POSTGRESQL_ROOT_USER -wc "\l"
+  [ $status != 1 ]
+  run psql_client -U $POSTGRESQL_ROOT_USER -c "\l"
+  [ $status != 1 ]
+  run psql_client -U $POSTGRESQL_USER $POSTGRESQL_DATABASE -c "\dt"
+  [ $status = 0 ]
 }
