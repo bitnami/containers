3bfc8feaf50008eb09418bad2cf990e2f05711e8
Sameer Naik <sameer@damagehead.com>
2015-08-10 21:19:06 +0530

    tests: "User postgres created with password"

--- bitnami/postgresql/test.sh
+++ bitnami/postgresql/test.sh
@@ -3,6 +3,7 @@ CONTAINER_NAME=bitnami-postgresql-test
 IMAGE_NAME=${IMAGE_NAME:-bitnami/postgresql}
 SLEEP_TIME=5
 POSTGRESQL_DATABASE=test_database
+POSTGRESQL_ROOT_USER=postgres
 POSTGRESQL_USER=test_user
 POSTGRESQL_PASSWORD=test_password
 
@@ -36,5 +37,14 @@ psql_client(){
   run docker run --rm --name 'linked' --link $CONTAINER_NAME:$CONTAINER_NAME\
     -e PGPASSWORD=$POSTGRESQL_PASSWORD $IMAGE_NAME\
       pg_isready -h $CONTAINER_NAME -p 5432 -t 5
+  [ $status = 0 ]
+}
+
+@test "User postgres created with password" {
+  create_container -d -e POSTGRESQL_PASSWORD=$POSTGRESQL_PASSWORD
+  # Can not login as postgres
+  run docker run --rm --link $CONTAINER_NAME:$CONTAINER_NAME $IMAGE_NAME psql -h $CONTAINER_NAME -U $POSTGRESQL_ROOT_USER -wc "\l"
+  [ $status != 1 ]
+  run psql_client -U $POSTGRESQL_ROOT_USER -c "\l"
   [ $status = 0 ]
 }
