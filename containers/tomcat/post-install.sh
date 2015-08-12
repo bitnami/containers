d3ac5b7330bcf5f344d1ca832f148bdf5a6d6b78
Sameer Naik <sameer@damagehead.com>
2015-08-12 23:40:07 +0530

    replaced `/app` volume with `$BITNAMI_APP_VOL_PREFIX/webapps` volume
    Users can drop the `.war` files of their applications into this
    directory and Tomcat will automatically pick them up.

--- containers/tomcat/post-install.sh
+++ containers/tomcat/post-install.sh
@@ -4,7 +4,10 @@ cd $BITNAMI_APP_DIR
 # set up default config
 mv conf conf.defaults
 
+# set up webapps directory
+mv webapps webapps.defaults
+
 # symlink mount points at root to install dir
-ln -s $BITNAMI_APP_DIR/webapps/app /app
+ln -s $BITNAMI_APP_DIR/webapps $BITNAMI_APP_VOL_PREFIX/webapps
 ln -s $BITNAMI_APP_DIR/conf $BITNAMI_APP_VOL_PREFIX/conf
 ln -s $BITNAMI_APP_DIR/logs $BITNAMI_APP_VOL_PREFIX/logs
