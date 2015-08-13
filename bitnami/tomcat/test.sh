263481a2e7281178b8e8f57095a501e1b1e66d75
Sameer Naik <sameer@damagehead.com>
2015-08-13 21:06:14 +0530

    tests: User manager created with password

--- bitnami/tomcat/test.sh
+++ bitnami/tomcat/test.sh
@@ -3,6 +3,7 @@ CONTAINER_NAME=bitnami-tomcat-test
 IMAGE_NAME=${IMAGE_NAME:-bitnami/tomcat}
 SLEEP_TIME=5
 TOMCAT_USER=manager
+TOMCAT_PASSWORD=test_password
 
 cleanup_running_containers() {
   if [ "$(docker ps -a | grep $CONTAINER_NAME)" ]; then
@@ -32,5 +33,11 @@ create_container(){
 @test "Manager has access to management area" {
   create_container -d
   run docker run --link $CONTAINER_NAME:tomcat --rm $IMAGE_NAME curl -L -i http://$TOMCAT_USER@tomcat:8080/manager/html
+  [[ "$output" =~ '200 OK' ]]
+}
+
+@test "User manager created with password" {
+  create_container -d -e TOMCAT_PASSWORD=$TOMCAT_PASSWORD
+  run docker run --link $CONTAINER_NAME:tomcat --rm $IMAGE_NAME curl -L -i http://$TOMCAT_USER:$TOMCAT_PASSWORD@tomcat:8080/manager/html
   [[ "$output" =~ '200 OK' ]]
 }
