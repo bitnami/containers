2bd8de6cb2dcda8c31cd557a1784cbed9da81361
Sameer Naik <sameer@damagehead.com>
2015-08-13 21:04:20 +0530

    tests: Manager has access to management area

--- containers/tomcat/test.sh
+++ containers/tomcat/test.sh
@@ -2,6 +2,7 @@
 CONTAINER_NAME=bitnami-tomcat-test
 IMAGE_NAME=${IMAGE_NAME:-bitnami/tomcat}
 SLEEP_TIME=5
+TOMCAT_USER=manager
 
 cleanup_running_containers() {
   if [ "$(docker ps -a | grep $CONTAINER_NAME)" ]; then
@@ -25,5 +26,11 @@ create_container(){
 @test "Port 8080 exposed and accepting external connections" {
   create_container -d
   run docker run --link $CONTAINER_NAME:tomcat --rm $IMAGE_NAME curl -L -i http://tomcat:8080
+  [[ "$output" =~ '200 OK' ]]
+}
+
+@test "Manager has access to management area" {
+  create_container -d
+  run docker run --link $CONTAINER_NAME:tomcat --rm $IMAGE_NAME curl -L -i http://$TOMCAT_USER@tomcat:8080/manager/html
   [[ "$output" =~ '200 OK' ]]
 }
