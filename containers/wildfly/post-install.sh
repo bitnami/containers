6bd058bca82b6ccba87693a3c7d40800177cb2d0
Sameer Naik <sameer@damagehead.com>
2015-08-14 21:50:36 +0530

    create symlink `$BITNAMI_APP_DIR/standalone/configuration` -> `$BITNAMI_APP_DIR/standalone/conf`

--- containers/wildfly/post-install.sh
+++ containers/wildfly/post-install.sh
@@ -3,6 +3,7 @@ cd $BITNAMI_APP_DIR
 
 # move the default conf directory
 mv $BITNAMI_APP_DIR/standalone/configuration $BITNAMI_APP_DIR/standalone/conf.defaults
+ln -sf $BITNAMI_APP_DIR/standalone/conf $BITNAMI_APP_DIR/standalone/configuration
 
 # symlink mount points at root to install dir
 ln -s $BITNAMI_APP_DIR/standalone/conf $BITNAMI_APP_VOL_PREFIX/conf
