b84c52ddd1b4f32d243c7373cfa005c1213338aa
Sameer Naik <sameer@damagehead.com>
2015-08-14 22:17:24 +0530

    added `/app` volume for deployments

--- containers/wildfly/bitnami-utils-custom.sh
+++ containers/wildfly/bitnami-utils-custom.sh
@@ -2,3 +2,12 @@
 PROGRAM_OPTIONS="-Dwildfly.as.deployment.ondemand=false -b 0.0.0.0 -bmanagement 0.0.0.0"
 JAVA_OPTS="$JAVA_OPTS -XX:MaxPermSize=768m -Xmx768m"
 export JAVA_OPTS
+
+initialize_wildfly_deployments_directory() {
+  echo "==> Initializing Wildfly deployments directory..."
+  echo ""
+  for f in $(ls $BITNAMI_APP_DIR/standalone/deployments.defaults/)
+  do
+    ln -sf $BITNAMI_APP_DIR/standalone/deployments.defaults/$f /app/
+  done
+}
--- containers/wildfly/post-install.sh
+++ containers/wildfly/post-install.sh
@@ -5,6 +5,10 @@ cd $BITNAMI_APP_DIR
 mv $BITNAMI_APP_DIR/standalone/configuration $BITNAMI_APP_DIR/standalone/conf.defaults
 ln -sf $BITNAMI_APP_DIR/standalone/conf $BITNAMI_APP_DIR/standalone/configuration
 
+# move the default deployments directory
+mv $BITNAMI_APP_DIR/standalone/deployments $BITNAMI_APP_DIR/standalone/deployments.defaults
+
 # symlink mount points at root to install dir
+ln -s $BITNAMI_APP_DIR/standalone/deployments /app
 ln -s $BITNAMI_APP_DIR/standalone/conf $BITNAMI_APP_VOL_PREFIX/conf
 ln -s $BITNAMI_APP_DIR/standalone/log $BITNAMI_APP_VOL_PREFIX/logs
