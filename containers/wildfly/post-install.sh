6a99a239d1dd9252783ea846ea75bb3d8673824b
Sameer Naik <sameer@damagehead.com>
2015-08-14 19:33:55 +0530

    link `logs` volume to `$BITNAMI_APP_DIR/standalone/log`

--- containers/wildfly/post-install.sh
+++ containers/wildfly/post-install.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 cd $BITNAMI_APP_DIR
 
-# set up logging logs volume
-ln -sf $BITNAMI_APP_DIR/logs/server.log $BITNAMI_APP_DIR/standalone/log/server.log
-
 # symlink mount points at root to install dir
-ln -s $BITNAMI_APP_DIR/logs $BITNAMI_APP_VOL_PREFIX/logs
+ln -s $BITNAMI_APP_DIR/standalone/log $BITNAMI_APP_VOL_PREFIX/logs
