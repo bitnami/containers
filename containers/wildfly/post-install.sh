015ffa4e4af6a7efd0b322bbd7825f5f31af2459
Sameer Naik <sameer@damagehead.com>
2015-08-14 21:52:05 +0530

    symlink `$BITNAMI_APP_VOL_PREFIX/logs` -> `$BITNAMI_APP_DIR/standalone/log`

--- containers/wildfly/post-install.sh
+++ containers/wildfly/post-install.sh
@@ -7,4 +7,4 @@ ln -sf $BITNAMI_APP_DIR/standalone/conf $BITNAMI_APP_DIR/standalone/configuratio
 
 # symlink mount points at root to install dir
 ln -s $BITNAMI_APP_DIR/standalone/conf $BITNAMI_APP_VOL_PREFIX/conf
-ln -s $BITNAMI_APP_DIR/standalone/logs $BITNAMI_APP_VOL_PREFIX/logs
+ln -s $BITNAMI_APP_DIR/standalone/log $BITNAMI_APP_VOL_PREFIX/logs
