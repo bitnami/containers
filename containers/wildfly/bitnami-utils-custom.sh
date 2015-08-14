c5795e10a224675f30ee5eb6909461acf3f77421
Sameer Naik <sameer@damagehead.com>
2015-08-14 21:48:47 +0530

    append `-XX:MaxPermSize=768m -Xmx768m` to `JAVA_OPTS`

--- containers/wildfly/bitnami-utils-custom.sh
+++ containers/wildfly/bitnami-utils-custom.sh
@@ -1,2 +1,4 @@
 # Wildfly Utility functions
 PROGRAM_OPTIONS="-Djboss.server.config.dir=$BITNAMI_APP_DIR/standalone/conf -Djboss.server.log.dir=$BITNAMI_APP_DIR/standalone/logs -Dwildfly.as.deployment.ondemand=false -b 0.0.0.0 -bmanagement 0.0.0.0"
+JAVA_OPTS="$JAVA_OPTS -XX:MaxPermSize=768m -Xmx768m"
+export JAVA_OPTS
