fc56d2a4d7db42bab7cd4881eab96f778d3e03ca
Sameer Naik <sameer@damagehead.com>
2015-08-15 11:07:21 +0530

    create `.initialized` file in the `app` directory to avoid reinitialization

--- containers/tomcat/bitnami-utils-custom.sh
+++ containers/tomcat/bitnami-utils-custom.sh
@@ -1,9 +1,15 @@
 # Tomcat Utility functions
 
 initialize_tomcat_webapps_directory() {
-  echo "==> Initializing Tomcat webapps directory..."
-  echo ""
-  cp -a $BITNAMI_APP_DIR/webapps.defaults/* /app/
+  if [ ! -f /app/.initialized ]; then
+    echo "==> Initializing Tomcat webapps directory..."
+    echo ""
+    cp -a $BITNAMI_APP_DIR/webapps.defaults/* /app/
+    touch /app/.initialized
+  else
+    echo "==> Tomcat webapps directory already initialized..."
+    echo ""
+  fi
 }
 
 print_tomcat_password() {
--- containers/tomcat/entrypoint.sh
+++ containers/tomcat/entrypoint.sh
@@ -28,9 +28,7 @@ chown -R $BITNAMI_APP_USER:$BITNAMI_APP_USER $BITNAMI_APP_VOL_PREFIX/conf/ $BITN
 if [ "$1" = 'catalina.sh' ]; then
   set -- $@ $EXTRA_OPTIONS
 
-  if [ ! -d /app/manager ]; then
-    initialize_tomcat_webapps_directory
-  fi
+  initialize_tomcat_webapps_directory
 
   exec gosu $BITNAMI_APP_USER "$@"
 else
