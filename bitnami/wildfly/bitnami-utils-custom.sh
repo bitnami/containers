1575787958da98c4ed98ceb78469628c32b9cdc7
Sameer Naik <sameer@damagehead.com>
2015-08-31 19:38:58 +0530

    removed `-Dwildfly.as.deployment.ondemand=false` from the `PROGRAM_OPTIONS`

--- bitnami/wildfly/bitnami-utils-custom.sh
+++ bitnami/wildfly/bitnami-utils-custom.sh
@@ -1,5 +1,5 @@
 # Wildfly Utility functions
-PROGRAM_OPTIONS="-Dwildfly.as.deployment.ondemand=false -b 0.0.0.0 -bmanagement 0.0.0.0"
+PROGRAM_OPTIONS="-b 0.0.0.0 -bmanagement 0.0.0.0"
 JAVA_OPTS="$JAVA_OPTS -XX:MaxPermSize=768m -Xmx768m"
 export JAVA_OPTS
 
