eb82caa77c542aa416906b7e43ef421678ff3add
Sameer Naik <sameer@damagehead.com>
2015-09-01 13:21:33 +0530

    add `config.dir` and `log.dir` configuration parameters to default `PROGRAM_OPTS`

--- containers/wildfly/bitnami-utils-custom.sh
+++ containers/wildfly/bitnami-utils-custom.sh
@@ -1,5 +1,7 @@
 # Wildfly Utility functions
-PROGRAM_OPTIONS="-b 0.0.0.0 -bmanagement 0.0.0.0"
+PROGRAM_OPTIONS="-b 0.0.0.0 -bmanagement 0.0.0.0 \
+  -Djboss.server.config.dir=$BITNAMI_APP_DIR/conf/standalone/configuration -Djboss.server.log.dir=$BITNAMI_APP_DIR/logs \
+  -Djboss.domain.config.dir=$BITNAMI_APP_DIR/conf/domain/configuration -Djboss.domain.log.dir=$BITNAMI_APP_DIR/logs"
 JAVA_OPTS="-XX:MaxPermSize=768m -Xmx768m $JAVA_OPTS"
 export JAVA_OPTS
 
