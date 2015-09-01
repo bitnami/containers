78e7f37b8e7020cc7c43fbbda7c72ce27a7e46aa
Sameer Naik <sameer@damagehead.com>
2015-09-01 10:32:08 +0530

    moved the log volume to `$BITNAMI_APP_DIR/logs`

--- containers/wildfly/post-install.sh
+++ containers/wildfly/post-install.sh
@@ -5,10 +5,14 @@ cd $BITNAMI_APP_DIR
 mv $BITNAMI_APP_DIR/standalone/configuration $BITNAMI_APP_DIR/standalone/conf.defaults
 ln -sf $BITNAMI_APP_DIR/standalone/conf $BITNAMI_APP_DIR/standalone/configuration
 
-# move the default deployments directory
+# setup the logs
+rm -rf $BITNAMI_APP_DIR/standalone/log
+ln -sf $BITNAMI_APP_DIR/logs $BITNAMI_APP_DIR/standalone/log
+
+# setup the default deployments
 mv $BITNAMI_APP_DIR/standalone/deployments $BITNAMI_APP_DIR/standalone/deployments.defaults
 
 # symlink mount points at root to install dir
 ln -s $BITNAMI_APP_DIR/standalone/deployments /app
 ln -s $BITNAMI_APP_DIR/standalone/conf $BITNAMI_APP_VOL_PREFIX/conf
-ln -s $BITNAMI_APP_DIR/standalone/log $BITNAMI_APP_VOL_PREFIX/logs
+ln -s $BITNAMI_APP_DIR/logs $BITNAMI_APP_VOL_PREFIX/logs
