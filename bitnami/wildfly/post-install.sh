6b4a74fce65c90c4a95c4de28a55920f7761819f
Sameer Naik <sameer@damagehead.com>
2015-09-01 13:22:09 +0530

    removed symlinks to logdir, configured in the `log.dir` params

--- bitnami/wildfly/post-install.sh
+++ bitnami/wildfly/post-install.sh
@@ -10,13 +10,6 @@ mkdir -p $BITNAMI_APP_DIR/conf.defaults/domain
 mv $BITNAMI_APP_DIR/domain/configuration $BITNAMI_APP_DIR/conf.defaults/domain/
 ln -sf $BITNAMI_APP_DIR/conf/domain/configuration $BITNAMI_APP_DIR/domain/
 
-# setup the logs
-rm -rf $BITNAMI_APP_DIR/standalone/log
-ln -sf $BITNAMI_APP_DIR/logs $BITNAMI_APP_DIR/standalone/log
-
-rm -rf $BITNAMI_APP_DIR/domain/log
-ln -sf $BITNAMI_APP_DIR/logs $BITNAMI_APP_DIR/domain/log
-
 # setup the default deployments
 mv $BITNAMI_APP_DIR/standalone/deployments $BITNAMI_APP_DIR/standalone/deployments.defaults
 
