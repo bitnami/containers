c9244178d3a0898c735c8d917f9ac04c58d049f2
Sameer Naik <sameer@damagehead.com>
2015-09-09 20:36:43 +0530

    tests: Master database is replicated on slave

--- containers/mariadb/test.sh
+++ containers/mariadb/test.sh
@@ -5,6 +5,8 @@ SLEEP_TIME=5
 MARIADB_DATABASE=test_database
 MARIADB_USER=test_user
 MARIADB_PASSWORD=test_password
+REPLICATION_USER=repl_user
+REPLICATION_PASSWORD=repl_password
 VOL_PREFIX=/bitnami/mariadb
 HOST_VOL_PREFIX=${HOST_VOL_PREFIX:-/tmp/bitnami/$CONTAINER_NAME}
 
@@ -168,4 +170,36 @@ create_full_container_mounted(){
   run docker run -v $HOST_VOL_PREFIX:$HOST_VOL_PREFIX --rm $IMAGE_NAME ls -l $HOST_VOL_PREFIX/conf/my.cnf $HOST_VOL_PREFIX/logs/mysqld.log
   [ $status = 0 ]
   cleanup_volumes_content
+}
+
+@test "Master database is replicated on slave" {
+  create_container -d --name $CONTAINER_NAME-master \
+   -e MARIADB_USER=$MARIADB_USER \
+   -e MARIADB_PASSWORD=$MARIADB_PASSWORD \
+   -e MARIADB_DATABASE=$MARIADB_DATABASE \
+   -e REPLICATION_MODE=master \
+   -e REPLICATION_USER=$REPLICATION_USER \
+   -e REPLICATION_PASSWORD=$REPLICATION_PASSWORD
+
+  create_container -d --name $CONTAINER_NAME-slave \
+   --link $CONTAINER_NAME-master:mariadb-master \
+   -e MASTER_HOST=$CONTAINER_NAME-master \
+   -e MASTER_USER=$MARIADB_USER \
+   -e MASTER_PASSWORD=$MARIADB_PASSWORD \
+   -e MARIADB_USER=$MARIADB_USER \
+   -e MARIADB_PASSWORD=$MARIADB_PASSWORD \
+   -e MARIADB_DATABASE=$MARIADB_DATABASE \
+   -e REPLICATION_MODE=slave \
+   -e REPLICATION_USER=$REPLICATION_USER \
+   -e REPLICATION_PASSWORD=$REPLICATION_PASSWORD
+
+  mysql_client master -u$MARIADB_USER -p$MARIADB_PASSWORD $MARIADB_DATABASE -e "CREATE TABLE users (id INT AUTO_INCREMENT, name VARCHAR(30), datum TIMESTAMP DEFAULT CURRENT_TIMESTAMP, PRIMARY KEY(id))"
+  mysql_client master -u$MARIADB_USER -p$MARIADB_PASSWORD $MARIADB_DATABASE -e "INSERT INTO users(name) VALUES ('Marko')"
+
+  run mysql_client slave -u$MARIADB_USER -p$MARIADB_PASSWORD $MARIADB_DATABASE -e "SELECT * FROM users"
+  [[ "$output" =~ "Marko" ]]
+  [ $status = 0 ]
+
+  cleanup_running_containers $CONTAINER_NAME-master
+  cleanup_running_containers $CONTAINER_NAME-slave
 }
