68c29dd1677f2f5742f568c1ccfc68ec2b541b48
Sameer Naik <sameer@damagehead.com>
2015-09-30 20:04:36 +0530

    tests: add `--noproxy nginx` argument to `curl`

--- bitnami/nginx/test.sh
+++ bitnami/nginx/test.sh
@@ -35,12 +35,12 @@ add_vhost() {
 
 @test "We can connect to the port 80 and 443" {
   create_container
-  docker run --link $CONTAINER_NAME:nginx --rm bitnami/nginx curl -L -i http://nginx:80 | {
+  docker run --link $CONTAINER_NAME:nginx --rm bitnami/nginx curl -L -i --noproxy nginx http://nginx:80 | {
     run grep "200 OK"
     [ $status = 0 ]
   }
 
-  docker run --link $CONTAINER_NAME:nginx --rm bitnami/nginx curl -L -i -k https://nginx:443 | {
+  docker run --link $CONTAINER_NAME:nginx --rm bitnami/nginx curl -L -i --noproxy nginx -k https://nginx:443 | {
     run grep "200 OK"
     [ $status = 0 ]
   }
@@ -48,12 +48,12 @@ add_vhost() {
 
 @test "Returns default page" {
   create_container
-  docker run --link $CONTAINER_NAME:nginx --rm bitnami/nginx curl -L -i http://nginx:80 | {
+  docker run --link $CONTAINER_NAME:nginx --rm bitnami/nginx curl -L -i --noproxy nginx http://nginx:80 | {
     run grep "It works!"
     [ $status = 0 ]
   }
 
-  docker run --link $CONTAINER_NAME:nginx --rm bitnami/nginx curl -L -i -k https://nginx:443 | {
+  docker run --link $CONTAINER_NAME:nginx --rm bitnami/nginx curl -L -i --noproxy nginx -k https://nginx:443 | {
     run grep "It works!"
     [ $status = 0 ]
   }
@@ -61,7 +61,7 @@ add_vhost() {
 
 @test "Logs to stdout" {
   create_container
-  docker run --link $CONTAINER_NAME:nginx --rm bitnami/nginx curl -L -i http://nginx:80
+  docker run --link $CONTAINER_NAME:nginx --rm bitnami/nginx curl -L -i --noproxy nginx http://nginx:80
   docker logs $CONTAINER_NAME | {
     run grep "GET / HTTP/1.1"
     [ $status = 0 ]
@@ -83,7 +83,7 @@ add_vhost() {
   add_vhost
   docker restart $CONTAINER_NAME
   sleep $SLEEP_TIME
-  docker run --link $CONTAINER_NAME:nginx --rm bitnami/nginx curl -L -i http://nginx:81 | {
+  docker run --link $CONTAINER_NAME:nginx --rm bitnami/nginx curl -L -i --noproxy nginx http://nginx:81 | {
     run grep "405 Not Allowed"
     [ $status = 0 ]
   }
