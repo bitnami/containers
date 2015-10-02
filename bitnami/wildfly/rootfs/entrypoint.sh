9a999e8359527d8b6e667b4ea96199189913d36b
Sameer Naik <sameer@damagehead.com>
2015-10-02 14:58:13 +0530

    configure wildfly as a s6 service

--- bitnami/wildfly/rootfs/entrypoint.sh
+++ bitnami/wildfly/rootfs/entrypoint.sh
@@ -1,36 +1,15 @@
 #!/bin/bash
-set -e
-source $BITNAMI_PREFIX/bitnami-utils.sh
 
-print_welcome_page
-
-# if command starts with an option, prepend 'standalone.sh'
 if [ "${1:0:1}" = '-' ]; then
-  EXTRA_OPTIONS="$@"
-  set -- standalone.sh
+  export EXTRA_OPTIONS="$@"
+  set --
+elif [ "${1}" == "standalone.sh" -o "${1}" == "$(which standalone.sh)" ]; then
+  export EXTRA_OPTIONS="${@:2}"
+  set --
+elif [ "${1}" == "domain.sh" -o "${1}" == "$(which domain.sh)" ]; then
+  export BITNAMI_APP_DAEMON=domain.sh
+  export EXTRA_OPTIONS="${@:2}"
+  set --
 fi
 
-if [ ! "$(ls -A $BITNAMI_APP_VOL_PREFIX/conf)" ]; then
-  generate_conf_files
-
-  if [ "$WILDFLY_PASSWORD" ]; then
-    echo ""
-    echo "Setting manager password..."
-    add-user.sh -u manager -p $WILDFLY_PASSWORD -r ManagementRealm
-  fi
-  print_app_credentials $BITNAMI_APP_NAME manager `print_wildfly_password`
-else
-  print_container_already_initialized $BITNAMI_APP_NAME
-fi
-
-chown -R $BITNAMI_APP_USER:$BITNAMI_APP_USER $BITNAMI_APP_VOL_PREFIX/conf/ $BITNAMI_APP_VOL_PREFIX/logs/ /app/ || true
-
-if [ "$1" = 'standalone.sh' -o "$1" = 'domain.sh' ]; then
-  set -- $@ $PROGRAM_OPTIONS $EXTRA_OPTIONS
-
-  initialize_wildfly_deployments_directory
-
-  exec gosu $BITNAMI_APP_USER "$@"
-else
-  exec "$@"
-fi
+exec /init "$@"
