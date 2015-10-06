d9317111a4f6c365ab861e527a7514cba0fb2e54
Sameer Naik <sameer@damagehead.com>
2015-10-06 21:51:16 +0530

    tests: added `--noproxy node` argument to `curl`

--- bitnami/node/test.sh
+++ bitnami/node/test.sh
@@ -68,6 +68,6 @@ teardown() {
   add_app
   docker exec -d $CONTAINER_NAME sh -c 'npm install express && node server.js'
   sleep 20
-  run docker run --rm --link $CONTAINER_NAME:node bitnami/node curl http://node:3000/
+  run docker run --rm --link $CONTAINER_NAME:node bitnami/node curl --noproxy node http://node:3000/
   [[ "$output" =~ "The night is dark and full of terrors" ]]
 }
