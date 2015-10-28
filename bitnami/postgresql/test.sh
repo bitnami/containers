6fbfe34623f1a7fd29b90d5a7e9b5daeb4385d1b
Sameer Naik <sameer@damagehead.com>
2015-10-28 16:21:01 +0530

    tests: Slave synchronizes with the master (delayed start)

--- bitnami/postgresql/test.sh
+++ bitnami/postgresql/test.sh
@@ -271,3 +271,27 @@ create_full_container_mounted() {
   cleanup_running_containers $CONTAINER_NAME-master
   cleanup_running_containers $CONTAINER_NAME-slave
 }
+
+@test "Slave synchronizes with the master (delayed start)" {
+  create_container -d --name $CONTAINER_NAME-master \
+   -e POSTGRESQL_USER=$POSTGRESQL_USER \
+   -e POSTGRESQL_PASSWORD=$POSTGRESQL_PASSWORD \
+   -e POSTGRESQL_DATABASE=$POSTGRESQL_DATABASE \
+   -e POSTGRESQL_REPLICATION_MODE=master \
+   -e POSTGRESQL_REPLICATION_USER=$POSTGRESQL_REPLICATION_USER \
+   -e POSTGRESQL_REPLICATION_PASSWORD=$POSTGRESQL_REPLICATION_PASSWORD
+
+  psql_client master -U $POSTGRESQL_USER $POSTGRESQL_DATABASE -c "CREATE TABLE users (id serial, name varchar(40) NOT NULL);"
+  psql_client master -U $POSTGRESQL_USER $POSTGRESQL_DATABASE -c "INSERT INTO users(name) VALUES ('Marko');"
+
+  create_container -d --name $CONTAINER_NAME-slave \
+   --link $CONTAINER_NAME-master:master \
+   -e POSTGRESQL_REPLICATION_MODE=slave
+
+  run psql_client slave -U $POSTGRESQL_USER $POSTGRESQL_DATABASE -c "SELECT * FROM users;"
+  [[ "$output" =~ "Marko" ]]
+  [ $status = 0 ]
+
+  cleanup_running_containers $CONTAINER_NAME-master
+  cleanup_running_containers $CONTAINER_NAME-slave
+}
