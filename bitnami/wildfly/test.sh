e661dd457a1a1f8d1aa832efab6892ddf753c02b
Sameer Naik <sameer@damagehead.com>
2015-11-06 20:15:46 +0530

    tests: rewrite tests for better maintenance and readability
     - Reuse docker_helper.bash script in tests
     - Speed up of tests
     - Better readability

--- bitnami/wildfly/test.sh
+++ bitnami/wildfly/test.sh
@@ -1,212 +1,215 @@
 #!/usr/bin/env bats
 
-CONTAINER_NAME=bitnami-wildfly-test
-IMAGE_NAME=${IMAGE_NAME:-bitnami/wildfly}
-SLEEP_TIME=5
 WILDFLY_USER=manager
 WILDFLY_DEFAULT_PASSWORD=wildfly
 WILDFLY_PASSWORD=test_password
-VOL_PREFIX=/bitnami/wildfly
-HOST_VOL_PREFIX=${HOST_VOL_PREFIX:-/tmp/bitnami/$CONTAINER_NAME}
 
-cleanup_running_containers() {
-  if [ "$(docker ps -a | grep $CONTAINER_NAME)" ]; then
-    docker rm -fv $CONTAINER_NAME
-  fi
-}
+# source the helper script
+APP_NAME=wildfly
+VOL_PREFIX=/bitnami/$APP_NAME
+VOLUMES=/app:$VOL_PREFIX/conf:$VOL_PREFIX/logs
+load tests/docker_helper
 
-setup() {
-  cleanup_running_containers
-  mkdir -p $HOST_VOL_PREFIX
-}
-
-teardown() {
-  cleanup_running_containers
-}
-
-cleanup_volumes_content() {
-  docker run --rm\
-    -v $HOST_VOL_PREFIX/app:/app\
-    -v $HOST_VOL_PREFIX/conf:$VOL_PREFIX/conf\
-    -v $HOST_VOL_PREFIX/logs:$VOL_PREFIX/logs\
-    $IMAGE_NAME rm -rf /app/ $VOL_PREFIX/logs/ $VOL_PREFIX/conf/
-}
-
-create_container() {
-  docker run --name $CONTAINER_NAME "$@" $IMAGE_NAME
-  sleep $SLEEP_TIME
-}
-
-create_container_domain() {
-  docker run --name $CONTAINER_NAME "$@" $IMAGE_NAME domain.sh
-  sleep $(expr $SLEEP_TIME + 5)
-}
-
-create_full_container_mounted() {
-  docker run -d --name $CONTAINER_NAME\
-   -e WILDFLY_PASSWORD=$WILDFLY_PASSWORD\
-   -v $HOST_VOL_PREFIX/app:/app\
-   -v $HOST_VOL_PREFIX/conf:$VOL_PREFIX/conf\
-   -v $HOST_VOL_PREFIX/logs:$VOL_PREFIX/logs\
-   $IMAGE_NAME
-  sleep $SLEEP_TIME
-}
-
-curl_client() {
-  docker run --link $CONTAINER_NAME:wildfly --rm $IMAGE_NAME curl --noproxy wildfly --retry 5 -L "$@"
-}
-
+# Link to container and execute jboss-cli
+# $1: name of the container to link to
+# ${@:2}: command to execute
 jboss_client() {
-  docker run --link $CONTAINER_NAME:wildfly --rm $IMAGE_NAME jboss-cli.sh --controller=wildfly:9990  "$@"
+  container_link_and_run_command $1 jboss-cli.sh --controller=$APP_NAME:9990 "${@:2}"
 }
 
-@test "Ports 8080 and 9990 exposed and accepting external connections (standalone server)" {
-  create_container -d
-  run curl_client -i http://wildfly:8080
+# Cleans up all running/stopped containers and host mounted volumes
+cleanup_environment() {
+  container_remove_full standalone
+  container_remove_full domain
+}
+
+# Teardown called at the end of each test
+teardown() {
+  cleanup_environment
+}
+
+# cleanup the environment before starting the tests
+cleanup_environment
+
+@test "Ports 8080 and 9990 exposed and accepting external connections (standalone)" {
+  container_create standalone -d
+
+  run curl_client standalone -i http://$APP_NAME:8080
   [[ "$output" =~ '200 OK' ]]
-  run curl_client -i http://wildfly:9990
+
+  run curl_client standalone -i http://$APP_NAME:9990
   [[ "$output" =~ '200 OK' ]]
 }
 
-@test "Ports 8080 and 9990 exposed and accepting external connections (managed domain)" {
-  create_container_domain -d
+@test "Ports 8080 and 9990 exposed and accepting external connections (domain)" {
+  container_create domain -d \
+    -e BITNAMI_APP_DAEMON=domain.sh
 
   sleep $SLEEP_TIME
-  run curl_client -i http://wildfly:8080
+  run curl_client domain -i http://$APP_NAME:8080
   [[ "$output" =~ '200 OK' ]]
-  run curl_client -i http://wildfly:9990
-  [[ "$output" =~ '200 OK' ]]
-}
-
-@test "Manager has access to management area (standalone server)" {
-  create_container -d
-  run curl_client -i --digest http://$WILDFLY_USER:$WILDFLY_DEFAULT_PASSWORD@wildfly:9990/management
+  run curl_client domain -i http://$APP_NAME:9990
   [[ "$output" =~ '200 OK' ]]
 }
 
-@test "Manager has access to management area (managed domain)" {
-  create_container_domain -d
-  run curl_client -i --digest http://$WILDFLY_USER:$WILDFLY_DEFAULT_PASSWORD@wildfly:9990/management
+@test "Manager has access to management area (standalone)" {
+  container_create standalone -d
+  run curl_client standalone -i --digest http://$WILDFLY_USER:$WILDFLY_DEFAULT_PASSWORD@$APP_NAME:9990/management
   [[ "$output" =~ '200 OK' ]]
 }
 
-@test "User manager created with custom password (standalone server)" {
-  create_container -d -e WILDFLY_PASSWORD=$WILDFLY_PASSWORD
-  run curl_client -i --digest http://$WILDFLY_USER:$WILDFLY_PASSWORD@wildfly:9990/management
+@test "Manager has access to management area (domain)" {
+  container_create domain -d \
+    -e BITNAMI_APP_DAEMON=domain.sh
+
+  run curl_client domain -i --digest http://$WILDFLY_USER:$WILDFLY_DEFAULT_PASSWORD@$APP_NAME:9990/management
   [[ "$output" =~ '200 OK' ]]
 }
 
-@test "User manager created with custom password (managed domain)" {
-  create_container_domain -d -e WILDFLY_PASSWORD=$WILDFLY_PASSWORD
-  run curl_client -i --digest http://$WILDFLY_USER:$WILDFLY_PASSWORD@wildfly:9990/management
+@test "User manager created with custom password (standalone)" {
+  container_create standalone -d \
+    -e WILDFLY_PASSWORD=$WILDFLY_PASSWORD
+
+  run curl_client standalone -i --digest http://$WILDFLY_USER:$WILDFLY_PASSWORD@$APP_NAME:9990/management
   [[ "$output" =~ '200 OK' ]]
 }
 
-@test "Can't access management area without password (standalone server)" {
-  create_container -d -e WILDFLY_PASSWORD=$WILDFLY_PASSWORD
-  run curl_client -i --digest http://$WILDFLY_USER@wildfly:9990/management
+@test "User manager created with custom password (domain)" {
+  container_create domain -d \
+    -e BITNAMI_APP_DAEMON=domain.sh \
+    -e WILDFLY_PASSWORD=$WILDFLY_PASSWORD
+
+  run curl_client domain -i --digest http://$WILDFLY_USER:$WILDFLY_PASSWORD@$APP_NAME:9990/management
+  [[ "$output" =~ '200 OK' ]]
+}
+
+@test "Can't access management area without password (standalone)" {
+  container_create standalone -d \
+    -e WILDFLY_PASSWORD=$WILDFLY_PASSWORD
+  run curl_client standalone -i --digest http://$WILDFLY_USER@$APP_NAME:9990/management
   [[ "$output" =~ '401 Unauthorized' ]]
 }
 
-@test "Can't access management area without password (managed domain)" {
-  create_container_domain -d -e WILDFLY_PASSWORD=$WILDFLY_PASSWORD
-  run curl_client -i --digest http://$WILDFLY_USER@wildfly:9990/management
+@test "Can't access management area without password (domain)" {
+  container_create domain -d \
+    -e BITNAMI_APP_DAEMON=domain.sh \
+    -e WILDFLY_PASSWORD=$WILDFLY_PASSWORD
+  run curl_client domain -i --digest http://$WILDFLY_USER@$APP_NAME:9990/management
   [[ "$output" =~ '401 Unauthorized' ]]
 }
 
-@test "jboss-cli.sh can connect to Wildfly server (standalone server)" {
-  create_container -d -e WILDFLY_PASSWORD=$WILDFLY_PASSWORD
-  run jboss_client --connect --user=$WILDFLY_USER --password=$WILDFLY_PASSWORD --command=version
+@test "jboss-cli.sh can connect to Wildfly server (standalone)" {
+  container_create standalone -d \
+    -e WILDFLY_PASSWORD=$WILDFLY_PASSWORD
+  run jboss_client standalone --connect --user=$WILDFLY_USER --password=$WILDFLY_PASSWORD --command=version
   [ $status = 0 ]
 }
 
-@test "jboss-cli.sh can connect to Wildfly server (managed domain)" {
-  create_container_domain -d -e WILDFLY_PASSWORD=$WILDFLY_PASSWORD
-  run jboss_client --connect --user=$WILDFLY_USER --password=$WILDFLY_PASSWORD --command=version
+@test "jboss-cli.sh can connect to Wildfly server (domain)" {
+  container_create domain -d \
+    -e BITNAMI_APP_DAEMON=domain.sh \
+    -e WILDFLY_PASSWORD=$WILDFLY_PASSWORD
+  run jboss_client domain --connect --user=$WILDFLY_USER --password=$WILDFLY_PASSWORD --command=version
   [ $status = 0 ]
 }
 
-@test "jboss-cli.sh can't access Wildfly server without password (standalone server)" {
-  create_container -d -e WILDFLY_PASSWORD=$WILDFLY_PASSWORD
-  run jboss_client --connect --user=$WILDFLY_USER --command=version
+@test "jboss-cli.sh can't access Wildfly server without password (standalone)" {
+  container_create standalone -d \
+    -e WILDFLY_PASSWORD=$WILDFLY_PASSWORD
+  run jboss_client standalone --connect --user=$WILDFLY_USER --command=version
   [[ "$output" =~ "Unable to authenticate against controller" ]]
   [ $status = 1 ]
 }
 
-@test "jboss-cli.sh can't access Wildfly server without password (managed domain)" {
-  create_container_domain -d -e WILDFLY_PASSWORD=$WILDFLY_PASSWORD
-  run jboss_client --connect --user=$WILDFLY_USER --command=version
+@test "jboss-cli.sh can't access Wildfly server without password (domain)" {
+  container_create domain -d \
+    -e BITNAMI_APP_DAEMON=domain.sh \
+    -e WILDFLY_PASSWORD=$WILDFLY_PASSWORD
+  run jboss_client domain --connect --user=$WILDFLY_USER --command=version
   [[ "$output" =~ "Unable to authenticate against controller" ]]
   [ $status = 1 ]
 }
 
-@test "Password is preserved after restart (standalone server)" {
-  create_container -d -e WILDFLY_PASSWORD=$WILDFLY_PASSWORD
+@test "Password is preserved after restart (standalone)" {
+  container_create standalone -d \
+    -e WILDFLY_PASSWORD=$WILDFLY_PASSWORD
 
-  docker stop $CONTAINER_NAME
-  docker start $CONTAINER_NAME
-  sleep $SLEEP_TIME
+  # restart container
+  container_restart standalone
 
-  run docker logs $CONTAINER_NAME
+  # get logs
+  run container_logs standalone
   [[ "$output" =~ "The credentials were set on first boot." ]]
 
-  run curl_client -i --digest http://$WILDFLY_USER:$WILDFLY_PASSWORD@wildfly:9990/management
+  run curl_client standalone -i --digest http://$WILDFLY_USER:$WILDFLY_PASSWORD@$APP_NAME:9990/management
   [[ "$output" =~ '200 OK' ]]
 }
 
-@test "Password is preserved after restart (managed domain)" {
-  create_container_domain -d -e WILDFLY_PASSWORD=$WILDFLY_PASSWORD
+@test "Password is preserved after restart (domain)" {
+  container_create domain -d \
+    -e BITNAMI_APP_DAEMON=domain.sh \
+    -e WILDFLY_PASSWORD=$WILDFLY_PASSWORD
 
-  docker stop $CONTAINER_NAME
-  docker start $CONTAINER_NAME
-  sleep $SLEEP_TIME
+  # restart container
+  container_restart domain
 
-  run docker logs $CONTAINER_NAME
+  # get logs
+  run container_logs domain
   [[ "$output" =~ "The credentials were set on first boot." ]]
 
-  run curl_client -i --digest http://$WILDFLY_USER:$WILDFLY_PASSWORD@wildfly:9990/management
+  run curl_client domain -i --digest http://$WILDFLY_USER:$WILDFLY_PASSWORD@$APP_NAME:9990/management
   [[ "$output" =~ '200 OK' ]]
 }
 
 @test "All the volumes exposed" {
-  create_container -d
-  run docker inspect $CONTAINER_NAME
+  container_create standalone -d
+  run container_inspect standalone --format {{.Mounts}}
   [[ "$output" =~ "$VOL_PREFIX/conf" ]]
   [[ "$output" =~ "$VOL_PREFIX/logs" ]]
 }
 
 @test "Data gets generated in conf and app volumes if bind mounted in the host" {
-  create_full_container_mounted
-  run docker run -v $HOST_VOL_PREFIX:$HOST_VOL_PREFIX --rm $IMAGE_NAME ls -la $HOST_VOL_PREFIX/conf/standalone $HOST_VOL_PREFIX/conf/domain $HOST_VOL_PREFIX/logs/server.log $HOST_VOL_PREFIX/app/.initialized
-  [ $status = 0 ]
-  cleanup_volumes_content
+  container_create_with_host_volumes standalone -d
+
+  # files expected in conf volume
+  run container_exec standalone ls -la $VOL_PREFIX/conf/
+  [[ "$output" =~ "domain" ]]
+  [[ "$output" =~ "standalone" ]]
+
+  # files expected in app volume (subset)
+  run container_exec standalone ls -la /app/
+  [[ "$output" =~ ".initialized" ]]
+  [[ "$output" =~ "mysql-connector-java" ]]
+  [[ "$output" =~ "postgresql" ]]
+
+  # files expected in logs volume
+  run container_exec standalone ls -la $VOL_PREFIX/logs/
+  [[ "$output" =~ "server.log" ]]
 }
 
 @test "If host mounted, password and settings are preserved after deletion" {
-  cleanup_volumes_content
-  create_full_container_mounted
+  container_create_with_host_volumes standalone -d \
+    -e WILDFLY_PASSWORD=$WILDFLY_PASSWORD
 
-  docker rm -fv $CONTAINER_NAME
-  create_container -d -v $HOST_VOL_PREFIX/app:/app -v $HOST_VOL_PREFIX/conf:$VOL_PREFIX/conf
+  # remove container
+  container_remove standalone
 
-  run curl_client -i --digest http://$WILDFLY_USER:$WILDFLY_PASSWORD@wildfly:9990/management
+  # recreate container without specifying any env parameters
+  container_create_with_host_volumes standalone -d
+
+  run curl_client standalone -i --digest http://$WILDFLY_USER:$WILDFLY_PASSWORD@$APP_NAME:9990/management
   [[ "$output" =~ '200 OK' ]]
-  cleanup_volumes_content
 }
 
-@test "Deploy sample application on standalone server" {
-  cleanup_volumes_content
-  create_full_container_mounted
+@test "Deploy sample application on standalone" {
+  container_create_with_host_volumes standalone -d \
+    -e WILDFLY_PASSWORD=$WILDFLY_PASSWORD
 
-  run docker run --rm \
-    --link $CONTAINER_NAME:wildfly \
-    -v $HOST_VOL_PREFIX/app:/app \
-    $IMAGE_NAME curl https://raw.githubusercontent.com/goldmann/wildfly-docker-deployment-example/master/node-info.war -o /app/node-info.war
-  [ $status = 0 ]
+  # download sample app into the deployments directory and allow it some time to come up
+  container_exec standalone curl --noproxy localhost --retry 5 https://raw.githubusercontent.com/goldmann/wildfly-docker-deployment-example/master/node-info.war -o /app/node-info.war
   sleep $SLEEP_TIME
 
-  run curl_client -i http://wildfly:8080/node-info/
+  # test the deployment
+  run curl_client standalone -i http://$APP_NAME:8080/node-info/
   [[ "$output" =~ '200 OK' ]]
-
-  cleanup_volumes_content
 }
