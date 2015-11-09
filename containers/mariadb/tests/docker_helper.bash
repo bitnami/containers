c14c4ff253ed075baf665501e8b159daa2aa51fd
Sameer Naik <sameer@damagehead.com>
2015-11-09 19:15:59 +0530

    tests: update to latest docker_helper

--- containers/mariadb/tests/docker_helper.bash
+++ containers/mariadb/tests/docker_helper.bash
@@ -6,12 +6,22 @@
 # The following variables should be defined in you BATS script for this helper
 # script to work correctly.
 #
-# CONTAINER_NAME   - prefix for the name of containers that will be created
-# IMAGE_NAME       - the docker image name
-# SLEEP_TIME       - time in seconds to wait for containers to start
-# VOL_PREFIX       - prefix of volumes inside the container
-# HOST_VOL_PREFIX  - prefix of volumes mounted from the host
+# APP_NAME                                    - app name, used as link alias in container_link_and_run_command
+# CONTAINER_NAME                              - prefix for the name of containers that will be created (default: bitnami-$APP_NAME-test)
+# IMAGE_NAME                                  - the docker image name (default: bitnami/$APP_NAME)
+# SLEEP_TIME                                  - time in seconds to wait for containers to start (default: 5)
+# VOL_PREFIX                                  - prefix of volumes inside the container (default: /bitnami/$APP_NAME)
+# VOLUMES                                     - colon separated list of container volumes (default: $VOL_PREFIX/data:$VOL_PREFIX/conf:$VOL_PREFIX/logs)
+# HOST_VOL_PREFIX                             - prefix of volumes mounted from the host (default: /tmp/bitnami/$CONTAINER_NAME)
+# container_link_and_run_command_DOCKER_ARGS  - optional arguments passed to docker run in container_link_and_run_command (default: none)
 ##
+
+CONTAINER_NAME=bitnami-$APP_NAME-test
+IMAGE_NAME=${IMAGE_NAME:-bitnami/$APP_NAME}
+SLEEP_TIME=${SLEEP_TIME:-5}
+VOL_PREFIX=${VOL_PREFIX:-/bitnami/$APP_NAME}
+VOLUMES=${VOLUMES:-$VOL_PREFIX/data:$VOL_PREFIX/conf:$VOL_PREFIX/logs}
+HOST_VOL_PREFIX=${HOST_VOL_PREFIX:-/tmp/bitnami/$CONTAINER_NAME}
 
 # Creates a container whose name has the prefix $CONTAINER_NAME
 # $1: name for the new container
@@ -21,20 +31,28 @@ container_create() {
   sleep $SLEEP_TIME
 }
 
-# Creates a container with host volumes mounted at $VOL_PREFIX
+# Creates a container with host mounted volumes for volumes listed in VOLUMES
 # $1: name for the new container
 # ${@:2}: additional arguments for docker run while starting the container
 container_create_with_host_volumes() {
-  container_create $1 "${@:2}" \
-    -v $HOST_VOL_PREFIX/$1/data:$VOL_PREFIX/data \
-    -v $HOST_VOL_PREFIX/$1/conf:$VOL_PREFIX/conf \
-    -v $HOST_VOL_PREFIX/$1/logs:$VOL_PREFIX/logs
+  # populate volume mount arguments from VOLUMES variable
+  VOLUME_ARGS=
+  OLD_IFS=${IFS}
+  IFS=":"
+  for VOLUME in $VOLUMES
+  do
+    VOL_NAME=$(basename $VOLUME)
+    VOLUME_ARGS+="-v $HOST_VOL_PREFIX/$1/$VOL_NAME:$VOLUME "
+  done
+  IFS=${OLD_IFS}
+
+  container_create $1 "${@:2}" $VOLUME_ARGS
 }
 
 # Start a stopped container
 # $1: name of the container
 container_start() {
-  if [ "$(docker ps -a | grep $CONTAINER_NAME-$1)" ]; then
+  if docker ps -a | grep -q $CONTAINER_NAME-$1; then
     docker start $CONTAINER_NAME-$1
     sleep $SLEEP_TIME
   else
@@ -45,7 +63,7 @@ container_start() {
 # Stop a running container
 # $1: name of the container
 container_stop() {
-  if [ "$(docker ps -a | grep $CONTAINER_NAME-$1)" ]; then
+  if docker ps | grep -q $CONTAINER_NAME-$1; then
     docker stop $CONTAINER_NAME-$1
   else
     return 1
@@ -55,15 +73,19 @@ container_stop() {
 # Restart a running container (stops the container and then starts it)
 # $1: name of the container
 container_restart() {
-  container_stop $1
-  container_start $1
+  if docker ps | grep -q $CONTAINER_NAME-$1; then
+    docker stop $CONTAINER_NAME-$1
+    docker start $CONTAINER_NAME-$1
+    sleep $SLEEP_TIME
+  fi
 }
 
 # Remove a running/stopped container
 # $1: name of the container
 container_remove() {
-  if [ "$(docker ps -a | grep $CONTAINER_NAME-$1)" ]; then
-    docker rm -fv $CONTAINER_NAME-$1
+  if docker ps -a | grep -q $CONTAINER_NAME-$1; then
+    docker stop $CONTAINER_NAME-$1
+    docker rm -v $CONTAINER_NAME-$1
   fi
 }
 
@@ -71,17 +93,28 @@ container_remove() {
 # $1: name of the container
 container_remove_full() {
   container_remove $1
-  docker run --rm --entrypoint bash \
-    -v $HOST_VOL_PREFIX/$1/data:$VOL_PREFIX/data \
-    -v $HOST_VOL_PREFIX/$1/conf:$VOL_PREFIX/conf \
-    -v $HOST_VOL_PREFIX/$1/logs:$VOL_PREFIX/logs \
-      $IMAGE_NAME -c "rm -rf $VOL_PREFIX/data/ $VOL_PREFIX/logs/ $VOL_PREFIX/conf/"
+
+  # populate volume mount and rm arguments from VOLUMES variable
+  VOLUME_ARGS=
+  RM_ARGS=
+  OLD_IFS=${IFS}
+  IFS=":"
+  for VOLUME in $VOLUMES
+  do
+    VOL_NAME=$(basename $VOLUME)
+    VOLUME_ARGS+="-v $HOST_VOL_PREFIX/$1/$VOL_NAME:$VOLUME "
+    RM_ARGS+="$VOLUME/* $VOLUME/.[^.]* "
+  done
+  IFS=${OLD_IFS}
+
+  docker run --rm --entrypoint bash $VOLUME_ARGS \
+      $IMAGE_NAME -c "rm -rf $RM_ARGS"
 }
 
 # Get the logs of a container
 # $1: name of the container
 container_logs() {
-  if [ "$(docker ps -a | grep $CONTAINER_NAME-$1)" ]; then
+  if docker ps -a | grep -q $CONTAINER_NAME-$1; then
     docker logs $CONTAINER_NAME-$1
   else
     return 1
@@ -91,7 +124,8 @@ container_logs() {
 # Docker inspect a container
 # $1: name of the container
 container_inspect() {
-  if [ "$(docker ps -a | grep $CONTAINER_NAME-$1)" ]; then
+  if docker ps -a | grep -q $CONTAINER_NAME-$1; then
+    # docker inspect "${@:2}" $CONTAINER_NAME-$1 # requires docker >= 1.9.0
     docker inspect $CONTAINER_NAME-$1
   else
     return 1
@@ -101,7 +135,7 @@ container_inspect() {
 # Execute a command in a running container using docker exec
 # $1: name of the container
 container_exec() {
-  if [ "$(docker ps | grep $CONTAINER_NAME-$1)" ]; then
+  if docker ps | grep -q $CONTAINER_NAME-$1; then
     docker exec $CONTAINER_NAME-$1 "${@:2}"
   else
     return 1
@@ -111,19 +145,33 @@ container_exec() {
 # Execute a command in a running container using docker exec (detached)
 # $1: name of the container
 container_exec_detached() {
-  if [ "$(docker ps | grep $CONTAINER_NAME-$1)" ]; then
+  if docker ps | grep -q $CONTAINER_NAME-$1; then
     docker exec -d $CONTAINER_NAME-$1 "${@:2}"
   else
     return 1
   fi
 }
 
-
 # Generates docker link parameter for linking to a container
 # $1: name of the container to link
 # $2: alias for the link
 container_link() {
-  if [ "$(docker ps -a | grep $CONTAINER_NAME-$1)" ]; then
+  if docker ps -a | grep -q $CONTAINER_NAME-$1; then
     echo "--link $CONTAINER_NAME-$1:$2"
   fi
+}
+
+# Link to container and execute command
+# $1: name of the container to link to
+# ${@:2}: command to execute
+container_link_and_run_command() {
+  # launch command as the entrypoint to skip the s6 init sequence (speeds up the tests)
+  docker run --rm $(container_link $1 $APP_NAME) $container_link_and_run_command_DOCKER_ARGS --entrypoint ${2} $IMAGE_NAME "${@:3}"
+}
+
+# Link to container and execute curl
+# $1: name of the container to link to
+# ${@:2}: arguments to curl
+curl_client() {
+  container_link_and_run_command $1 curl --noproxy $APP_NAME --retry 5 -L "${@:2}"
 }
