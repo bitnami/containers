4ad0ff2fe902fdbf5c1796c51fa7b8a36a1ae533
Sameer Naik <sameer@damagehead.com>
2016-01-12 12:37:29 +0530

    avoid duplicate entries in `postgresql.conf`

--- bitnami/postgresql/post-install.sh
+++ bitnami/postgresql/post-install.sh
@@ -10,13 +10,10 @@ mv $BITNAMI_APP_DIR/data/pg_hba.conf conf.defaults/
 mv $BITNAMI_APP_DIR/data/pg_ident.conf conf.defaults/
 rm -rf $BITNAMI_APP_DIR/data
 
-# listen on all interface and enable logging_collector
-cat >> $BITNAMI_APP_DIR/conf.defaults/postgresql.conf <<EOF
-listen_addresses='*'
-logging_collector = on
-log_directory = '$BITNAMI_APP_DIR/logs'
-log_filename = 'postgresql.log'
-EOF
+sed -i "s|^[#]*[ ]*listen_addresses = .*|listen_addresses = '*'|" $BITNAMI_APP_DIR/conf.defaults/postgresql.conf
+sed -i "s|^[#]*[ ]*logging_collector = .*|logging_collector = on|" $BITNAMI_APP_DIR/conf.defaults/postgresql.conf
+sed -i "s|^[#]*[ ]*log_directory = .*|log_directory = '$BITNAMI_APP_DIR/logs'|" $BITNAMI_APP_DIR/conf.defaults/postgresql.conf
+sed -i "s|^[#]*[ ]*log_filename = .*|log_filename = 'postgresql.log'|" $BITNAMI_APP_DIR/conf.defaults/postgresql.conf
 
 cat >> $BITNAMI_APP_DIR/conf.defaults/pg_hba.conf <<EOF
 host    all             all             0.0.0.0/0               md5
