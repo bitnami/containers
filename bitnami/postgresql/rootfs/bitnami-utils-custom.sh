7e9a8df0c543843379009f39b6922607fef79843
Sameer Naik <sameer@damagehead.com>
2016-01-13 12:23:56 +0530

    renamed function `configure_replication_slave` to `configure_recovery`

--- bitnami/postgresql/rootfs/bitnami-utils-custom.sh
+++ bitnami/postgresql/rootfs/bitnami-utils-custom.sh
@@ -160,15 +160,12 @@ create_replication_user() {
   fi
 }
 
-configure_replication_slave() {
+configure_recovery() {
   if [ "$POSTGRESQL_REPLICATION_MODE" == "slave" ]; then
     echo "==> Setting up streaming replication slave..."
     echo ""
 
-    if [ ! -f $BITNAMI_APP_DIR/data/recovery.conf ]; then
-      s6-setuidgid $BITNAMI_APP_USER cp $BITNAMI_APP_DIR/share/recovery.conf.sample $BITNAMI_APP_DIR/data/recovery.conf
-    fi
-
+    s6-setuidgid $BITNAMI_APP_USER cp -a $BITNAMI_APP_DIR/share/recovery.conf.sample $BITNAMI_APP_DIR/data/recovery.conf
     s6-setuidgid $BITNAMI_APP_USER sed -i "s|^[#]*[ ]*standby_mode = .*|standby_mode = on|" $BITNAMI_APP_DIR/data/recovery.conf
     s6-setuidgid $BITNAMI_APP_USER sed -i "s|^[#]*[ ]*primary_conninfo = .*|primary_conninfo = 'host=${POSTGRESQL_MASTER_HOST} port=${POSTGRESQL_MASTER_PORT} user=${POSTGRESQL_REPLICATION_USER} password=${POSTGRESQL_REPLICATION_PASSWORD}'|" $BITNAMI_APP_DIR/data/recovery.conf
     s6-setuidgid $BITNAMI_APP_USER sed -i "s|^[#]*[ ]*trigger_file = .*|trigger_file = '/tmp/postgresql.trigger.5432'|" $BITNAMI_APP_DIR/data/recovery.conf
