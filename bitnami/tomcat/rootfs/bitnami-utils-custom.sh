9c74910421bff06324a620b193142e958ada1b82
Sameer Naik <sameer@damagehead.com>
2016-01-17 23:32:22 +0530

    moved password setting into `set_manager_password` function

--- bitnami/tomcat/rootfs/bitnami-utils-custom.sh
+++ bitnami/tomcat/rootfs/bitnami-utils-custom.sh
@@ -7,6 +7,14 @@ initialize_webapps_directory() {
   touch /app/.initialized
 }
 
+set_manager_password() {
+  if [ "$TOMCAT_PASSWORD" ]; then
+    echo ""
+    echo "==> Setting manager password..."
+    sed -i "s/^\(<user username=\"manager\" password=\"\)\([^ \"]*\)\(\".*\)/\1$TOMCAT_PASSWORD\3/" $BITNAMI_APP_VOL_PREFIX/conf/tomcat-users.xml
+  fi
+}
+
 print_tomcat_password() {
   if [ -z $TOMCAT_PASSWORD ]; then
     echo "**none**"
