940c2b0d490acd740b414dc375b5e14076c493be
Sameer Naik <sameer@damagehead.com>
2016-01-18 00:06:06 +0530

    renamed `initialize_wildfly_deployments_directory` to `initialize_deployments_directory`

--- bitnami/wildfly/rootfs/bitnami-utils-custom.sh
+++ bitnami/wildfly/rootfs/bitnami-utils-custom.sh
@@ -5,16 +5,11 @@ PROGRAM_OPTIONS="-b 0.0.0.0 -bmanagement 0.0.0.0 \
 JAVA_OPTS="-XX:MaxPermSize=768m -Xmx768m $JAVA_OPTS"
 export JAVA_OPTS
 
-initialize_wildfly_deployments_directory() {
-  if [ ! -f /app/.initialized ]; then
-    echo "==> Initializing Wildfly deployments directory..."
-    echo ""
-    cp -a $BITNAMI_APP_DIR/standalone/deployments.defaults/* /app/
-    touch /app/.initialized
-  else
-    echo "==> Wildfly deployments directory already initialized..."
-    echo ""
-  fi
+initialize_deployments_directory() {
+  echo "==> Initializing deployments directory..."
+  echo ""
+  cp -a $BITNAMI_APP_DIR/standalone/deployments.defaults/* /app/
+  touch /app/.initialized
 }
 
 print_wildfly_password() {
