8a584eb66ca9a7fb8762b1a97350df7766832b08
Sameer Naik <sameer@damagehead.com>
2016-01-17 14:14:06 +0530

    renamed `initialize_tomcat_webapps_directory` to `initialize_webapps_directory`

--- containers/tomcat/rootfs/bitnami-utils-custom.sh
+++ containers/tomcat/rootfs/bitnami-utils-custom.sh
@@ -1,15 +1,10 @@
 # Tomcat Utility functions
 
-initialize_tomcat_webapps_directory() {
-  if [ ! -f /app/.initialized ]; then
-    echo "==> Initializing Tomcat webapps directory..."
-    echo ""
-    cp -a $BITNAMI_APP_DIR/webapps.defaults/* /app/
-    touch /app/.initialized
-  else
-    echo "==> Tomcat webapps directory already initialized..."
-    echo ""
-  fi
+initialize_webapps_directory() {
+  echo "==> Initializing webapps directory..."
+  echo ""
+  cp -a $BITNAMI_APP_DIR/webapps.defaults/* /app/
+  touch /app/.initialized
 }
 
 print_tomcat_password() {
