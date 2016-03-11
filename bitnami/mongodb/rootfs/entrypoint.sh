028b3cf3b779250892eabde462a4332542613ddf
Sameer Naik <sameer@damagehead.com>
2016-03-11 14:31:08 +0530

    hack: create the `data/db` directory before `harpoon initialize`

--- bitnami/mongodb/rootfs/entrypoint.sh
+++ bitnami/mongodb/rootfs/entrypoint.sh
@@ -55,6 +55,10 @@ check_for_updates &
 if [ "$1" == "harpoon" ]; then
   status=`harpoon inspect $BITNAMI_APP_NAME`
   if [[ "$status" == *'"lifecycle": "unpacked"'* ]]; then
+    # HACK: harpoon initialize should create the db directory
+    mkdir -p $BITNAMI_APP_DIR/data/db
+    chown -R $BITNAMI_APP_USER:root $BITNAMI_APP_DIR/data/db
+
     harpoon initialize $BITNAMI_APP_NAME \
       --username ${MONGODB_USER:-root} \
       --password ${MONGODB_PASSWORD:-password}
