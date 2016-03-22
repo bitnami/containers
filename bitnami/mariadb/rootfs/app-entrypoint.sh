1842fdca846919e4857068fd2874fe4d90aede2a
Sameer Naik <sameer@damagehead.com>
2016-03-22 13:09:42 +0530

    fix typo

--- bitnami/mariadb/rootfs/app-entrypoint.sh
+++ bitnami/mariadb/rootfs/app-entrypoint.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -e
 
-#!/bin/bash
-set -e
-
 if [[ "$1" == "harpoon" && "$2" == "start" ]]; then
   status=`harpoon inspect $BITNAMI_APP_NAME`
   if [[ "$status" == *'"lifecycle": "unpacked"'* ]]; then
