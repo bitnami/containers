a95c39ae3c0d8bcb0c9057724ff9573eef3b89b4
Fran <fortiz@bitnami.com>
2016-07-04 09:25:47 +0200

    Initial commit

---
+++ containers/testlink/rootfs/app-entrypoint.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -e
+
+function initialize {
+    # Package can be "installed" or "unpacked"
+    status=`harpoon inspect $1`
+    if [[ "$status" == *'"lifecycle": "unpacked"'* ]]; then
+        # Clean up inputs
+        inputs=""
+        if [[ -f /$1-inputs.json ]]; then
+            inputs=--inputs-file=/$1-inputs.json
+        fi
+        harpoon initialize $1 $inputs
+    fi
+}
+
+# Set default values
+export APACHE_HTTP_PORT=${APACHE_HTTP_PORT:-"80"}
+export APACHE_HTTPS_PORT=${APACHE_HTTPS_PORT:-"443"}
+export TESTLINK_LANGUAGE=${TESTLINK_LANGUAGE:-"en_US"}
+export MARIADB_USER=${MARIADB_USER:-"root"}
+export MARIADB_HOST=${MARIADB_HOST:-"mariadb"}
+export MARIADB_PORT=${MARIADB_PORT:-"3306"}
+
+
+
+if [[ "$1" == "harpoon" && "$2" == "start" ]] ||  [[ "$1" == "/init.sh" ]]; then
+   for module in apache testlink; do
+    initialize $module
+   done
+   echo "Starting application ..."
+fi
+
+exec /entrypoint.sh "$@"
