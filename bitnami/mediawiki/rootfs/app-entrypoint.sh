9813fdc61caa5062a8ceb27919aca0047454f337
Jorge Marin <jorge@bitnami.com>
2016-07-26 17:02:21 +0200

    New Mediawiki version 1.27.0

--- bitnami/mediawiki/rootfs/app-entrypoint.sh
+++ bitnami/mediawiki/rootfs/app-entrypoint.sh
@@ -5,6 +5,8 @@ function initialize {
     # Package can be "installed" or "unpacked"
     status=`harpoon inspect $1`
     if [[ "$status" == *'"lifecycle": "unpacked"'* ]]; then
+        # Clean up inputs
+        inputs=""
         if [[ -f /$1-inputs.json ]]; then
             inputs=--inputs-file=/$1-inputs.json
         fi
@@ -13,9 +15,12 @@ function initialize {
 }
 
 # Set default values
+export APACHE_HTTP_PORT=${APACHE_HTTP_PORT:-"80"}
+export APACHE_HTTPS_PORT=${APACHE_HTTPS_PORT:-"443"}
 export MEDIAWIKI_USERNAME=${MEDIAWIKI_USERNAME:-"user"}
 export MEDIAWIKI_PASSWORD=${MEDIAWIKI_PASSWORD:-"bitnami1"}
 export MEDIAWIKI_EMAIL=${MEDIAWIKI_EMAIL:-"user@example.com"}
+export MEDIAWIKI_WIKI_NAME=${MEDIAWIKI_WIKI_NAME:-"Bitnami MediaWiki"}
 export MARIADB_USER=${MARIADB_USER:-"root"}
 export MARIADB_HOST=${MARIADB_HOST:-"mariadb"}
 export MARIADB_PORT=${MARIADB_PORT:-"3306"}
