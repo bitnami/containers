e32231be2bdb08ba79ed5cbefcbac907ccf00dbf
Alejandro Polvillo <apolvillo@bitnami.com>
2016-08-29 11:51:13 +0200

    First commit. Initial release

---
+++ containers/opencart/rootfs/app-entrypoint.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+set -e
+
+function initialize {
+    # Package can be "installed" or "unpacked"
+    status=`harpoon inspect $1`
+    if [[ "$status" == *'"lifecycle": "unpacked"'* ]]; then
+        # Clean up inputs
+        inputs=""
+        if [[ -f /$1-inputs.json ]]; then
+            inputs=--inputs-file=/$1-inputs.json
+        fi
+        harpoon initialize $1 $inputs
+    fi
+}
+
+# Set default values
+export APACHE_HTTP_PORT=${APACHE_HTTP_PORT:-"80"}
+export APACHE_HTTPS_PORT=${APACHE_HTTPS_PORT:-"443"}
+export OPENCART_USER_NAME=${OPENCART_USER_NAME:-"user"}
+export OPENCART_PASSWORD=${OPENCART_PASSWORD:-"bitnami1"}
+export OPENCART_EMAIL=${OPENCART_EMAIL:-"user@example.com"}
+export MARIADB_USER=${MARIADB_USER:-"root"}
+export MARIADB_HOST=${MARIADB_HOST:-"mariadb"}
+export MARIADB_PORT=${MARIADB_PORT:-"3306"}
+
+
+
+if [[ "$1" == "harpoon" && "$2" == "start" ]] ||  [[ "$1" == "/init.sh" ]]; then
+   for module in apache opencart; do
+    initialize $module
+   done
+   echo "Starting application ..."
+fi
+
+exec /entrypoint.sh "$@"
