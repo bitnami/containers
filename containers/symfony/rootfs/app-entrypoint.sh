db62cf493c0030f723bde0dfe07820ddac48067c
dgonzalez <dgonzalez@bitnami.com>
2016-08-31 12:44:43 +0200

    Generalized improvements of workability.

--- containers/symfony/rootfs/app-entrypoint.sh
+++ containers/symfony/rootfs/app-entrypoint.sh
@@ -5,23 +5,26 @@ set -e
 export MARIADB_USER=${MARIADB_USER:-"root"}
 export MARIADB_HOST=${MARIADB_HOST:-"mariadb"}
 export MARIADB_PORT=${MARIADB_PORT:-"3306"}
-export SYMFONY_PROJECT_NAME=${SYMFONY_PROJECT_NAME:-"appasd_template"}
+export SYMFONY_PROJECT_NAME=${SYMFONY_PROJECT_NAME:-"app_template"}
 
 PROJECT_DIRECTORY=/app/$SYMFONY_PROJECT_NAME
+DEPLOY="$@"
+OLDHOME=$HOME
+HOME=/root
 
 log () {
     echo -e "\033[0;33m$(date "+%H:%M:%S")\033[0;37m ==> $1."
 }
 
-sudo -i harpoon restart mariadb
+sudo harpoon restart mariadb
 echo "Starting application ..."
 
 if [ "$1" == "php" -a "$2" == "-S" ] ; then
     if [ ! -d $PROJECT_DIRECTORY ] ; then
-      harpoon initialize symfony --inputs-file=/symfony-inputs.json
+      sudo harpoon initialize symfony --inputs-file=/symfony-inputs.json
       log "Creating example Symfony application"
       cd /app
-      harpoon execute symfony createProject $SYMFONY_PROJECT_NAME
+      sudo harpoon execute symfony createProject $SYMFONY_PROJECT_NAME
       log "Symfony app created"
       cd $SYMFONY_PROJECT_NAME
     else
@@ -29,10 +32,10 @@ if [ "$1" == "php" -a "$2" == "-S" ] ; then
       cd $PROJECT_DIRECTORY
     fi
   if [ ! -f $PROJECT_DIRECTORY/web/index.php ] ; then
-    ln -s $PROJECT_DIRECTORY/web/app.php $PROJECT_DIRECTORY/web/index.php
+    sudo ln -s $PROJECT_DIRECTORY/web/app.php $PROJECT_DIRECTORY/web/index.php
   fi
-  exec /entrypoint.sh "$@" -t $PROJECT_DIRECTORY/web
+  DEPLOY="$@ -t $PROJECT_DIRECTORY/web/"
 fi
 
-
-exec "$@"
+HOME=$OLDHOME
+exec /entrypoint.sh $DEPLOY
