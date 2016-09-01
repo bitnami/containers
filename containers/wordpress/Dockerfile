fd794a2b8d9fb185df886545cd60d20ef0f8ad6e
Jorge Marin <jorge@bitnami.com>
2016-09-02 00:54:02 +0200

    Add support for more environment variables, bump versions and improve several files (#25)
    * Add support for more environment variables and improve README.md
    
    * Add a new configurable env variable
    
    * Bump versions
    
    * Fix typos in README
    
    * Minor improvements to several files

--- containers/wordpress/Dockerfile
+++ containers/wordpress/Dockerfile
@@ -3,17 +3,17 @@ FROM gcr.io/stacksmith-images/ubuntu:14.04-r8
 MAINTAINER Bitnami <containers@bitnami.com>
 
 ENV BITNAMI_APP_NAME=wordpress \
-    BITNAMI_IMAGE_VERSION=4.6-r0 \
+    BITNAMI_IMAGE_VERSION=4.6-r1 \
     PATH=/opt/bitnami/php/bin:/opt/bitnami/mysql/bin/:$PATH
 
 # Additional modules required
+RUN bitnami-pkg unpack apache-2.4.23-2 --checksum 80605a5b1a3103cefc125cb150cc08a017702d0c1e3ea5f806540ad91c77cf38
 RUN bitnami-pkg install php-5.6.25-0 --checksum f0e8d07d155abdb5d6843931d3ffbf9b4208fff248c409444fdd5a8e3a3da01d
-RUN bitnami-pkg unpack apache-2.4.23-1 --checksum c8d14a79313c5e47dbf617e9a55e88ff91d8361357386bab520aabccd35c59d8
 RUN bitnami-pkg install libphp-5.6.21-2 --checksum 83d19b750b627fa70ed9613504089732897a48e1a7d304d8d73dec61a727b222
 RUN bitnami-pkg install mysql-client-10.1.13-4 --checksum 14b45c91dd78b37f0f2366712cbe9bfdf2cb674769435611955191a65dbf4976
 
 # Install wordpress
-RUN bitnami-pkg unpack wordpress-4.6-0 --checksum a2c4039e0dd22b1bed6947c2e651f11b472c9852de70db38cf66d5cea71da6bf
+RUN bitnami-pkg unpack wordpress-4.6-1 --checksum 8e809d1719bbef52b89dd909350702f463d2e0c8afc2bb46642f4b0552dc51a2
 
 COPY rootfs /
 
--- containers/wordpress/rootfs/app-entrypoint.sh
+++ containers/wordpress/rootfs/app-entrypoint.sh
@@ -5,6 +5,8 @@ function initialize {
     # Package can be "installed" or "unpacked"
     status=`harpoon inspect $1`
     if [[ "$status" == *'"lifecycle": "unpacked"'* ]]; then
+        # Clean up inputs
+        inputs=""
         if [[ -f /$1-inputs.json ]]; then
             inputs=--inputs-file=/$1-inputs.json
         fi
@@ -13,14 +15,21 @@ function initialize {
 }
 
 # Set default values
+export APACHE_HTTP_PORT=${APACHE_HTTP_PORT:-"80"}
+export APACHE_HTTPS_PORT=${APACHE_HTTPS_PORT:-"443"}
 export WORDPRESS_USERNAME=${WORDPRESS_USERNAME:-"user"}
 export WORDPRESS_PASSWORD=${WORDPRESS_PASSWORD:-"bitnami"}
 export WORDPRESS_EMAIL=${WORDPRESS_EMAIL:-"user@example.com"}
+export WORDPRESS_FIRST_NAME=${WORDPRESS_FIRST_NAME:-"FirstName"}
+export WORDPRESS_LAST_NAME=${WORDPRESS_LAST_NAME:-"LastName"}
+export WORDPRESS_BLOG_NAME=${WORDPRESS_BLOG_NAME:-"User's Blog!"}
 export MARIADB_USER=${MARIADB_USER:-"root"}
 export MARIADB_HOST=${MARIADB_HOST:-"mariadb"}
 export MARIADB_PORT=${MARIADB_PORT:-"3306"}
 
-if [[ "$1" == "harpoon" && "$2" == "start" ]]; then
+
+
+if [[ "$1" == "harpoon" && "$2" == "start" ]] ||  [[ "$1" == "/init.sh" ]]; then
    for module in apache wordpress; do
     initialize $module
    done
