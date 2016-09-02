4408fe65663999f88d3286feb8c66184d553b18c
Alejandro Polvillo <apolvillo@bitnami.com>
2016-09-02 17:08:30 +0200

    Initial release

---
+++ bitnami/parse/Dockerfile
@@ -0,0 +1,24 @@
+FROM gcr.io/stacksmith-images/ubuntu:14.04-r9
+
+MAINTAINER Bitnami <containers@bitnami.com>
+
+ENV BITNAMI_APP_NAME=parse \
+    BITNAMI_IMAGE_VERSION=2.2.18-r0 \
+    PATH=/opt/bitnami/node/bin:/opt/bitnami/mongodb/bin:/opt/bitnami/parse/bin:$PATH
+
+# Additional modules required
+RUN bitnami-pkg install node-4.5.0-0 --checksum af047acbfe0c0f918536de0dfa690178808d2e5fc07dfa7acc34c77f0a60fd55
+RUN bitnami-pkg install mongodb-client-3.2.7-4 --checksum 251b952828fa1dfaae4063914bba46ea8a1aaf9c0954850c00ee772000077f8d
+
+# Install parse
+RUN bitnami-pkg unpack parse-2.2.18-0 --checksum 03939b9b68d1555bea5e648db34dc5e45f6ea1dd25cc8a6b989a493cc9b294b9
+
+COPY rootfs /
+
+VOLUME ["/bitnami/parse"]
+
+EXPOSE 1337
+
+ENTRYPOINT ["/app-entrypoint.sh"]
+
+CMD ["harpoon", "start", "--foreground", "parse"]
---
+++ bitnami/parse/rootfs/app-entrypoint.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -e
+
+function initialize {
+    # Package can be "installed" or "unpacked"
+    status=`harpoon inspect $1`
+    if [[ "$status" == *'"lifecycle": "unpacked"'* ]]; then
+        # Clean up inputs
+        inputs=""
+        if [[ -f /$1-inputs.json ]]; then
+            inputs=--inputs-file=/$1-inputs.json
+        fi
+        harpoon initialize $1 $inputs
+    fi
+}
+
+# Set default values
+export PARSE_SERVER_PORT=${PARSE_SERVER_PORT:-"1337"}
+export PARSE_SERVER_HOST=${PARSE_SERVER_HOST:-"127.0.0.1"}
+export PARSE_SERVER_MOUNT_PATH=${PARSE_SERVER_MOUNT_PATH:-"/parse"}
+export PARSE_SERVER_APP_ID=${PARSE_SERVER_APP_ID:-"myappID"}
+export PARSE_SERVER_MASTER_KEY=${PARSE_SERVER_MASTER_KEY:-"mymasterKey"}
+export MONGODB_HOST=${MONGODB_HOST:-"mongodb"}
+export MONGODB_PORT=${MONGODB_PORT:-"27017"}
+
+
+
+if [[ "$1" == "harpoon" && "$2" == "start" ]] ||  [[ "$1" == "/init.sh" ]]; then
+   initialize parse
+   echo "Starting application ..."
+fi
+
+exec /entrypoint.sh "$@"
