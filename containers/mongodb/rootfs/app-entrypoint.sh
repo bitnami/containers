09356473bc0af55f0ae658dfccd5cfa589737d08
Sameer Naik <sameer@bitnami.com>
2016-09-03 14:13:16 +0530

    `harpoon` has been renamed to `nami`

--- containers/mongodb/rootfs/app-entrypoint.sh
+++ containers/mongodb/rootfs/app-entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -e
 
-if [[ "$1" == "harpoon" && "$2" == "start" ]]; then
-  status=`harpoon inspect $BITNAMI_APP_NAME`
+if [[ "$1" == "nami" && "$2" == "start" ]]; then
+  status=`nami inspect $BITNAMI_APP_NAME`
   if [[ "$status" == *'"lifecycle": "unpacked"'* ]]; then
-    harpoon initialize $BITNAMI_APP_NAME \
+    nami initialize $BITNAMI_APP_NAME \
       ${MONGODB_ROOT_PASSWORD:+--rootPassword $MONGODB_ROOT_PASSWORD} \
       ${MONGODB_USER:+--username $MONGODB_USER} \
       ${MONGODB_PASSWORD:+--password $MONGODB_PASSWORD} \
