f19c374ddfdf5218e62fe5a9f343afc1dc9a5c17
Sameer Naik <sameer@bitnami.com>
2016-09-03 13:19:14 +0530

    entrypoint: load the module inputs.json file

--- bitnami/apache/rootfs/app-entrypoint.sh
+++ bitnami/apache/rootfs/app-entrypoint.sh
@@ -1,12 +1,26 @@
-#!/bin/bash
-set -e
+#!/bin/bash -e
 
-if [[ "$1" == "nami" && "$2" == "start" ]]; then
-  status=`nami inspect $BITNAMI_APP_NAME`
-  if [[ "$status" == *'"lifecycle": "unpacked"'* ]]; then
-    nami initialize $BITNAMI_APP_NAME
-  fi
-  chown -R :$BITNAMI_APP_USER /bitnami/$BITNAMI_APP_NAME || true
+function initialize {
+    # Package can be "installed" or "unpacked"
+    status=`nami inspect $1`
+    if [[ "$status" == *'"lifecycle": "unpacked"'* ]]; then
+        # Clean up inputs
+        inputs=""
+        if [[ -f /$1-inputs.json ]]; then
+            inputs=--inputs-file=/$1-inputs.json
+        fi
+        nami initialize $1 $inputs
+    fi
+}
+
+# Set default values
+export APACHE_HTTP_PORT=${APACHE_HTTP_PORT:-80}
+export APACHE_HTTPS_PORT=${APACHE_HTTPS_PORT:-443}
+
+if [[ "$1" == "nami" && "$2" == "start" ]] ||  [[ "$1" == "/init.sh" ]]; then
+    initialize apache
+    chown -R :$BITNAMI_APP_USER /bitnami/apache || true
+    echo "Starting application ..."
 fi
 
 exec /entrypoint.sh "$@"
