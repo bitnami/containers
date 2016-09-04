2f19af2c605d0c80040bc87fc7005d496ea2466c
Sameer Naik <sameer@bitnami.com>
2016-09-04 21:07:08 +0530

    `harpoon` has been renamed to `nami`

--- containers/jenkins/rootfs/app-entrypoint.sh
+++ containers/jenkins/rootfs/app-entrypoint.sh
@@ -3,14 +3,14 @@ set -e
 
 function initialize {
     # Package can be "installed" or "unpacked"
-    status=`harpoon inspect $1`
+    status=`nami inspect $1`
     if [[ "$status" == *'"lifecycle": "unpacked"'* ]]; then
         # Clean up inputs
         inputs=""
         if [[ -f /$1-inputs.json ]]; then
             inputs=--inputs-file=/$1-inputs.json
         fi
-        harpoon initialize $1 $inputs
+        nami initialize $1 $inputs
     fi
 }
 
@@ -20,7 +20,7 @@ export JENKINS_PASSWORD=${JENKINS_PASSWORD:-"bitnami"}
 
 
 
-if [[ "$1" == "harpoon" && "$2" == "start" ]] ||  [[ "$1" == "/init.sh" ]]; then
+if [[ "$1" == "nami" && "$2" == "start" ]] ||  [[ "$1" == "/init.sh" ]]; then
    initialize jenkins
    echo "Starting application ..."
 fi
