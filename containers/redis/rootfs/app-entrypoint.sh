97e383eea685d0dbbdb237b55f32ffec0afdf147
Sameer Naik <sameer@bitnami.com>
2016-09-04 20:31:22 +0530

    `harpoon` has been renamed to `nami`

--- containers/redis/rootfs/app-entrypoint.sh
+++ containers/redis/rootfs/app-entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -e
 
-if [[ "$1" == "harpoon" && "$2" == "start" ]]; then
-  status=`harpoon inspect $BITNAMI_APP_NAME`
+if [[ "$1" == "nami" && "$2" == "start" ]]; then
+  status=`nami inspect $BITNAMI_APP_NAME`
   if [[ "$status" == *'"lifecycle": "unpacked"'* ]]; then
-    harpoon initialize $BITNAMI_APP_NAME \
+    nami initialize $BITNAMI_APP_NAME \
       ${REDIS_PASSWORD:+--password $REDIS_PASSWORD} \
       ${REDIS_REPLICATION_MODE:+--replicationMode $REDIS_REPLICATION_MODE} \
       ${REDIS_MASTER_HOST:+--masterHost $REDIS_MASTER_HOST} \
