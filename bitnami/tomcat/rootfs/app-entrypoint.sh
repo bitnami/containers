00824e020037b647c4ca2c2247188cb84addcb9a
Sameer Naik <sameer@bitnami.com>
2016-09-04 23:25:11 +0530

    `harpoon` has been renamed to `nami`

--- bitnami/tomcat/rootfs/app-entrypoint.sh
+++ bitnami/tomcat/rootfs/app-entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -e
 
-if [[ "$1" == "harpoon" && "$2" == "start" ]]; then
-  status=`harpoon inspect $BITNAMI_APP_NAME`
+if [[ "$1" == "nami" && "$2" == "start" ]]; then
+  status=`nami inspect $BITNAMI_APP_NAME`
   if [[ "$status" == *'"lifecycle": "unpacked"'* ]]; then
-    harpoon initialize $BITNAMI_APP_NAME \
+    nami initialize $BITNAMI_APP_NAME \
       ${TOMCAT_USER:+--username $TOMCAT_USER} \
       ${TOMCAT_PASSWORD:+--password $TOMCAT_PASSWORD}
   fi
