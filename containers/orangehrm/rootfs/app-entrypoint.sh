a0942fec18d643771a5ff59df571d076faef68a9
Carlos Rodríguez <carrodher1179@gmail.com>
2016-11-08 13:15:19 +0100

    Requires initialization of Apache

--- containers/orangehrm/rootfs/app-entrypoint.sh
+++ containers/orangehrm/rootfs/app-entrypoint.sh
@@ -15,6 +15,8 @@ function initialize {
 }
 
 # Set default values
+export APACHE_HTTP_PORT=${APACHE_HTTP_PORT:-"80"}
+export APACHE_HTTPS_PORT=${APACHE_HTTPS_PORT:-"443"}
 export ORANGEHRM_USERNAME=${ORANGEHRM_USERNAME:-"admin"}
 export ORANGEHRM_PASSWORD=${ORANGEHRM_PASSWORD:-"bitnami"}
 export SMTP_PROTOCOL=${SMTP_PROTOCOL:-"none"}
@@ -22,10 +24,10 @@ export MARIADB_USER=${MARIADB_USER:-"root"}
 export MARIADB_HOST=${MARIADB_HOST:-"mariadb"}
 export MARIADB_PORT=${MARIADB_PORT:-"3306"}
 
-
-
 if [[ "$1" == "nami" && "$2" == "start" ]] ||  [[ "$1" == "/init.sh" ]]; then
-   initialize orangehrm
+   for module in apache orangehrm; do
+    initialize $module
+   done
    echo "Starting application ..."
 fi
 
