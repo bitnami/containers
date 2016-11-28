f53bbdd7146b6267c903bfd8536dfcd9a4a0de0c
jdrios <jdrios@bitnami.com>
2016-11-28 10:05:50 +0100

    Change nodeName default value to fix a bug when restoring a previous deployment.

--- containers/rabbitmq/Dockerfile
+++ containers/rabbitmq/Dockerfile
@@ -3,7 +3,7 @@ FROM gcr.io/stacksmith-images/minideb:jessie-r2
 MAINTAINER Bitnami <containers@bitnami.com>
 
 ENV BITNAMI_APP_NAME=rabbitmq \
-    BITNAMI_IMAGE_VERSION=3.6.6-r0 \
+    BITNAMI_IMAGE_VERSION=3.6.6-r1 \
     PATH=/opt/bitnami/erlang/bin:/opt/bitnami/rabbitmq/bin:$PATH
 
 # Additional modules required
--- containers/rabbitmq/rootfs/app-entrypoint.sh
+++ containers/rabbitmq/rootfs/app-entrypoint.sh
@@ -20,7 +20,7 @@ export RABBITMQ_USERNAME=${RABBITMQ_USERNAME:-"user"}
 export RABBITMQ_PASSWORD=${RABBITMQ_PASSWORD:-"bitnami"}
 export RABBITMQ_NODE_TYPE=${RABBITMQ_NODE_TYPE:-"stats"}
 export RABBITMQ_NODE_PORT=${RABBITMQ_NODE_PORT:-"5672"}
-export RABBITMQ_NODE_NAME=${RABBITMQ_NODE_NAME:-"rabbit"}
+export RABBITMQ_NODE_NAME=${RABBITMQ_NODE_NAME:-"rabbit@localhost"}
 export RABBITMQ_MANAGER_PORT=${RABBITMQ_MANAGER_PORT:-"15672"}
 
 if [[ "$1" == "nami" && "$2" == "start" ]] ||  [[ "$1" == "/init.sh" ]]; then
