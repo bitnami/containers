a7bd6f8735d39d622f80e8da0b149ba57c0c553b
Sameer Naik <sameer@bitnami.com>
2017-02-13 18:12:29 +0530

    config: move default config var to Dockerfile

--- containers/osclass/rootfs/app-entrypoint.sh
+++ containers/osclass/rootfs/app-entrypoint.sh
@@ -14,19 +14,6 @@ function initialize {
     fi
 }
 
-# Set default values
-export APACHE_HTTP_PORT=${APACHE_HTTP_PORT:-"80"}
-export APACHE_HTTPS_PORT=${APACHE_HTTPS_PORT:-"443"}
-export OSCLASS_PING_ENGINES=${OSCLASS_PING_ENGINES:-"1"}
-export OSCLASS_SAVE_STATS=${OSCLASS_SAVE_STATS:-"1"}
-export OSCLASS_USERNAME=${OSCLASS_USERNAME:-"user"}
-export OSCLASS_PASSWORD=${OSCLASS_PASSWORD:-"bitnami1"}
-export OSCLASS_WEB_TITLE=${OSCLASS_WEB_TITLE:-"Sample Web Page"}
-export OSCLASS_EMAIL=${OSCLASS_EMAIL:-"user@example.com"}
-export MARIADB_USER=${MARIADB_USER:-"root"}
-export MARIADB_HOST=${MARIADB_HOST:-"mariadb"}
-export MARIADB_PORT=${MARIADB_PORT:-"3306"}
-
 
 
 if [[ "$1" == "nami" && "$2" == "start" ]] ||  [[ "$1" == "/init.sh" ]]; then
