bb4a12fb0422a94e39643553a50140420099b2cd
Sameer Naik <sameer@bitnami.com>
2017-02-13 17:57:58 +0530

    config: move default config var to Dockerfile

--- bitnami/mediawiki/rootfs/app-entrypoint.sh
+++ bitnami/mediawiki/rootfs/app-entrypoint.sh
@@ -14,17 +14,6 @@ function initialize {
     fi
 }
 
-# Set default values
-export APACHE_HTTP_PORT=${APACHE_HTTP_PORT:-"80"}
-export APACHE_HTTPS_PORT=${APACHE_HTTPS_PORT:-"443"}
-export MEDIAWIKI_USERNAME=${MEDIAWIKI_USERNAME:-"user"}
-export MEDIAWIKI_PASSWORD=${MEDIAWIKI_PASSWORD:-"bitnami1"}
-export MEDIAWIKI_EMAIL=${MEDIAWIKI_EMAIL:-"user@example.com"}
-export MEDIAWIKI_WIKI_NAME=${MEDIAWIKI_WIKI_NAME:-"Bitnami MediaWiki"}
-export MARIADB_USER=${MARIADB_USER:-"root"}
-export MARIADB_HOST=${MARIADB_HOST:-"mariadb"}
-export MARIADB_PORT=${MARIADB_PORT:-"3306"}
-
 
 
 if [[ "$1" == "nami" && "$2" == "start" ]] ||  [[ "$1" == "/init.sh" ]]; then
