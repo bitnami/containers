1dcf7f85596b8f85533463eca78f199c631b3e51
Sameer Naik <sameer@bitnami.com>
2017-02-13 18:13:08 +0530

    config: move default config var to Dockerfile

--- bitnami/owncloud/rootfs/app-entrypoint.sh
+++ bitnami/owncloud/rootfs/app-entrypoint.sh
@@ -14,16 +14,6 @@ function initialize {
     fi
 }
 
-# Set default values
-export APACHE_HTTP_PORT=${APACHE_HTTP_PORT:-"80"}
-export APACHE_HTTPS_PORT=${APACHE_HTTPS_PORT:-"443"}
-export OWNCLOUD_USERNAME=${OWNCLOUD_USERNAME:-"user"}
-export OWNCLOUD_PASSWORD=${OWNCLOUD_PASSWORD:-"bitnami"}
-export OWNCLOUD_EMAIL=${OWNCLOUD_EMAIL:-"user@example.com"}
-export MARIADB_USER=${MARIADB_USER:-"root"}
-export MARIADB_HOST=${MARIADB_HOST:-"mariadb"}
-export MARIADB_PORT=${MARIADB_PORT:-"3306"}
-
 if [[ "$1" == "nami" && "$2" == "start" ]] ||  [[ "$1" == "/init.sh" ]]; then
    for module in apache php owncloud; do
     initialize $module
