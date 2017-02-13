f4bd28aabafef484b5c98e33af0ff9cc30baa12c
Sameer Naik <sameer@bitnami.com>
2017-02-13 18:03:04 +0530

    config: move default config var to Dockerfile

--- bitnami/mysql/rootfs/app-entrypoint.sh
+++ bitnami/mysql/rootfs/app-entrypoint.sh
@@ -13,20 +13,6 @@ function initialize {
     fi
 }
 
-# Set default values
-export MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD:-}
-export MYSQL_USER=${MYSQL_USER:-}
-export MYSQL_PASSWORD=${MYSQL_PASSWORD:-}
-export MYSQL_DATABASE=${MYSQL_DATABASE:-}
-export MYSQL_PORT=${MYSQL_PORT:-}
-export MYSQL_REPLICATION_MODE=${MYSQL_REPLICATION_MODE:-}
-export MYSQL_REPLICATION_USER=${MYSQL_REPLICATION_USER:-}
-export MYSQL_REPLICATION_PASSWORD=${MYSQL_REPLICATION_PASSWORD:-}
-export MYSQL_MASTER_HOST=${MYSQL_MASTER_HOST:-}
-export MYSQL_MASTER_PORT=${MYSQL_MASTER_PORT:-3306}
-export MYSQL_MASTER_USER=${MYSQL_MASTER_USER:-root}
-export MYSQL_MASTER_PASSWORD=${MYSQL_MASTER_PASSWORD:-}
-
 if [[ "$1" == "nami" && "$2" == "start" ]] ||  [[ "$1" == "/init.sh" ]]; then
     initialize mysql
     echo "Starting application ..."
