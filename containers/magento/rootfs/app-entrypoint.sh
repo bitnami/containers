6e72e861de4c653cba56b054ede154d7a13105d2
Sameer Naik <sameer@bitnami.com>
2017-02-13 17:50:41 +0530

    config: move default config var to Dockerfile

--- containers/magento/rootfs/app-entrypoint.sh
+++ containers/magento/rootfs/app-entrypoint.sh
@@ -14,20 +14,6 @@ function initialize {
     fi
 }
 
-# Set default values
-export APACHE_HTTP_PORT=${APACHE_HTTP_PORT:-"80"}
-export APACHE_HTTPS_PORT=${APACHE_HTTPS_PORT:-"443"}
-export MAGENTO_USERNAME=${MAGENTO_USERNAME:-"user"}
-export MAGENTO_PASSWORD=${MAGENTO_PASSWORD:-"bitnami1"}
-export MAGENTO_EMAIL=${MAGENTO_EMAIL:-"user@example.com"}
-export MAGENTO_ADMINURI=${MAGENTO_ADMINURI:-"admin"}
-export MAGENTO_FIRSTNAME=${MAGENTO_FIRSTNAME:-"FirstName"}
-export MAGENTO_LASTNAME=${MAGENTO_LASTNAME:-"LastName"}
-export MAGENTO_MODE=${MAGENTO_MODE:-"developer"}
-export MARIADB_USER=${MARIADB_USER:-"root"}
-export MARIADB_HOST=${MARIADB_HOST:-"mariadb"}
-export MARIADB_PORT=${MARIADB_PORT:-"3306"}
-
 # Adding cron entries
 ln -fs /opt/bitnami/magento/conf/cron /etc/cron.d/magento
 
