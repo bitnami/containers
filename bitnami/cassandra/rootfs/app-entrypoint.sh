9c8ec2bb5ac316ac2eed74fe70e7bec77b1e2b02
Sameer Naik <sameer@bitnami.com>
2017-02-13 17:13:16 +0530

    config: move default config var to Dockerfile

--- bitnami/cassandra/rootfs/app-entrypoint.sh
+++ bitnami/cassandra/rootfs/app-entrypoint.sh
@@ -14,17 +14,6 @@ function initialize {
     fi
 }
 
-# Set default values
-export CASSANDRA_CLUSTER_NAME=${CASSANDRA_CLUSTER_NAME:-"My Cluster"}
-export CASSANDRA_TRANSPORT_PORT=${CASSANDRA_TRANSPORT_PORT:-"7000"}
-export CASSANDRA_SSL_TRANSPORT_PORT=${CASSANDRA_SSL_TRANSPORT_PORT:-"7001"}
-export CASSANDRA_JMX_PORT=${CASSANDRA_JMX_PORT:-"7199"}
-export CASSANDRA_CQL_PORT=${CASSANDRA_CQL_PORT:-"9042"}
-export CASSANDRA_RPC_PORT=${CASSANDRA_RPC_PORT:-"9160"}
-export CASSANDRA_USER=${CASSANDRA_USER:-"cassandra"}
-export CASSANDRA_PASSWORD=${CASSANDRA_PASSWORD:-"cassandra"}
-export CASSANDRA_ENDPOINT_SNITCH=${CASSANDRA_ENDPOINT_SNITCH:-"SimpleSnitch"}
-
 if [[ "$1" == "nami" && "$2" == "start" ]] ||  [[ "$1" == "/init.sh" ]]; then
    initialize cassandra
    echo "Starting application ..."
