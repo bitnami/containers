1e48c25f2cd4f4ae6551e74886e4dbebf8f4e5e6
Sameer Naik <sameer@bitnami.com>
2017-02-13 17:46:56 +0530

    config: move default config var to Dockerfile

--- bitnami/jasperreports/rootfs/app-entrypoint.sh
+++ bitnami/jasperreports/rootfs/app-entrypoint.sh
@@ -14,14 +14,6 @@ function initialize {
     fi
 }
 
-# Set default values
-export JASPERREPORTS_USERNAME=${JASPERREPORTS_USERNAME:-"user"}
-export JASPERREPORTS_PASSWORD=${JASPERREPORTS_PASSWORD:-"bitnami"}
-export JASPERREPORTS_EMAIL=${JASPERREPORTS_EMAIL:-"user@example.com"}
-export MARIADB_USER=${MARIADB_USER:-"root"}
-export MARIADB_HOST=${MARIADB_HOST:-"mariadb"}
-export MARIADB_PORT=${MARIADB_PORT:-"3306"}
-
 
 
 if [[ "$1" == "nami" && "$2" == "start" ]] ||  [[ "$1" == "/init.sh" ]]; then
