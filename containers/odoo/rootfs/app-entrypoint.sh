66fdde43f5ae8eb0cfda5a870723a69d3d1b9701
Sameer Naik <sameer@bitnami.com>
2017-02-13 18:10:43 +0530

    config: move default config var to Dockerfile

--- containers/odoo/rootfs/app-entrypoint.sh
+++ containers/odoo/rootfs/app-entrypoint.sh
@@ -14,13 +14,6 @@ function initialize {
     fi
 }
 
-# Set default values
-export ODOO_PASSWORD=${ODOO_PASSWORD:-"bitnami"}
-export ODOO_EMAIL=${ODOO_EMAIL:-"user@example.com"}
-export POSTGRESQL_USER=${POSTGRESQL_USER:-"postgres"}
-export POSTGRESQL_HOST=${POSTGRESQL_HOST:-"postgresql"}
-export POSTGRESQL_PORT=${POSTGRESQL_PORT:-"5432"}
-
 
 
 if [[ "$1" == "nami" && "$2" == "start" ]] ||  [[ "$1" == "/init.sh" ]]; then
