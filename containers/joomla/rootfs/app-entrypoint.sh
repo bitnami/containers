c0e1ee662d2a7c1a1c94fdca795ec35d5db37a91
Sameer Naik <sameer@bitnami.com>
2017-02-13 17:48:37 +0530

    config: move default config var to Dockerfile

--- containers/joomla/rootfs/app-entrypoint.sh
+++ containers/joomla/rootfs/app-entrypoint.sh
@@ -13,18 +13,6 @@ function initialize {
     fi
 }
 
-# Set default values
-export APACHE_HTTP_PORT=${APACHE_HTTP_PORT:-"80"}
-export APACHE_HTTPS_PORT=${APACHE_HTTPS_PORT:-"443"}
-export JOOMLA_FIRST_NAME=${JOOMLA_FIRST_NAME:-"User"}
-export JOOMLA_LAST_NAME=${JOOMLA_LAST_NAME:-"Name"}
-export JOOMLA_USERNAME=${JOOMLA_USERNAME:-"user"}
-export JOOMLA_PASSWORD=${JOOMLA_PASSWORD:-"bitnami"}
-export JOOMLA_EMAIL=${JOOMLA_EMAIL:-"user@example.com"}
-export MARIADB_USER=${MARIADB_USER:-"root"}
-export MARIADB_HOST=${MARIADB_HOST:-"mariadb"}
-export MARIADB_PORT=${MARIADB_PORT:-"3306"}
-
 if [[ "$1" == "nami" && "$2" == "start" ]] ||  [[ "$1" == "/init.sh" ]]; then
    for module in apache php joomla; do
     initialize $module
