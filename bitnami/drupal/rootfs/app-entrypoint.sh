b0bab92ef91ba67a19d9280434c73e73db85b3c0
Sameer Naik <sameer@bitnami.com>
2017-02-13 17:40:45 +0530

    config: move default config var to Dockerfile

--- bitnami/drupal/rootfs/app-entrypoint.sh
+++ bitnami/drupal/rootfs/app-entrypoint.sh
@@ -13,16 +13,6 @@ function initialize {
     fi
 }
 
-# Set default values
-export APACHE_HTTP_PORT=${APACHE_HTTP_PORT:-"80"}
-export APACHE_HTTPS_PORT=${APACHE_HTTPS_PORT:-"443"}
-export DRUPAL_USERNAME=${DRUPAL_USERNAME:-"user"}
-export DRUPAL_PASSWORD=${DRUPAL_PASSWORD:-"bitnami"}
-export DRUPAL_EMAIL=${DRUPAL_EMAIL:-"user@example.com"}
-export MARIADB_USER=${MARIADB_USER:-"root"}
-export MARIADB_HOST=${MARIADB_HOST:-"mariadb"}
-export MARIADB_PORT=${MARIADB_PORT:-"3306"}
-
 if [[ "$1" == "nami" && "$2" == "start" ]] ||  [[ "$1" == "/init.sh" ]]; then
    for module in apache php drupal; do
     initialize $module
