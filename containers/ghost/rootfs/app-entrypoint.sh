a17bca7335fa4d72de489664021e8f38f3bb8057
Sameer Naik <sameer@bitnami.com>
2017-02-13 17:46:18 +0530

    config: move default config var to Dockerfile

--- containers/ghost/rootfs/app-entrypoint.sh
+++ containers/ghost/rootfs/app-entrypoint.sh
@@ -13,16 +13,6 @@ function initialize {
     fi
 }
 
-# Set default values
-export GHOST_PORT=${GHOST_PORT:-"80"}
-export GHOST_USERNAME=${GHOST_USERNAME:-"user"}
-export GHOST_PASSWORD=${GHOST_PASSWORD:-"bitnami1"}
-export GHOST_EMAIL=${GHOST_EMAIL:-"user@example.com"}
-export BLOG_TITLE=${BLOG_TITLE:-"User's Blog"}
-export MARIADB_USER=${MARIADB_USER:-"root"}
-export MARIADB_HOST=${MARIADB_HOST:-"mariadb"}
-export MARIADB_PORT=${MARIADB_PORT:-"3306"}
-
 
 if [[ "$1" == "nami" && "$2" == "start" ]] ||  [[ "$1" == "/init.sh" ]]; then
    initialize ghost
