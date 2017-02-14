845d6580016bcf6e63ed2e65d417d8b63c3a60f2
Sameer Naik <sameer@bitnami.com>
2017-02-14 15:42:10 +0530

    config: move default config var to Dockerfile

--- bitnami/suitecrm/rootfs/app-entrypoint.sh
+++ bitnami/suitecrm/rootfs/app-entrypoint.sh
@@ -14,17 +14,6 @@ function initialize {
     fi
 }
 
-# Set default values
-export APACHE_HTTP_PORT=${APACHE_HTTP_PORT:-"80"}
-export APACHE_HTTPS_PORT=${APACHE_HTTPS_PORT:-"443"}
-export SUITECRM_USERNAME=${SUITECRM_USERNAME:-"user"}
-export SUITECRM_LAST_NAME=${SUITECRM_LAST_NAME:-"Name"}
-export SUITECRM_PASSWORD=${SUITECRM_PASSWORD:-"bitnami"}
-export SUITECRM_EMAIL=${SUITECRM_EMAIL:-"user@example.com"}
-export MARIADB_USER=${MARIADB_USER:-"root"}
-export MARIADB_HOST=${MARIADB_HOST:-"mariadb"}
-export MARIADB_PORT=${MARIADB_PORT:-"3306"}
-
 # Adding cron entries
 ln -fs /opt/bitnami/suitecrm/conf/cron /etc/cron.d/suitecrm
 
