b912ccb268c7e412c6746dac9f25f8142257b524
Sameer Naik <sameer@bitnami.com>
2017-02-14 15:31:54 +0530

    config: move default config var to Dockerfile

--- containers/postgresql/rootfs/app-entrypoint.sh
+++ containers/postgresql/rootfs/app-entrypoint.sh
@@ -13,16 +13,6 @@ function initialize {
     fi
 }
 
-# Set default values
-export POSTGRESQL_USERNAME=${POSTGRESQL_USERNAME:-postgres}
-export POSTGRESQL_PASSWORD=${POSTGRESQL_PASSWORD:-}
-export POSTGRESQL_DATABASE=${POSTGRESQL_DATABASE:-}
-export POSTGRESQL_REPLICATION_MODE=${POSTGRESQL_REPLICATION_MODE:-master}
-export POSTGRESQL_REPLICATION_USER=${POSTGRESQL_REPLICATION_USER:-}
-export POSTGRESQL_REPLICATION_PASSWORD=${POSTGRESQL_REPLICATION_PASSWORD:-}
-export POSTGRESQL_MASTER_HOST=${POSTGRESQL_MASTER_HOST:-}
-export POSTGRESQL_MASTER_PORT=${POSTGRESQL_MASTER_PORT:-5432}
-
 if [[ "$1" == "nami" && "$2" == "start" ]] ||  [[ "$1" == "/init.sh" ]]; then
     initialize postgresql
     echo "Starting application ..."
