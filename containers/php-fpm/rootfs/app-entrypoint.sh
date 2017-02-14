7c52ad1037f8791dfddfbe95c9689a898ff1172e
Sameer Naik <sameer@bitnami.com>
2017-02-14 15:13:44 +0530

    entrypoint: remove references to `entrypoint.sh` from base image

--- containers/php-fpm/rootfs/app-entrypoint.sh
+++ containers/php-fpm/rootfs/app-entrypoint.sh
@@ -1,26 +1,18 @@
 #!/bin/bash -e
+. /opt/bitnami/base/functions
+. /opt/bitnami/base/helpers
 
-function initialize {
-    # Package can be "installed" or "unpacked"
-    status=`nami inspect $1`
-    if [[ "$status" == *'"lifecycle": "unpacked"'* ]]; then
-        # Clean up inputs
-        inputs=""
-        if [[ -f /$1-inputs.json ]]; then
-            inputs=--inputs-file=/$1-inputs.json
-        fi
-        nami initialize $1 $inputs
-    fi
-}
+print_welcome_page
+check_for_updates &
 
 if [ -f composer.json  ]; then
   composer install
 fi
 
-if [[ "$1" == "nami" && "$2" == "start" ]] ||  [[ "$1" == "/init.sh" ]]; then
-    initialize php
-    chown -R :$BITNAMI_APP_USER /bitnami/php || true
-    echo "Starting application ..."
+if [[ "$1" == "nami" && "$2" == "start" ]] || [[ "$1" == "/init.sh" ]]; then
+  nami_initialize php
+  chown -R :daemon /bitnami/php || true
+  info "Starting php-fpm..."
 fi
 
-exec /entrypoint.sh "$@"
+exec tini -- "$@"
