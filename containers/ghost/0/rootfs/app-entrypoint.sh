9f9e99b26005740a2f4704ce6d288615d8827c56
Sameer Naik <sameer@bitnami.com>
2017-03-08 20:12:45 +0530

    launch ghost using PM2

--- containers/ghost/0/rootfs/app-entrypoint.sh
+++ containers/ghost/0/rootfs/app-entrypoint.sh
@@ -5,7 +5,7 @@
 print_welcome_page
 check_for_updates &
 
-if [[ "$1" == "nami" && "$2" == "start" ]] || [[ "$(basename $1)" == "node" ]] ||  [[ "$1" == "/init.sh" ]]; then
+if [[ "$1" == "nami" && "$2" == "start" ]] || [[ "$(basename $1)" == "pm2" ]] ||  [[ "$1" == "/init.sh" ]]; then
   nami_initialize ghost
 fi
 
