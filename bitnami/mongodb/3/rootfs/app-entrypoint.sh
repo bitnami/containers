615cc9f3171b7776eb59f9436881442c93547069
Sameer Naik <sameer@bitnami.com>
2017-03-09 15:29:44 +0530

    drop `nami start mongodb` CMD from Dockerfile

--- bitnami/mongodb/3/rootfs/app-entrypoint.sh
+++ bitnami/mongodb/3/rootfs/app-entrypoint.sh
@@ -5,9 +5,12 @@
 print_welcome_page
 check_for_updates &
 
-if [[ "$1" == "nami" && "$2" == "start" ]] || [[ "$1" == "/init.sh" ]]; then
+if [[ "$1" == "nami" && "$2" == "start" ]] || [[ "$(basename $1)" == "mongod" ]] || [[ "$1" == "/init.sh" ]]; then
   nami_initialize mongodb
-  info "Starting mongodb..."
+
+  if [[ "$(basename $1)" == "mongod" ]]; then
+    set -- "$@" --config /opt/bitnami/mongodb/conf/mongodb.conf
+  fi
 fi
 
 exec tini -- "$@"
