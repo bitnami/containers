9246fd8be211b0d49357fe89d5288181a0feb3d2
Sameer Naik <sameer@bitnami.com>
2017-03-09 17:00:11 +0530

    adds `run-nginx.sh` script

--- containers/nginx/1/rootfs/app-entrypoint.sh
+++ containers/nginx/1/rootfs/app-entrypoint.sh
@@ -5,18 +5,8 @@
 print_welcome_page
 check_for_updates &
 
-if [[ "$1" == "nami" && "$2" == "start" ]] || [[ "$(basename $1)" == "nginx" ]] || [[ "$1" == "/init.sh" ]]; then
+if [[ "$1" == "nami" && "$2" == "start" ]] || [[ "$1" == "/run-nginx.sh" ]]; then
   nami_initialize nginx
-
-  # redirect nginx logs to stdout/stderr
-  ln -sf /dev/stdout /opt/bitnami/nginx/logs/access.log
-  ln -sf /dev/stderr /opt/bitnami/nginx/logs/error.log
-
-  # ensure nginx is not daemonized
-  if [[ "$(basename $1)" == "nginx" ]]; then
-    set -- "$@" -g 'daemon off;'
-  fi
-
   chown -R :daemon /bitnami/nginx || true
 fi
 
