ccfeab0957f49eb7c064345724f63bdab8c9feb7
Sameer Naik <sameer@bitnami.com>
2017-03-09 14:44:08 +0530

    redirect apache logs to stdout/stderr

--- bitnami/drupal/8/rootfs/app-entrypoint.sh
+++ bitnami/drupal/8/rootfs/app-entrypoint.sh
@@ -12,6 +12,10 @@ if [[ "$1" == "nami" && "$2" == "start" ]] || [[ "$(basename $1)" == "httpd" ]] 
     # drupal initialization leaves a running httpd instance
     apachectl stop
 
+    # redirect apache logs to stdout/stderr
+    ln -sf /dev/stdout /opt/bitnami/apache/logs/access_log
+    ln -sf /dev/stdout /opt/bitnami/apache/logs/error_log
+
     set -- "$@" -f /opt/bitnami/apache/conf/httpd.conf -D FOREGROUND
   fi
 fi
