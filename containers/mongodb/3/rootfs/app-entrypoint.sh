ebfdfb2cba305b4375553571e7ce2e2255c9e10c
Sameer Naik <sameer@bitnami.com>
2017-03-09 16:36:01 +0530

    adds `run-mongodb.sh` script

--- containers/mongodb/3/rootfs/app-entrypoint.sh
+++ containers/mongodb/3/rootfs/app-entrypoint.sh
@@ -5,15 +5,8 @@
 print_welcome_page
 check_for_updates &
 
-if [[ "$1" == "nami" && "$2" == "start" ]] || [[ "$(basename $1)" == "mongod" ]] || [[ "$1" == "/init.sh" ]]; then
+if [[ "$1" == "nami" && "$2" == "start" ]] || [[ "$1" == "/run-mongodb.sh" ]]; then
   nami_initialize mongodb
-
-  # log output to stdout
-  sed -i 's/path: .*\/mongodb.log/path: /' /bitnami/mongodb/conf/mongodb.conf
-
-  if [[ "$(basename $1)" == "mongod" ]]; then
-    set -- gosu mongo "$@" --config /opt/bitnami/mongodb/conf/mongodb.conf
-  fi
 fi
 
 exec tini -- "$@"
