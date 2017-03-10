9e4eaa357a3fddfd5d583f9333964a0b78d9788d
Sameer Naik <sameer@bitnami.com>
2017-03-10 12:40:49 +0530

    run-ghost.sh: define production environment
    fixes broken links issue

--- bitnami/ghost/0/rootfs/run-ghost.sh
+++ bitnami/ghost/0/rootfs/run-ghost.sh
@@ -2,5 +2,7 @@
 . /opt/bitnami/base/functions
 . /opt/bitnami/base/helpers
 
+export NODE_ENV=production
+
 info "Starting ghost..."
 exec /opt/bitnami/ghost/node_modules/pm2/bin/pm2 start --no-daemon index.js
