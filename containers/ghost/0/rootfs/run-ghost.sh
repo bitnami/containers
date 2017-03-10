f1957e37e423533a4532540674e06cabab11e767
Sameer Naik <sameer@bitnami.com>
2017-03-10 20:54:30 +0530

    launch application server using `gosu`

--- containers/ghost/0/rootfs/run-ghost.sh
+++ containers/ghost/0/rootfs/run-ghost.sh
@@ -2,7 +2,9 @@
 . /opt/bitnami/base/functions
 . /opt/bitnami/base/helpers
 
+USER=ghost
+
 export NODE_ENV=production
 
 info "Starting ghost..."
-exec /opt/bitnami/ghost/node_modules/pm2/bin/pm2 start --no-daemon /opt/bitnami/ghost/index.js
+exec gosu ${USER} /opt/bitnami/ghost/node_modules/pm2/bin/pm2 start --no-daemon /opt/bitnami/ghost/index.js
