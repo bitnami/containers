0727bda5b0ed8d07c64cde9e95d15932081d425e
Sameer Naik <sameer@bitnami.com>
2017-03-10 12:41:34 +0530

    run-ghost.sh: specify absolute path to `index.js`

--- containers/ghost/0/rootfs/run-ghost.sh
+++ containers/ghost/0/rootfs/run-ghost.sh
@@ -5,4 +5,4 @@
 export NODE_ENV=production
 
 info "Starting ghost..."
-exec /opt/bitnami/ghost/node_modules/pm2/bin/pm2 start --no-daemon index.js
+exec /opt/bitnami/ghost/node_modules/pm2/bin/pm2 start --no-daemon /opt/bitnami/ghost/index.js
