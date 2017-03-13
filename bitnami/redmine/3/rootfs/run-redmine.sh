78ae0c3838cb3a16638e2cadbe265fb2f6da4693
Sameer Naik <sameer@bitnami.com>
2017-03-13 11:09:39 +0530

    launch passenger app server as `redmine` user

--- bitnami/redmine/3/rootfs/run-redmine.sh
+++ bitnami/redmine/3/rootfs/run-redmine.sh
@@ -2,7 +2,15 @@
 . /opt/bitnami/base/functions
 . /opt/bitnami/base/helpers
 
-info "Starting redmine..."
+USER=redmine
+RAILS_ENV=production
+PID_FILE=/opt/bitnami/redmine/tmp/pids/redmine.pid
+LOG_FILE=/opt/bitnami/redmine/logs/production.log
 
+mkdir -p /opt/bitnami/redmine/tmp/pids
+chown ${USER}: -R /opt/bitnami/redmine/tmp
+chmod -R 1777 /opt/bitnami/redmine/tmp
+
+info "Starting redmine..."
 cd /opt/bitnami/redmine
-exec bundle exec passenger start -e production --log-file /opt/bitnami/redmine/logs/production.log -p 3000
+exec gosu ${USER} bundle exec passenger start -e ${RAILS_ENV} --pid-file ${PID_FILE} --log-file ${LOG_FILE} -p 3000
