6a39859486f14bef1a48c5f1240a06ae0429d324
Sameer Naik <sameer@bitnami.com>
2017-03-20 16:50:38 +0100

    adds `--merge-logs` to `pm2` launch command

--- containers/ghost/0/rootfs/run.sh
+++ containers/ghost/0/rootfs/run.sh
@@ -7,4 +7,4 @@ USER=ghost
 export NODE_ENV=production
 
 info "Starting ghost..."
-exec gosu ${USER} /opt/bitnami/ghost/node_modules/pm2/bin/pm2 start --no-daemon /opt/bitnami/ghost/index.js
+exec gosu ${USER} /opt/bitnami/ghost/node_modules/pm2/bin/pm2 start --no-daemon --merge-logs /opt/bitnami/ghost/index.js
