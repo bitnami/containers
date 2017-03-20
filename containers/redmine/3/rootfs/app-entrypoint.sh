23cd4d3445be2b498b72d3a7854ac7311c70bbee
Sameer Naik <sameer@bitnami.com>
2017-03-20 12:58:57 +0100

    redmine: renamed `run-redmine.sh` to `run.sh`

--- containers/redmine/3/rootfs/app-entrypoint.sh
+++ containers/redmine/3/rootfs/app-entrypoint.sh
@@ -5,7 +5,7 @@
 print_welcome_page
 check_for_updates &
 
-if [[ "$1" == "nami" && "$2" == "start" ]] || [[ "$1" == "/run-redmine.sh" ]]; then
+if [[ "$1" == "nami" && "$2" == "start" ]] || [[ "$1" == "/run.sh" ]]; then
   nami_initialize redmine
 fi
 
