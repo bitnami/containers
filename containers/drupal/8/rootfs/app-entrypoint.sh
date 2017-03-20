6d80825678d132532c2016572abc5ffea953363b
Sameer Naik <sameer@bitnami.com>
2017-03-20 14:16:48 +0100

    drupal: renamed `run-drupal.sh` to `run.sh`

--- containers/drupal/8/rootfs/app-entrypoint.sh
+++ containers/drupal/8/rootfs/app-entrypoint.sh
@@ -5,7 +5,7 @@
 print_welcome_page
 check_for_updates &
 
-if [[ "$1" == "nami" && "$2" == "start" ]] || [[ "$1" == "/run-drupal.sh" ]]; then
+if [[ "$1" == "nami" && "$2" == "start" ]] || [[ "$1" == "/run.sh" ]]; then
   nami_initialize apache php drupal
 fi
 
