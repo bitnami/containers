bd81f94d7c73ab3477bae45cb17bfdfbe00935e1
Sameer Naik <sameer@bitnami.com>
2017-03-28 19:18:08 +0200

    php: log output to stdout

--- containers/php-fpm/5.6/rootfs/run.sh
+++ containers/php-fpm/5.6/rootfs/run.sh
@@ -6,5 +6,8 @@ DAEMON=php-fpm
 EXEC=$(which $DAEMON)
 ARGS="-F --pid /opt/bitnami/php/tmp/php5-fpm.pid --fpm-config /opt/bitnami/php/etc/php-fpm.conf --prefix /opt/bitnami/php -c /opt/bitnami/php/etc/php.ini"
 
+# redirect php logs to stdout
+ln -sf /dev/stdout /opt/bitnami/php/logs/php-fpm.log
+
 info "Starting ${DAEMON}..."
 exec ${EXEC} ${ARGS}
