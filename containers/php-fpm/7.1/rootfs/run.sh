c96072fa6e38754a7d00cb9c17c0c1756fbeb436
Sameer Naik <sameer@bitnami.com>
2017-03-28 19:14:04 +0200

    php-fpm: fix launch arguments

--- containers/php-fpm/7.1/rootfs/run.sh
+++ containers/php-fpm/7.1/rootfs/run.sh
@@ -4,7 +4,7 @@
 
 DAEMON=php-fpm
 EXEC=$(which $DAEMON)
-ARGS="-F"
+ARGS="-F --pid /opt/bitnami/php/tmp/php5-fpm.pid --fpm-config /opt/bitnami/php/etc/php-fpm.conf --prefix /opt/bitnami/php -c /opt/bitnami/php/etc/php.ini"
 
 info "Starting ${DAEMON}..."
 exec ${EXEC} ${ARGS}
