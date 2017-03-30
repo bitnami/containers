f7484f07673d2a81d2ecc98782d03c6006f0b546
Sameer Naik <sameer@bitnami.com>
2017-03-30 11:06:55 +0200

    moved `chown` instruction to `run.sh`

--- bitnami/php-fpm/7.1/rootfs/run.sh
+++ bitnami/php-fpm/7.1/rootfs/run.sh
@@ -6,6 +6,8 @@ DAEMON=php-fpm
 EXEC=$(which $DAEMON)
 ARGS="-F --pid /opt/bitnami/php/tmp/php5-fpm.pid --fpm-config /opt/bitnami/php/etc/php-fpm.conf --prefix /opt/bitnami/php -c /opt/bitnami/php/etc/php.ini"
 
+chown -R :daemon /bitnami/php || true
+
 # redirect php logs to stdout
 ln -sf /dev/stdout /opt/bitnami/php/logs/php-fpm.log
 
