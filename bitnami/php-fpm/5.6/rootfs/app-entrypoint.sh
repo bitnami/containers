b1a3a2d47254f457023b6012fcc6313842900f8c
Sameer Naik <sameer@bitnami.com>
2017-03-30 11:08:08 +0200

    moved `composer install` to `run.sh`

--- bitnami/php-fpm/5.6/rootfs/app-entrypoint.sh
+++ bitnami/php-fpm/5.6/rootfs/app-entrypoint.sh
@@ -5,10 +5,6 @@
 print_welcome_page
 check_for_updates &
 
-if [ -f composer.json  ]; then
-  composer install
-fi
-
 if [[ "$1" == "nami" && "$2" == "start" ]] || [[ "$1" == "/run.sh" ]]; then
   nami_initialize php
 fi
