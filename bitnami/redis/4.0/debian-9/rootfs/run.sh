941fb699be080ae4b108a79dd393ae55e2b1e11f
juan131 <juan@bitnami.com>
2018-10-02 10:27:03 +0200

    Fix typos

--- bitnami/redis/4.0/debian-9/rootfs/run.sh
+++ bitnami/redis/4.0/debian-9/rootfs/run.sh
@@ -17,7 +17,7 @@ REDIS_EXTRA_FLAGS=${REDIS_EXTRA_FLAGS:-}
 
 # configure extra command line flags
 if [[ -n "$REDIS_EXTRA_FLAGS" ]]; then
-    warn "REDIS_EXTRA_FLAGS is depredated. Please specify any extra-flag use 'run.sh $REDIS_EXTRA_FLAGS' as command instead"
+    warn "REDIS_EXTRA_FLAGS is deprecated. Please specify any extra-flag using 'run.sh $REDIS_EXTRA_FLAGS' as command instead"
     ARGS+=" $REDIS_EXTRA_FLAGS"
 fi
 
