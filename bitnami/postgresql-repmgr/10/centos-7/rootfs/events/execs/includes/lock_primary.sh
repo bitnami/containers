2b7b0e039e66787d01da0b16e97d1a596330fca9
juan131 <juan@bitnami.com>
2020-01-15 10:55:29 +0100

    Remove Centos7 from supported distros

--- bitnami/postgresql-repmgr/10/centos-7/rootfs/events/execs/includes/lock_primary.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -o errexit
-set -o nounset
-set -o pipefail
-# set -o xtrace # Uncomment this line for debugging purpose
-# shellcheck disable=SC2154
-
-echo  "$header Locking primary..."
