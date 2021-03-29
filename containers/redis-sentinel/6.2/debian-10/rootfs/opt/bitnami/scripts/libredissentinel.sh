14165fea8a5a027becca8d225f46b656db061864
Corina Purcarea <corinapurcarea@gmail.com>
2021-03-29 18:01:16 +0300

    added setting announce-hostnames for redis 6.2 (#39)

--- containers/redis-sentinel/6.2/debian-10/rootfs/opt/bitnami/scripts/libredissentinel.sh
+++ containers/redis-sentinel/6.2/debian-10/rootfs/opt/bitnami/scripts/libredissentinel.sh
@@ -220,6 +220,7 @@ redis_initialize() {
         [[ -z "$REDIS_SENTINEL_ANNOUNCE_PORT" ]] || redis_conf_set "sentinel announce-port" "${REDIS_SENTINEL_ANNOUNCE_PORT}"
         # Sentinel's configuration was refactored for Redis 6.2 and hostname's support now has to be enabled using a configuration parameter. 
         [[ $(redis_version --major) -ge 6 ]] && [[ $(redis_version --minor) -ge 2 ]] && redis_conf_set "sentinel resolve-hostnames" "${REDIS_SENTINEL_RESOLVE_HOSTNAMES}"
+        [[ $(redis_version --major) -ge 6 ]] && [[ $(redis_version --minor) -ge 2 ]] && redis_conf_set "sentinel announce-hostnames" "${REDIS_SENTINEL_ANNOUNCE_HOSTNAMES}"
 
         # Sentinel Configuration (maybe overwritten by more specific init blocks like TLS configuration)
         redis_conf_set port "$REDIS_SENTINEL_PORT_NUMBER"
