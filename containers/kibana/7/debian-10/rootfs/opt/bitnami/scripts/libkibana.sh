6ae366670feabf5668afaf806b2a7f252bb57cce
jerryharrow <84593277+jerryharrow@users.noreply.github.com>
2021-05-25 03:11:26 -0400

    Handle server.basePath in is_kibana_ready check (#49)

--- containers/kibana/7/debian-10/rootfs/opt/bitnami/scripts/libkibana.sh
+++ containers/kibana/7/debian-10/rootfs/opt/bitnami/scripts/libkibana.sh
@@ -112,6 +112,24 @@ kibana_conf_set() {
 }
 
 ########################
+# Read a configuration setting value
+# Globals:
+#   KIBANA_*
+# Arguments:
+#   $1 - key
+# Returns:
+#   Outputs the key to stdout (Empty response if key is not set)
+#########################
+kibana_conf_get() {
+    local key="${1:?missing key}"
+
+    if [[ -r "$KIBANA_CONF_FILE" ]]; then
+        yq r "$KIBANA_CONF_FILE" "$key"
+    fi
+}
+
+
+########################
 # Configure/initialize Kibana
 # For backwards compatibility, it is allowed to specify the host and port in
 # different env-vars and this function will build the correct url.
@@ -181,8 +199,13 @@ is_kibana_not_running() {
 #   Boolean
 #########################
 is_kibana_ready() {
+    local basePath=
+	local rewriteBasePath=$(kibana_conf_get "[server.rewriteBasePath]")
+	if [[ "$rewriteBasePath" == "true" ]]; then
+		basePath=$(kibana_conf_get "[server.basePath]")
+	fi
     if is_kibana_running; then
-        local -r state="$(yq r - "status.overall.state" <<<"$(curl -s "127.0.0.1:${KIBANA_PORT_NUMBER}/api/status")")"
+        local -r state="$(yq r - "status.overall.state" <<<"$(curl -s "127.0.0.1:${KIBANA_PORT_NUMBER}${basePath}/api/status")")"
         [[ "$state" == "green" ]]
     else
         false
