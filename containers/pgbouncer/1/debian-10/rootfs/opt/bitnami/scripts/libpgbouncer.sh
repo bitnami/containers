4d906bb006e4b8b57a4825aa069a3aff18836e2a
James L <somesocks@gmail.com>
2021-07-14 11:09:39 +0200

    Update 1/debian-10/rootfs/opt/bitnami/scripts/libpgbouncer.sh
    Co-authored-by: Javier J. Salmerón-García <jsalmeron@vmware.com>

--- containers/pgbouncer/1/debian-10/rootfs/opt/bitnami/scripts/libpgbouncer.sh
+++ containers/pgbouncer/1/debian-10/rootfs/opt/bitnami/scripts/libpgbouncer.sh
@@ -198,7 +198,7 @@ pgbouncer_initialize() {
         ini-file set --section "pgbouncer" --key "logfile" --value "$PGBOUNCER_LOG_FILE" "$PGBOUNCER_CONF_FILE"
         ini-file set --section "pgbouncer" --key "admin_users" --value "$POSTGRESQL_USERNAME" "$PGBOUNCER_CONF_FILE"
         ini-file set --section "pgbouncer" --key "client_tls_sslmode" --value "$PGBOUNCER_CLIENT_TLS_SSLMODE" "$PGBOUNCER_CONF_FILE"
-        if [[ -n "$PGBOUNCER_IGNORE_STARTUP_PARAMETERS" ]]; then
+        if ! is_empty_value "$PGBOUNCER_IGNORE_STARTUP_PARAMETERS"; then
             ini-file set --section "pgbouncer" --key "ignore_startup_parameters" --value "$PGBOUNCER_IGNORE_STARTUP_PARAMETERS" "$PGBOUNCER_CONF_FILE"
         fi
         if [[ "$PGBOUNCER_CLIENT_TLS_SSLMODE" != "disable" ]]; then
