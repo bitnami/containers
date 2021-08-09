a6f681952514b9fba344c8813fa2db7f67c74664
Josh Girón <n0rig@users.noreply.github.com>
2021-08-09 03:10:50 -0400

    Fix typo: 'sonar_conf_set' -> 'sonarqube_conf_set'. (#38)
    Co-authored-by: Josh Giron <jgiron@vmware.com>

--- bitnami/sonarqube/9/debian-10/rootfs/opt/bitnami/scripts/libsonarqube.sh
+++ bitnami/sonarqube/9/debian-10/rootfs/opt/bitnami/scripts/libsonarqube.sh
@@ -133,7 +133,7 @@ sonarqube_initialize() {
     if [[ "${#additional_properties[@]}" -gt 0 ]]; then
         info "Adding properties provided via SONARQUBE_EXTRA_PROPERTIES to sonar.properties"
         for property in "${additional_properties[@]}"; do
-            sonar_conf_set "${property%=*}" "${property#*=}"
+            sonarqube_conf_set "${property%=*}" "${property#*=}"
         done
     fi
 
