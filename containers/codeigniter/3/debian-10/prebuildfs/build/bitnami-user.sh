c5be5b0adf62ccf28e19319bf6656b292a238a92
Juan José Martos <jotamartos@gmail.com>
2021-11-18 12:17:28 +0100

    Remove unsupported branch 3 (#95)
    Co-authored-by: Jota Martos <jotamartos@vmware.com>

--- containers/codeigniter/3/debian-10/prebuildfs/build/bitnami-user.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-useradd -ms /bin/bash bitnami
-mkdir -p /opt/bitnami
-sed -i -e 's/\s*Defaults\s*secure_path\s*=/# Defaults secure_path=/' /etc/sudoers
-echo 'bitnami ALL=NOPASSWD: ALL' >> /etc/sudoers
