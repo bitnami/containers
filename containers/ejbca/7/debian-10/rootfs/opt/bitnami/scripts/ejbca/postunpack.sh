1c83fa78beb5aa33bd5c427adc415baa37834e64
Miguel A. Cabrera Minagorri <mcabrera@vmware.com>
2021-12-17 18:18:23 +0000

    Refactorize persistence
    Signed-off-by: Miguel A. Cabrera Minagorri <mcabrera@vmware.com>

--- containers/ejbca/7/debian-10/rootfs/opt/bitnami/scripts/ejbca/postunpack.sh
+++ containers/ejbca/7/debian-10/rootfs/opt/bitnami/scripts/ejbca/postunpack.sh
@@ -16,11 +16,11 @@ set -o pipefail
 # Load ejbca environment variables
 . /opt/bitnami/scripts/ejbca-env.sh
 
-for dir in "$EJBCA_TMP_DIR" "$EJBCA_VOLUME_DIR"; do
+for dir in "$EJBCA_BASE_DIR" "$EJBCA_WILDFLY_BASE_DIR"  "$EJBCA_TMP_DIR" "$EJBCA_VOLUME_DIR" \
+           "$EJBCA_WILDFLY_VOLUME_DIR" "${EJBCA_WILDFLY_BASE_DIR}/standalone" \
+           "${EJBCA_WILDFLY_BASE_DIR}/domain" "$EJBCA_WILDFLY_TMP_DIR"; do
     ensure_dir_exists "$dir"
     chmod -R g+rwX "$dir"
 done
 
-chmod -R g+rwX "$EJBCA_BASE_DIR"
-chmod -R g+rwX "$EJBCA_WILDFLY_BASE_DIR"/standalone "$EJBCA_WILDFLY_BASE_DIR"/domain
 chmod g+rw "$EJBCA_WILDFLY_STANDALONE_CONF_FILE"
