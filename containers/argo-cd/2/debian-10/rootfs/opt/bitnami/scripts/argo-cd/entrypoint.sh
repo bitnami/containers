29b6b7ba96ac6ef7169e41bbded49c751d8102a7
juan131 <juan.ariza.1311993@gmail.com>
2022-04-25 20:28:18 +0200

    Fix nss_wrapper configuration
    Signed-off-by: juan131 <juan.ariza.1311993@gmail.com>

--- containers/argo-cd/2/debian-10/rootfs/opt/bitnami/scripts/argo-cd/entrypoint.sh
+++ containers/argo-cd/2/debian-10/rootfs/opt/bitnami/scripts/argo-cd/entrypoint.sh
@@ -14,17 +14,23 @@ set -o pipefail
 
 print_welcome_page
 
+# Set nss_wrapper vars only when running as non-root
 # Configure libnss_wrapper based on the UID/GID used to run the container
 # This container supports arbitrary UIDs, therefore we have do it dynamically
-if ! am_i_root && ! user_exists "$(id -u)" && [[ -f "$LIBNSS_WRAPPER_PATH" ]]; then
-    info "Configuring libnss_wrapper"
-    NSS_WRAPPER_PASSWD="$(mktemp)"
-    export NSS_WRAPPER_PASSWD
-    NSS_WRAPPER_GROUP="$(mktemp)"
-    export NSS_WRAPPER_GROUP
-    echo "argocd:x:$(id -u):$(id -g):ArgoCD:/opt/bitnami/argo-cd:/bin/false" > "$NSS_WRAPPER_PASSWD"
-    echo "argocd:x:$(id -g):" > "$NSS_WRAPPER_GROUP"
-    chmod 400 "$NSS_WRAPPER_PASSWD" "$NSS_WRAPPER_GROUP"
+if ! am_i_root; then
+    export LNAME="argocd"
+    export LD_PRELOAD="/opt/bitnami/common/lib/libnss_wrapper.so"
+    if ! user_exists "$(id -u)" && [[ -f "$LD_PRELOAD" ]]; then
+        info "Configuring libnss_wrapper"
+        NSS_WRAPPER_PASSWD="$(mktemp)"
+        export NSS_WRAPPER_PASSWD
+        NSS_WRAPPER_GROUP="$(mktemp)"
+        export NSS_WRAPPER_GROUP
+        echo "argocd:x:$(id -u):$(id -g):ArgoCD:/opt/bitnami/argo-cd:/bin/false" > "$NSS_WRAPPER_PASSWD"
+        echo "argocd:x:$(id -g):" > "$NSS_WRAPPER_GROUP"
+        chmod 400 "$NSS_WRAPPER_PASSWD" "$NSS_WRAPPER_GROUP"
+    fi
 fi
 
-exec /opt/bitnami/argo-cd/bin/argocd
+echo ""
+exec "$@"
