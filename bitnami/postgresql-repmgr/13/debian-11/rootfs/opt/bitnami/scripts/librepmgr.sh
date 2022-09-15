c28945c6773e42c715b7a636c3a96e94ed6d1186
Martin Salas <martinfar@gmail.com>
2022-09-14 20:13:39 -0400

    [bitnami/postgresql-repmgr]
    This commit is the result of the revision of the code after migruiz4 did his comments.
    Some variables where replace.
    Some formating issues corrected
    
    Change on every postgres version
    
    Signed-off-by: Martin Salas <martinfar@gmail.com>

--- bitnami/postgresql-repmgr/13/debian-11/rootfs/opt/bitnami/scripts/librepmgr.sh
+++ bitnami/postgresql-repmgr/13/debian-11/rootfs/opt/bitnami/scripts/librepmgr.sh
@@ -222,23 +222,16 @@ repmgr_get_primary_node() {
         fi
     else
         if [[ -z "$upstream_host" ]]; then
-
             info "No upstream detected: primary_host: '${REPMGR_PRIMARY_HOST}:${REPMGR_PRIMARY_PORT}'"
             info "No upstream detected: node_network: '${REPMGR_NODE_NETWORK_NAME}:${REPMGR_PORT_NUMBER}'"
-
             if [[ "${REPMGR_PRIMARY_HOST}:${REPMGR_PRIMARY_PORT}" != "${REPMGR_NODE_NETWORK_NAME}:${REPMGR_PORT_NUMBER}" ]]; then
-
                 debug "Primary hosts differences"
-
                 primary_host="$REPMGR_PRIMARY_HOST"
                 primary_port="$REPMGR_PRIMARY_PORT"
             fi
-
             repmgr_wait_primary_node || repmgr_switchover_standby
-
         else
             info "Upstream detected: primary_host: '${upstream_host}:${upstream_port}'"
-
             primary_host="$upstream_host"
             primary_port="$upstream_port"
         fi
@@ -263,17 +256,12 @@ repmgr_get_primary_node() {
 #########################
 
 repmgr_switchover_standby() {
-    local -i timeout="${REPMGR_WAIT_PRIMARY_TIME:-60}"
-
     primary_host=""
     primary_port="${REPMGR_PORT_NUMBER}"
 
-info "Primary node is unavailable. Assuming primary role..."
-
+    info "Primary node is unavailable. Assuming primary role..."
     touch "$REPMGR_PROMOTE_STANDBY_LOCK_FILE_NAME"
-
     debug "Marking node to be promoted from standby to primary by writing the following file: '$REPMGR_PROMOTE_STANDBY_LOCK_FILE_NAME' "
-
     [[ -n "$primary_host" ]] && debug "Primary node: '${primary_host}:${primary_port}'"
 
 }
@@ -878,7 +866,7 @@ repmgr_initialize() {
             repmgr_upgrade_extension
         else
             debug "Skipping repmgr configuration..."
-            debug "Checking if node is marked for promotion "
+            debug "Checking if node is marked for promotion"
             if [[ ! -f "$REPMGR_PRIMARY_ROLE_LOCK_FILE_NAME" && -f "$REPMGR_PROMOTE_STANDBY_LOCK_FILE_NAME"  ]] ; then
                 info "Promoting old Standby to Primary"
                 postgresql_start_bg
