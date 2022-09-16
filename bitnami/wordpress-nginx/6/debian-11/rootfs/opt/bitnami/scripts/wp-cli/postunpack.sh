98d28af1cacbf842d68d2b145c63e3c93471aa36
Bitnami Containers <bitnami-bot@vmware.com>
2022-09-16 20:45:32 +0000

    [bitnami/wordpress-nginx] Release 6.0.2-debian-11-r9
    Signed-off-by: Bitnami Containers <bitnami-bot@vmware.com>

--- bitnami/wordpress-nginx/6/debian-11/rootfs/opt/bitnami/scripts/wp-cli/postunpack.sh
+++ bitnami/wordpress-nginx/6/debian-11/rootfs/opt/bitnami/scripts/wp-cli/postunpack.sh
@@ -5,7 +5,7 @@
 set -o errexit
 set -o nounset
 set -o pipefail
-# set -o xtrace # Uncomment this line for debugging purpose
+# set -o xtrace # Uncomment this line for debugging purposes
 
 # Load WP-CLI environment
 . /opt/bitnami/scripts/wp-cli-env.sh
