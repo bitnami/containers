19484ce4c0ddfcee42b3f932aae8deb54dd33f50
Bitnami Containers <bitnami-bot@vmware.com>
2022-09-17 13:17:46 +0000

    [bitnami/pgbouncer] Release 1.17.0-debian-11-r39
    Signed-off-by: Bitnami Containers <bitnami-bot@vmware.com>

--- bitnami/pgbouncer/1/debian-11/rootfs/opt/bitnami/scripts/pgbouncer/postunpack.sh
+++ bitnami/pgbouncer/1/debian-11/rootfs/opt/bitnami/scripts/pgbouncer/postunpack.sh
@@ -5,7 +5,7 @@
 set -o errexit
 set -o nounset
 set -o pipefail
-# set -o xtrace # Uncomment this line for debugging purpose
+# set -o xtrace # Uncomment this line for debugging purposes
 
 # Load pgbouncer environment
 . /opt/bitnami/scripts/pgbouncer-env.sh
