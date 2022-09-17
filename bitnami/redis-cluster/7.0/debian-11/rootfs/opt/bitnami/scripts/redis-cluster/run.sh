f46aec6c870ed4e58258037e3efffb1db02d46b9
Bitnami Containers <bitnami-bot@vmware.com>
2022-09-17 14:24:10 +0000

    [bitnami/redis-cluster] Release 7.0.4-debian-11-r17
    Signed-off-by: Bitnami Containers <bitnami-bot@vmware.com>

--- bitnami/redis-cluster/7.0/debian-11/rootfs/opt/bitnami/scripts/redis-cluster/run.sh
+++ bitnami/redis-cluster/7.0/debian-11/rootfs/opt/bitnami/scripts/redis-cluster/run.sh
@@ -5,7 +5,7 @@
 set -o errexit
 set -o nounset
 set -o pipefail
-# set -o xtrace # Uncomment this line for debugging purpose
+# set -o xtrace # Uncomment this line for debugging purposes
 
 # Enable job control
 # ref https://www.gnu.org/software/bash/manual/bash.html#The-Set-Builtin
