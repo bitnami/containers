31bc57325e720cac0c2320b3dee9c7e6f1fd0b59
Bitnami Containers <bitnami-bot@vmware.com>
2022-09-17 13:46:58 +0000

    [bitnami/neo4j] Release 4.4.11-debian-11-r5
    Signed-off-by: Bitnami Containers <bitnami-bot@vmware.com>

--- bitnami/neo4j/4/debian-11/rootfs/opt/bitnami/scripts/neo4j/postunpack.sh
+++ bitnami/neo4j/4/debian-11/rootfs/opt/bitnami/scripts/neo4j/postunpack.sh
@@ -5,7 +5,7 @@
 set -o errexit
 set -o nounset
 set -o pipefail
-# set -o xtrace # Uncomment this line for debugging purpose
+# set -o xtrace # Uncomment this line for debugging purposes
 
 # Load Neo4j environment
 . /opt/bitnami/scripts/neo4j-env.sh
