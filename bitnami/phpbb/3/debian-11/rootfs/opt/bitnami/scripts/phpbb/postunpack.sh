6db41df11404058f53cc51789b693c39922b31ae
Bitnami Containers <bitnami-bot@vmware.com>
2022-09-18 20:53:21 +0000

    [bitnami/phpbb] Release 3.3.8-debian-11-r28
    Signed-off-by: Bitnami Containers <bitnami-bot@vmware.com>

--- bitnami/phpbb/3/debian-11/rootfs/opt/bitnami/scripts/phpbb/postunpack.sh
+++ bitnami/phpbb/3/debian-11/rootfs/opt/bitnami/scripts/phpbb/postunpack.sh
@@ -5,7 +5,7 @@
 set -o errexit
 set -o nounset
 set -o pipefail
-# set -o xtrace # Uncomment this line for debugging purpose
+# set -o xtrace # Uncomment this line for debugging purposes
 
 # Load phpBB environment
 . /opt/bitnami/scripts/phpbb-env.sh
