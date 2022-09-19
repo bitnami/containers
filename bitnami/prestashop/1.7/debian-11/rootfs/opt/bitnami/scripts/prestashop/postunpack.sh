9aff7348491533e6984d5663267dfa0df0433fca
Bitnami Containers <bitnami-bot@vmware.com>
2022-09-19 00:03:18 +0000

    [bitnami/prestashop] Release 1.7.8-7-debian-11-r18
    Signed-off-by: Bitnami Containers <bitnami-bot@vmware.com>

--- bitnami/prestashop/1.7/debian-11/rootfs/opt/bitnami/scripts/prestashop/postunpack.sh
+++ bitnami/prestashop/1.7/debian-11/rootfs/opt/bitnami/scripts/prestashop/postunpack.sh
@@ -5,7 +5,7 @@
 set -o errexit
 set -o nounset
 set -o pipefail
-# set -o xtrace # Uncomment this line for debugging purpose
+# set -o xtrace # Uncomment this line for debugging purposes
 
 # Load PrestaShop environment
 . /opt/bitnami/scripts/prestashop-env.sh
