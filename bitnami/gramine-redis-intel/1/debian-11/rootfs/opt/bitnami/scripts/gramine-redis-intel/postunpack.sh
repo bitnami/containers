31a68641cc029f299f8f391f7fda79c933ca3abf
Bitnami Containers <bitnami-bot@vmware.com>
2022-10-17 16:01:13 +0000

    [bitnami/gramine-redis-intel] Release 1.3.1-debian-11-r0
    Signed-off-by: Bitnami Containers <bitnami-bot@vmware.com>

--- bitnami/gramine-redis-intel/1/debian-11/rootfs/opt/bitnami/scripts/gramine-redis-intel/postunpack.sh
+++ bitnami/gramine-redis-intel/1/debian-11/rootfs/opt/bitnami/scripts/gramine-redis-intel/postunpack.sh
@@ -21,9 +21,9 @@ chmod -R g+rwX "$GRAMINE_KEY_DIR" "$REDIS_BASE_DIR"
 info "Relocating gramine python libs"
 mv "${GRAMINE_BASE_DIR}/lib/python3.10/site-packages/graminelibos" /opt/bitnami/python/lib/python3.10/site-packages
 
-# Install libreary dependencies
-info "Installing gramine python depedencies"
-/opt/bitnami/python/bin/pip install  click jinja2 toml protobuf===3.20.0 cryptography
+# Install library dependencies
+info "Installing gramine python dependencies"
+/opt/bitnami/python/bin/pip install  click jinja2 toml protobuf===3.20.0 cryptography pyelftools
 
 # Workaround for gramine scripts that looks for pyton on /usr/bin/python3
 info "Creating python3 symbolic link"
