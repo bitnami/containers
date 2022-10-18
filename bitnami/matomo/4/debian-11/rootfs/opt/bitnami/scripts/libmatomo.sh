f97e41789fdd3c66a09c6bf9ea09b0f86619e164
JoostVanDerSluis <44533996+JoostVanDerSluis@users.noreply.github.com>
2022-10-18 11:21:49 +0200

    [bitnami/matomo] Proxy host headers (#6969)
    * Implemented MATOMO_PROXY_HOST_HEADER
    
    Use the MATOMO_PROXY_HOST_HEADER environment variable to configure the proxy_host_headers-matomo setting
    
    Signed-off-by: JoostVanDerSluis <44533996+JoostVanDerSluis@users.noreply.github.com>
    
    * Documented MATOMO_PROXY_HOST_HEADER
    
    Signed-off-by: JoostVanDerSluis <44533996+JoostVanDerSluis@users.noreply.github.com>
    
    Signed-off-by: JoostVanDerSluis <44533996+JoostVanDerSluis@users.noreply.github.com>
    Signed-off-by: Jiakuan(Daniel) Li <jiakuan.li.cs@gmail.com>

--- bitnami/matomo/4/debian-11/rootfs/opt/bitnami/scripts/libmatomo.sh
+++ bitnami/matomo/4/debian-11/rootfs/opt/bitnami/scripts/libmatomo.sh
@@ -159,6 +159,11 @@ EOF
         info "Configuring Matomo to check proxy_client_headers for client IP"
             ini-file set -s "General" -k "proxy_client_headers[]" -v "$MATOMO_PROXY_CLIENT_HEADER" "$MATOMO_CONF_FILE"
         fi
+        
+        if ! is_empty_value "$MATOMO_PROXY_HOST_HEADER"; then
+        info "Configuring Matomo to check proxy_host_headers for client IP"
+            ini-file set -s "General" -k "proxy_host_headers[]" -v "$MATOMO_PROXY_HOST_HEADER" "$MATOMO_CONF_FILE"
+        fi        
 
         if is_boolean_yes "$MATOMO_ENABLE_ASSUME_SECURE_PROTOCOL"; then
             info "Configuring Matomo to always assume secure protocol"
