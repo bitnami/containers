9ecdb7001db51866fa5a3e67d8a3d0e64af1d9ae
Taso N. Devetzis <git@tarc.net>
2022-11-30 16:47:54 -0800

    Add LDAP_CUSTOM_SCHEMA_DIR option to enable initialization of multiple schemas
    Signed-off-by: Taso N. Devetzis <git@tarc.net>

--- bitnami/openldap/2.6/debian-11/rootfs/opt/bitnami/scripts/libopenldap.sh
+++ bitnami/openldap/2.6/debian-11/rootfs/opt/bitnami/scripts/libopenldap.sh
@@ -36,6 +36,7 @@ export LDAP_ONLINE_CONF_DIR="${LDAP_VOLUME_DIR}/slapd.d"
 export LDAP_PID_FILE="${LDAP_VAR_DIR}/run/slapd.pid"
 export LDAP_CUSTOM_LDIF_DIR="${LDAP_CUSTOM_LDIF_DIR:-/ldifs}"
 export LDAP_CUSTOM_SCHEMA_FILE="${LDAP_CUSTOM_SCHEMA_FILE:-/schema/custom.ldif}"
+export LDAP_CUSTOM_SCHEMA_DIR="${LDAP_CUSTOM_SCHEMA_DIR:-/schemas}"
 export PATH="${LDAP_BIN_DIR}:${LDAP_SBIN_DIR}:$PATH"
 export LDAP_TLS_CERT_FILE="${LDAP_TLS_CERT_FILE:-}"
 export LDAP_TLS_KEY_FILE="${LDAP_TLS_KEY_FILE:-}"
@@ -362,6 +363,23 @@ ldap_add_schemas() {
 ldap_add_custom_schema() {
     info "Adding custom Schema : $LDAP_CUSTOM_SCHEMA_FILE ..."
     debug_execute slapadd -F "$LDAP_ONLINE_CONF_DIR" -n 0 -l  "$LDAP_CUSTOM_SCHEMA_FILE"
+    ldap_stop
+     while is_ldap_running; do sleep 1; done
+    ldap_start_bg
+}
+
+########################
+# Add custom schemas
+# Globals:
+#   LDAP_*
+# Arguments:
+#   None
+# Returns
+#   None
+#########################
+ldap_add_custom_schemas() {
+    info "Adding custom Schemas : $LDAP_CUSTOM_SCHEMA_DIR ..."
+    find "$LDAP_CUSTOM_SCHEMA_DIR" -maxdepth 1 \( -type f -o -type l \) -iname '*.ldif' -print0 | sort -z | xargs --null -I{} bash -c ". /opt/bitnami/scripts/libos.sh && debug_execute slapadd -F \"$LDAP_ONLINE_CONF_DIR\" -n 0 -l {}"    
     ldap_stop
      while is_ldap_running; do sleep 1; done
     ldap_start_bg
