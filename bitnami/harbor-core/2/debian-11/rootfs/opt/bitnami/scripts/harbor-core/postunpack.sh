bd74ded540b794b2f9edc5c77cdf07694e69163b
Bitnami Containers <bitnami-bot@vmware.com>
2022-12-09 23:00:04 +0000

    [bitnami/harbor-core] Release 2.6.2-debian-11-r10
    Signed-off-by: Bitnami Containers <bitnami-bot@vmware.com>

--- bitnami/harbor-core/2/debian-11/rootfs/opt/bitnami/scripts/harbor-core/postunpack.sh
+++ bitnami/harbor-core/2/debian-11/rootfs/opt/bitnami/scripts/harbor-core/postunpack.sh
@@ -2,16 +2,29 @@
 
 # shellcheck disable=SC1091
 
+set -o errexit
+set -o nounset
+set -o pipefail
+# set -o xtrace # Uncomment this line for debugging purposes
+
 # Load libraries
 . /opt/bitnami/scripts/libfs.sh
+. /opt/bitnami/scripts/libos.sh
+. /opt/bitnami/scripts/libservice.sh
 . /opt/bitnami/scripts/libharbor.sh
 
-read -r -a directories <<<"$(get_system_cert_paths)"
-directories+=("/etc/core" "/data")
+# Load environment
+. /opt/bitnami/scripts/harbor-core-env.sh
 
-# Ensure a set of directories exist
+ensure_user_exists "$HARBOR_CORE_DAEMON_USER" --group "$HARBOR_CORE_DAEMON_GROUP"
+
+# Ensure a set of directories exist and the non-root user has write privileges to them
+read -r -a directories <<<"$(get_system_cert_paths)"
+directories+=("/etc/core" "${HARBOR_CORE_VOLUME_DIR}/certificates" "${HARBOR_CORE_VOLUME_DIR}/ca_download" "${HARBOR_CORE_VOLUME_DIR}/psc")
 for dir in "/etc/core" "/data"; do
     ensure_dir_exists "$dir"
+    chmod -R g+rwX "$dir"
+    chown -R "$HARBOR_CORE_DAEMON_USER" "$dir"
 done
 
 # Fix for CentOS Internal TLS
@@ -24,11 +37,6 @@ if [[ -f /etc/pki/tls/certs/ca-bundle.trust.crt ]]; then
 fi
 
 # Add persisted configuration
-ln -sf /data/certificates /etc/core/certificates
-ln -sf /data/ca_download /etc/core/ca_download
-ln -sf /data/psc /etc/core/token
-
-# Ensure the non-root user has writing permission at a set of directories
-for dir in "${directories[@]}"; do
-    chmod -R g+rwX "$dir"
-done
+ln -sf "${HARBOR_CORE_VOLUME_DIR}/certificates" /etc/core/certificates
+ln -sf "${HARBOR_CORE_VOLUME_DIR}/ca_download" /etc/core/ca_download
+ln -sf "${HARBOR_CORE_VOLUME_DIR}/psc" /etc/core/token
