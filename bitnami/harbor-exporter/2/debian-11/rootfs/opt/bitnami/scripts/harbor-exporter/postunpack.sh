128233b17fbbe006187111e015fefc81c8217b42
Bitnami Containers <bitnami-bot@vmware.com>
2022-12-10 22:50:30 +0000

    [bitnami/harbor-exporter] Release 2.6.2-debian-11-r10
    Signed-off-by: Bitnami Containers <bitnami-bot@vmware.com>

--- bitnami/harbor-exporter/2/debian-11/rootfs/opt/bitnami/scripts/harbor-exporter/postunpack.sh
+++ bitnami/harbor-exporter/2/debian-11/rootfs/opt/bitnami/scripts/harbor-exporter/postunpack.sh
@@ -2,18 +2,30 @@
 
 # shellcheck disable=SC1091
 
+set -o errexit
+set -o nounset
+set -o pipefail
+# set -o xtrace # Uncomment this line for debugging purposes
+
 # Load libraries
 . /opt/bitnami/scripts/libfs.sh
+. /opt/bitnami/scripts/libos.sh
+. /opt/bitnami/scripts/libservice.sh
 . /opt/bitnami/scripts/libharbor.sh
+. /opt/bitnami/scripts/libharborexporter.sh
 
+# Load environment
+. /opt/bitnami/scripts/harbor-exporter-env.sh
+
+ensure_user_exists "$HARBOR_EXPORTER_DAEMON_USER" --group "$HARBOR_EXPORTER_DAEMON_GROUP"
+
+# Ensure a set of directories exist and the non-root user has write privileges to them
 read -r -a directories <<<"$(get_system_cert_paths)"
 directories+=("/var/log/jobs")
-
-# Ensure a set of directories exist
-# Ensure the non-root user has writing permission at a set of directories
 for dir in "${directories[@]}"; do
     ensure_dir_exists "$dir"
     chmod -R g+rwX "$dir"
+    chown -R "$HARBOR_EXPORTER_DAEMON_USER" "$dir"
 done
 
 # Fix for CentOS Internal TLS
