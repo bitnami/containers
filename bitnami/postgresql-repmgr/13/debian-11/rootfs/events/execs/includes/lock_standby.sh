4519d7254bb3faa7a156c1b3a7932eda2e79d0a8
Bitnami Containers <bitnami-bot@vmware.com>
2023-01-11 14:12:15 +0000

    [bitnami/postgresql-repmgr] Release 13.9.0-debian-11-r21
    Signed-off-by: Bitnami Containers <bitnami-bot@vmware.com>

--- bitnami/postgresql-repmgr/13/debian-11/rootfs/events/execs/includes/lock_standby.sh
+++ bitnami/postgresql-repmgr/13/debian-11/rootfs/events/execs/includes/lock_standby.sh
@@ -5,8 +5,8 @@ set -o nounset
 set -o pipefail
 # set -o xtrace # Uncomment this line for debugging purposes
 
-readonly query="SELECT upstream_node_id FROM repmgr.nodes WHERE node_id=$(repmgr_get_node_id)"
-readonly new_upstream_node_id="$(echo "$query" | BITNAMI_DEBUG=true postgresql_execute "$REPMGR_DATABASE" "$POSTGRESQL_REPLICATION_USER" "$POSTGRESQL_REPLICATION_PASSWORD" "" "" "-tA")"
+query="SELECT upstream_node_id FROM repmgr.nodes WHERE node_id=$(repmgr_get_node_id)"
+new_upstream_node_id="$(echo "$query" | BITNAMI_DEBUG=true postgresql_execute "$REPMGR_DATABASE" "$POSTGRESQL_REPLICATION_USER" "$POSTGRESQL_REPLICATION_PASSWORD" "" "" "-tA")"
 if [[ -n "$new_upstream_node_id" ]]; then
     # shellcheck disable=SC2154
     echo "$header Locking standby (new_upstream_node_id=$new_upstream_node_id)..."
