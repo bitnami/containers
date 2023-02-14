9cd8733fb38c28b7890dcf06338818cdc09c4ed8
Bitnami Containers <bitnami-bot@vmware.com>
2023-02-14 16:01:25 +0000

    [bitnami/airflow-worker] Release 2.5.1-debian-11-r9
    Signed-off-by: Bitnami Containers <bitnami-bot@vmware.com>

--- bitnami/airflow-worker/2/debian-11/rootfs/opt/bitnami/scripts/libairflowworker.sh
+++ bitnami/airflow-worker/2/debian-11/rootfs/opt/bitnami/scripts/libairflowworker.sh
@@ -32,6 +32,18 @@ airflow_worker_validate() {
     # Check postgresql host
     [[ -z "$AIRFLOW_DATABASE_HOST" ]] && print_validation_error "Missing AIRFLOW_DATABASE_HOST"
 
+    # Check cryptography parameters
+    if [[ -n "$AIRFLOW_RAW_FERNET_KEY" && -z "$AIRFLOW_FERNET_KEY" ]]; then
+        local fernet_char_count
+        fernet_char_count="$(echo -n "$AIRFLOW_RAW_FERNET_KEY")"
+        if [[ "$fernet_char_count" -lt 32 ]]; then
+            print_validation_error "AIRFLOW_RAW_FERNET_KEY must have at least 32 characters"
+        elif [[ "$fernet_char_count" -gt 32 ]]; then
+            warn "AIRFLOW_RAW_FERNET_KEY has more than 32 characters, the rest will be ignored"
+        fi
+        AIRFLOW_FERNET_KEY="$(echo -n "${AIRFLOW_RAW_FERNET_KEY:0:32}" | base64)"
+    fi
+
     # Avoid fail because of the above check
     true
 }
