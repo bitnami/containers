e458c9416ee4bed34cf9de14f3b16f578dcb93eb
Jose Antonio Carmona <jcarmona@vmware.com>
2023-03-08 15:04:09 +0100

    Exclude paths
    Signed-off-by: Jose Antonio Carmona <jcarmona@vmware.com>

--- .vib/common/goss/scripts/check-linked-libraries.sh
+++ .vib/common/goss/scripts/check-linked-libraries.sh
@@ -7,5 +7,8 @@ set -o pipefail
 mapfile -t files < <( find "$BITNAMI_ROOT_DIR"/"$BITNAMI_APP_NAME" "$BITNAMI_ROOT_DIR"/common -type f -executable )
 
 for file in "${files[@]}"; do
+  if [[ -n $PATHS_TO_EXCLUDE ]] && [[ "$file" =~ $PATHS_TO_EXCLUDE ]]; then
+    continue
+  fi
   [[ $(ldd "$file" | grep -c "not found") -eq 0 ]] || exit 1
 done
