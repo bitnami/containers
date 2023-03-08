3e08e7be9483e75eabdf33b87f2dd18980661e20
Jose Antonio Carmona <jcarmona@vmware.com>
2023-03-08 17:24:20 +0100

    Use new naming for property
    Signed-off-by: Jose Antonio Carmona <jcarmona@vmware.com>

--- .vib/common/goss/scripts/check-linked-libraries.sh
+++ .vib/common/goss/scripts/check-linked-libraries.sh
@@ -7,7 +7,7 @@ set -o pipefail
 mapfile -t files < <( find "$BITNAMI_ROOT_DIR"/"$BITNAMI_APP_NAME" "$BITNAMI_ROOT_DIR"/common -type f -executable )
 
 for file in "${files[@]}"; do
-  if [[ -n $PATHS_TO_EXCLUDE ]] && [[ "$file" =~ $PATHS_TO_EXCLUDE ]]; then
+  if [[ -n $EXCLUDE_PATHS ]] && [[ "$file" =~ $EXCLUDE_PATHS ]]; then
     continue
   fi
   [[ $(ldd "$file" | grep -c "not found") -eq 0 ]] || exit 1
