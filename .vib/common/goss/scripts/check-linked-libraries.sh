0fec0ce8e9089e4fcd8dbdb867c538140e0d3ec9
Jose Antonio Carmona <jcarmona@vmware.com>
2023-03-09 16:15:49 +0100

    [bitnami/jruby] Add VIB tests (#26705)
    * [bitnami/jruby] Add VIB tests
    
    Signed-off-by: Jose Antonio Carmona <jcarmona@vmware.com>
    
    * Remove VIB trigger
    
    Signed-off-by: Jose Antonio Carmona <jcarmona@vmware.com>
    
    * Exclude paths
    
    Signed-off-by: Jose Antonio Carmona <jcarmona@vmware.com>
    
    * Add linked-libraries
    
    Signed-off-by: Jose Antonio Carmona <jcarmona@vmware.com>
    
    * Increase timeouts
    
    Signed-off-by: Jose Antonio Carmona <jcarmona@vmware.com>
    
    * Use new naming for property
    
    Signed-off-by: Jose Antonio Carmona <jcarmona@vmware.com>
    
    * Improve identification of optional args
    
    Signed-off-by: Jose Antonio Carmona <jcarmona@vmware.com>
    
    * Remove VIB trigger
    
    Signed-off-by: Jose Antonio Carmona <jcarmona@vmware.com>
    
    * Fix files.mode path
    
    Signed-off-by: Jose Antonio Carmona <jcarmona@vmware.com>
    
    ---------
    
    Signed-off-by: Jose Antonio Carmona <jcarmona@vmware.com>

--- .vib/common/goss/scripts/check-linked-libraries.sh
+++ .vib/common/goss/scripts/check-linked-libraries.sh
@@ -7,5 +7,8 @@ set -o pipefail
 mapfile -t files < <( find "$BITNAMI_ROOT_DIR"/"$BITNAMI_APP_NAME" "$BITNAMI_ROOT_DIR"/common -type f -executable )
 
 for file in "${files[@]}"; do
+  if [[ -n $EXCLUDE_PATHS ]] && [[ "$file" =~ $EXCLUDE_PATHS ]]; then
+    continue
+  fi
   [[ $(ldd "$file" | grep -c "not found") -eq 0 ]] || exit 1
 done
