3b1db1f8968c3616719409cf578d6b1379652e73
Nikolai Prokoschenko <nikolai@prokoschenko.de>
2023-04-27 09:52:14 +0200

    [bitnami/redis] Fix fetching `REDIS_MASTER_HOST` via `REDIS_SENTINEL_HOST`
    Signed-off-by: Nikolai Prokoschenko <nikolai@prokoschenko.de>

--- bitnami/redis/7.0/debian-11/rootfs/opt/bitnami/scripts/libredis.sh
+++ bitnami/redis/7.0/debian-11/rootfs/opt/bitnami/scripts/libredis.sh
@@ -265,7 +265,7 @@ redis_configure_replication() {
             local -a sentinel_info_command=("redis-cli" "-h" "${REDIS_SENTINEL_HOST}" "-p" "${REDIS_SENTINEL_PORT_NUMBER}")
             is_boolean_yes "$REDIS_TLS_ENABLED" && sentinel_info_command+=("--tls" "--cert" "${REDIS_TLS_CERT_FILE}" "--key" "${REDIS_TLS_KEY_FILE}" "--cacert" "${REDIS_TLS_CA_FILE}")
             sentinel_info_command+=("sentinel" "get-master-addr-by-name" "${REDIS_SENTINEL_MASTER_NAME}")
-            read -r -a REDIS_SENTINEL_INFO <<< "$("${sentinel_info_command[@]}")"
+            read -r -a REDIS_SENTINEL_INFO <<< "$("${sentinel_info_command[@]}" | tr '\n' ' ')"
             REDIS_MASTER_HOST=${REDIS_SENTINEL_INFO[0]}
             REDIS_MASTER_PORT_NUMBER=${REDIS_SENTINEL_INFO[1]}
         fi
