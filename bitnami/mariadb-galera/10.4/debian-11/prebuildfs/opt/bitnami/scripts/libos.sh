4dfe96eae34075aa584b2183a47f25ae73bbe2cc
Bitnami Containers <bitnami-bot@vmware.com>
2023-05-06 08:35:10 +0000

    [bitnami/mariadb-galera] Release 10.4.28-debian-11-r27
    Signed-off-by: Bitnami Containers <bitnami-bot@vmware.com>

--- bitnami/mariadb-galera/10.4/debian-11/prebuildfs/opt/bitnami/scripts/libos.sh
+++ bitnami/mariadb-galera/10.4/debian-11/prebuildfs/opt/bitnami/scripts/libos.sh
@@ -412,11 +412,16 @@ generate_random_string() {
     ascii)
         filter="[:print:]"
         ;;
+    numeric)
+        filter="0-9"
+        ;;
     alphanumeric)
         filter="a-zA-Z0-9"
         ;;
-    numeric)
-        filter="0-9"
+    alphanumeric+special|special+alphanumeric)
+        # Limit variety of special characters, so there is a higher chance of containing more alphanumeric characters
+        # Special characters are harder to write, and it could impact the overall UX if most passwords are too complex
+        filter='a-zA-Z0-9!@#$%^'
         ;;
     *)
         echo "Invalid type ${type}" >&2
