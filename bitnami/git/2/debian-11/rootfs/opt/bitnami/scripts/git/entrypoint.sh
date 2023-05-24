959d33ccb4e6dcc975809ff6ad9a7147778cbb44
Bitnami Containers <bitnami-bot@vmware.com>
2023-05-24 14:34:11 +0000

    [bitnami/git] Release 2.40.1-debian-11-r9
    Signed-off-by: Bitnami Containers <bitnami-bot@vmware.com>

--- bitnami/git/2/debian-11/rootfs/opt/bitnami/scripts/git/entrypoint.sh
+++ bitnami/git/2/debian-11/rootfs/opt/bitnami/scripts/git/entrypoint.sh
@@ -6,6 +6,19 @@ set -o nounset
 set -o pipefail
 #set -o xtrace
 
+# Generate new SSH key pairs if they don't exist
+if [[ ! -f /etc/ssh/ssh_host_rsa_key ]]; then
+    ssh-keygen -q -t rsa -f /etc/ssh/ssh_host_rsa_key -N "" <<<y >/dev/null 2>&1
+fi
+
+if [[ ! -f /etc/ssh/ssh_host_ecdsa_key ]]; then
+    ssh-keygen -q -t ecdsa -f /etc/ssh/ssh_host_ecdsa_key -N "" <<<y >/dev/null 2>&1
+fi
+
+if [[ ! -f /etc/ssh/ssh_host_ed25519_key ]]; then
+    ssh-keygen -q -t ed25519 -f /etc/ssh/ssh_host_ed25519_key -N "" <<<y >/dev/null 2>&1
+fi
+
 if ! getent passwd "$(id -u)" &>/dev/null && [ -e "$NSS_WRAPPER_LIB" ]; then
     export LD_PRELOAD="$NSS_WRAPPER_LIB"
     # shellcheck disable=SC2155
