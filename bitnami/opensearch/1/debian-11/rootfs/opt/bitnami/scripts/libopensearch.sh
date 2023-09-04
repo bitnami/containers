ff8ece86b91a44a4260dc48e37af3089d61da607
Andrea Rabbaglietti <rabbagliettiandrea@gmail.com>
2023-09-04 21:09:03 +0200

    [bitnami/opensearch] Fix readiness/liveness probe not working if "restEncryption: false" in values.yaml
    Signed-off-by: Andrea Rabbaglietti <rabbagliettiandrea@gmail.com>

--- bitnami/opensearch/1/debian-11/rootfs/opt/bitnami/scripts/libopensearch.sh
+++ bitnami/opensearch/1/debian-11/rootfs/opt/bitnami/scripts/libopensearch.sh
@@ -940,7 +940,8 @@ elasticsearch_healthcheck() {
 
     host=$(get_elasticsearch_hostname)
 
-    is_boolean_yes "$DB_ENABLE_SECURITY" && is_boolean_yes "$DB_ENABLE_REST_TLS" && protocol="https" && command_args+=("-k" "--user" "${DB_USERNAME}:${DB_PASSWORD}")
+    is_boolean_yes "$DB_ENABLE_SECURITY" && command_args+=("-k" "--user" "${DB_USERNAME}:${DB_PASSWORD}")
+    is_boolean_yes "$DB_ENABLE_REST_TLS" && protocol="https"
 
     # Combination of --silent, --output and --write-out allows us to obtain both the status code and the request body
     output=$(mktemp)
