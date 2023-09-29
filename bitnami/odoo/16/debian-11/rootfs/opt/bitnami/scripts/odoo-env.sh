77883fae9d131aca7c0319eba2b9ed9e2a3577fb
Luis Valdés <luis@valdes.com.br>
2023-09-29 19:11:22 +0000

    Fix unbound variable
    Signed-off-by: Luis Valdés <luis@valdes.com.br>

--- bitnami/odoo/16/debian-11/rootfs/opt/bitnami/scripts/odoo-env.sh
+++ bitnami/odoo/16/debian-11/rootfs/opt/bitnami/scripts/odoo-env.sh
@@ -95,7 +95,7 @@ export ODOO_LONGPOLLING_PORT_NUMBER="${ODOO_LONGPOLLING_PORT_NUMBER:-8072}" # on
 export ODOO_SKIP_BOOTSTRAP="${ODOO_SKIP_BOOTSTRAP:-no}" # only used during the first initialization
 export ODOO_SKIP_MODULES_UPDATE="${ODOO_SKIP_MODULES_UPDATE:-no}" # only used during the first initialization
 export ODOO_LOAD_DEMO_DATA="${ODOO_LOAD_DEMO_DATA:-no}" # only used during the first initialization
-export ODOO_DATABASE_FILTER="${ODOO_DATABASE_FILTER:.*}"
+export ODOO_DATABASE_FILTER="${ODOO_DATABASE_FILTER:-.*}"
 
 # Odoo credentials
 export ODOO_EMAIL="${ODOO_EMAIL:-user@example.com}" # only used during the first initialization
