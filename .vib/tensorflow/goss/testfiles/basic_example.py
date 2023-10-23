4ec82e7c8d84cc583747999e68d48e23eec3c13f
Javier Salmeron Garcia <jsalmeron@vmware.com>
2023-10-23 10:39:10 +0200

    test: :white_check_mark: Add basic example
    Signed-off-by: Javier Salmeron Garcia <jsalmeron@vmware.com>

---
+++ .vib/tensorflow/goss/testfiles/basic_example.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+# Taken from https://www.tensorflow.org/tutorials/quickstart/beginner
+import tensorflow as tf
+
+mnist = tf.keras.datasets.mnist
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+x_train, x_test = x_train / 255.0, x_test / 255.0
+
+model = tf.keras.models.Sequential([
+  tf.keras.layers.Flatten(input_shape=(28, 28)),
+  tf.keras.layers.Dense(128, activation='relu'),
+  tf.keras.layers.Dropout(0.2),
+  tf.keras.layers.Dense(10)
+])
+
+predictions = model(x_train[:1]).numpy()
+
+tf.nn.softmax(predictions).numpy()
+
+loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+
+loss_fn(y_train[:1], predictions).numpy()
+
+model.compile(optimizer='adam',
+              loss=loss_fn,
+              metrics=['accuracy'])
+
+model.fit(x_train, y_train, epochs=5)
+
+
+model.evaluate(x_test,  y_test, verbose=2)
+
+
+probability_model = tf.keras.Sequential([
+  model,
+  tf.keras.layers.Softmax()
+])
+
+print(probability_model(x_test[:5]))
