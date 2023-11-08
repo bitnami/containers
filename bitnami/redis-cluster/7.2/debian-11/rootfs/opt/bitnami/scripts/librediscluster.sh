afa8d85a154e4626e3c5528b6f1fb13fb00cbb69
ChrisSch <122783475+Christine-Schneidenwind@users.noreply.github.com>
2023-11-08 10:01:19 +0100

    [bitnami/redis-cluster] add info level to logs Signed-off-by: Schneidenwind Christine (LDC) <… (#52377)
    * add info level to logs Signed-off-by: Schneidenwind Christine (LDC) <Christine.Schneidenwind@liebherr.com>
    
    Signed-off-by: Schneidenwind Christine (LDC) <Christine.Schneidenwind@liebherr.com>
    
    * use info instead of log
    Signed-off-by: Schneidenwind Christine (LDC) <Christine.Schneidenwind@liebherr.com>
    
    ---------
    
    Signed-off-by: Schneidenwind Christine (LDC) <Christine.Schneidenwind@liebherr.com>

--- bitnami/redis-cluster/7.2/debian-11/rootfs/opt/bitnami/scripts/librediscluster.sh
+++ bitnami/redis-cluster/7.2/debian-11/rootfs/opt/bitnami/scripts/librediscluster.sh
@@ -224,7 +224,7 @@ redis_cluster_update_ips() {
             newIP=$(wait_for_dns_lookup "${host_and_port[0]}" "$REDIS_DNS_RETRIES" 5)
             # The node can be new if we are updating the cluster, so catch the unbound variable error
             if [[ ${host_2_ip_array[$node]+true} ]]; then
-                echo "Changing old IP ${host_2_ip_array[$node]} by the new one ${newIP}"
+                info "Changing old IP ${host_2_ip_array[$node]} by the new one ${newIP}"
                 nodesFile=$(sed "s/ ${host_2_ip_array[$node]}:/ $newIP<NEWIP>:/g" "${REDIS_DATA_DIR}/nodes.conf")
                 echo "$nodesFile" >"${REDIS_DATA_DIR}/nodes.conf"
             fi
