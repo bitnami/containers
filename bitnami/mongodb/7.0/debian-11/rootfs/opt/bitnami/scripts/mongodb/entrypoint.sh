6f59a54ef9b39db02dea5b1ad55c51cf865ebca5
Bitnami Containers <bitnami-bot@vmware.com>
2024-02-16 16:32:33 +0000

    [bitnami/mongodb] Release 7.0.5-debian-11-r23
    Signed-off-by: Bitnami Containers <bitnami-bot@vmware.com>

--- bitnami/mongodb/7.0/debian-11/rootfs/opt/bitnami/scripts/mongodb/entrypoint.sh
+++ bitnami/mongodb/7.0/debian-11/rootfs/opt/bitnami/scripts/mongodb/entrypoint.sh
@@ -18,6 +18,12 @@ set -o pipefail
 
 print_welcome_page
 
+# We add the copy from default config in the entrypoint to not break users 
+# bypassing the setup.sh logic. If the file already exists do not overwrite (in
+# case someone mounts a configuration file in /opt/bitnami/mongodb/conf)
+debug "Copying files from $MONGODB_DEFAULT_CONF_DIR to $MONOGODB_CONF_DIR"
+cp -nr "$MONGODB_DEFAULT_CONF_DIR"/. "$MONGODB_CONF_DIR"
+
 if [[ "$1" = "/opt/bitnami/scripts/mongodb/run.sh" ]]; then
     info "** Starting MongoDB setup **"
     /opt/bitnami/scripts/mongodb/setup.sh
