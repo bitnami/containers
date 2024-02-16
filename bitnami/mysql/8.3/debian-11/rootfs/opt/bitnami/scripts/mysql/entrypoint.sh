5023957d95495e4d50d2b05336ae0ca418e66535
Bitnami Containers <bitnami-bot@vmware.com>
2024-02-16 12:08:24 +0000

    [bitnami/mysql] Release 8.3.0-debian-11-r16
    Signed-off-by: Bitnami Containers <bitnami-bot@vmware.com>

--- bitnami/mysql/8.3/debian-11/rootfs/opt/bitnami/scripts/mysql/entrypoint.sh
+++ bitnami/mysql/8.3/debian-11/rootfs/opt/bitnami/scripts/mysql/entrypoint.sh
@@ -18,6 +18,12 @@ set -o pipefail
 
 print_welcome_page
 
+# We add the copy from default config in the entrypoint to not break users 
+# bypassing the setup.sh logic. If the file already exists do not overwrite (in
+# case someone mounts a configuration file in /opt/bitnami/mysql/conf)
+debug "Copying files from $DB_DEFAULT_CONF_DIR to $DB_CONF_DIR"
+cp -nfr "$DB_DEFAULT_CONF_DIR"/. "$DB_CONF_DIR"
+
 if [[ "$1" = "/opt/bitnami/scripts/mysql/run.sh" ]]; then
     info "** Starting MySQL setup **"
     /opt/bitnami/scripts/mysql/setup.sh
