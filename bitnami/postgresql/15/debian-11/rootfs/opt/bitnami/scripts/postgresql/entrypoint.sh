4eb7eabb976800cb5422c6ffc614d1e9d0a0207b
Bitnami Containers <bitnami-bot@vmware.com>
2024-02-16 13:01:03 +0000

    [bitnami/postgresql] Release 15.6.0-debian-11-r14
    Signed-off-by: Bitnami Containers <bitnami-bot@vmware.com>

--- bitnami/postgresql/15/debian-11/rootfs/opt/bitnami/scripts/postgresql/entrypoint.sh
+++ bitnami/postgresql/15/debian-11/rootfs/opt/bitnami/scripts/postgresql/entrypoint.sh
@@ -21,6 +21,12 @@ print_welcome_page
 # Enable the nss_wrapper settings
 postgresql_enable_nss_wrapper
 
+# We add the copy from default config in the entrypoint to not break users 
+# bypassing the setup.sh logic. If the file already exists do not overwrite (in
+# case someone mounts a configuration file in /opt/bitnami/postgresql/conf)
+debug "Copying files from $POSTGRESQL_DEFAULT_CONF_DIR to $POSTGRESQL_CONF_DIR"
+cp -nr "$POSTGRESQL_DEFAULT_CONF_DIR"/. "$POSTGRESQL_CONF_DIR"
+
 if [[ "$*" = *"/opt/bitnami/scripts/postgresql/run.sh"* ]]; then
     info "** Starting PostgreSQL setup **"
     /opt/bitnami/scripts/postgresql/setup.sh
