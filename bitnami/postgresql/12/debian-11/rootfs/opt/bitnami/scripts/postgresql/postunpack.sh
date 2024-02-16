8dd615a6224c2654f6ef33e2d6e20c0c45b66875
Bitnami Containers <bitnami-bot@vmware.com>
2024-02-16 12:48:02 +0000

    [bitnami/postgresql] Release 12.18.0-debian-11-r13
    Signed-off-by: Bitnami Containers <bitnami-bot@vmware.com>

--- bitnami/postgresql/12/debian-11/rootfs/opt/bitnami/scripts/postgresql/postunpack.sh
+++ bitnami/postgresql/12/debian-11/rootfs/opt/bitnami/scripts/postgresql/postunpack.sh
@@ -11,7 +11,7 @@
 # Load PostgreSQL environment variables
 . /opt/bitnami/scripts/postgresql-env.sh
 
-for dir in "$POSTGRESQL_INITSCRIPTS_DIR" "$POSTGRESQL_TMP_DIR" "$POSTGRESQL_LOG_DIR" "$POSTGRESQL_CONF_DIR" "${POSTGRESQL_CONF_DIR}/conf.d" "$POSTGRESQL_MOUNTED_CONF_DIR" "${POSTGRESQL_MOUNTED_CONF_DIR}/conf.d" "$POSTGRESQL_VOLUME_DIR"; do
+for dir in "$POSTGRESQL_INITSCRIPTS_DIR" "$POSTGRESQL_TMP_DIR" "$POSTGRESQL_LOG_DIR" "$POSTGRESQL_CONF_DIR" "${POSTGRESQL_CONF_DIR}/conf.d" "$POSTGRESQL_MOUNTED_CONF_DIR" "$POSTGRESQL_DEFAULT_CONF_DIR" "${POSTGRESQL_MOUNTED_CONF_DIR}/conf.d" "$POSTGRESQL_VOLUME_DIR"; do
     ensure_dir_exists "$dir"
 done
 
@@ -21,6 +21,10 @@ postgresql_allow_local_connection
 postgresql_create_config
 
 chmod -R g+rwX "$POSTGRESQL_INITSCRIPTS_DIR" "$POSTGRESQL_TMP_DIR" "$POSTGRESQL_LOG_DIR" "$POSTGRESQL_CONF_DIR" "${POSTGRESQL_CONF_DIR}/conf.d" "$POSTGRESQL_MOUNTED_CONF_DIR" "${POSTGRESQL_MOUNTED_CONF_DIR}/conf.d" "$POSTGRESQL_VOLUME_DIR"
+
+# Copy all initially generated configuration files to the default directory
+# (this is to avoid breaking when entrypoint is being overridden)
+cp -r "${POSTGRESQL_CONF_DIR}/"* "$POSTGRESQL_DEFAULT_CONF_DIR"
 
 # Redirect all logging to stdout
 ln -sf /dev/stdout "$POSTGRESQL_LOG_DIR/postgresql.log"
