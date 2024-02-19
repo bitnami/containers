1f1d2c7d2a242dda05900262b94bc1d529f5d221
Bitnami Containers <bitnami-bot@vmware.com>
2024-02-19 16:12:07 +0000

    [bitnami/clickhouse] Release 23.8.9-debian-11-r24
    Signed-off-by: Bitnami Containers <bitnami-bot@vmware.com>

--- bitnami/clickhouse/23.8/debian-11/rootfs/opt/bitnami/scripts/clickhouse/entrypoint.sh
+++ bitnami/clickhouse/23.8/debian-11/rootfs/opt/bitnami/scripts/clickhouse/entrypoint.sh
@@ -18,6 +18,12 @@ set -o pipefail
 
 print_welcome_page
 
+# We add the copy from default config in the entrypoint to not break users 
+# bypassing the setup.sh logic. If the file already exists do not overwrite (in
+# case someone mounts a configuration file in /opt/bitnami/clickhouse/etc)
+debug "Copying files from $CLICKHOUSE_DEFAULT_CONF_DIR to $CLICKHOUSE_CONF_DIR"
+cp -nr "$CLICKHOUSE_DEFAULT_CONF_DIR"/. "$CLICKHOUSE_CONF_DIR"
+
 if [[ "$1" = "/opt/bitnami/scripts/clickhouse/run.sh" ]]; then
     info "** Starting ClickHouse setup **"
     /opt/bitnami/scripts/clickhouse/setup.sh
