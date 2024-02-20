aa960bf60857eeafdbf3113ebcf214898a340a09
Bitnami Containers <bitnami-bot@vmware.com>
2024-02-20 22:30:33 +0000

    [bitnami/opensearch] Release 1.3.13-debian-12-r16
    Signed-off-by: Bitnami Containers <bitnami-bot@vmware.com>

--- bitnami/opensearch/1/debian-12/rootfs/opt/bitnami/scripts/opensearch/entrypoint.sh
+++ bitnami/opensearch/1/debian-12/rootfs/opt/bitnami/scripts/opensearch/entrypoint.sh
@@ -18,6 +18,25 @@ set -o pipefail
 
 print_welcome_page
 
+# We add the copy from default config in the entrypoint to not break users 
+# bypassing the setup.sh logic. If the file already exists do not overwrite (in
+# case someone mounts a configuration file in /opt/bitnami/elasticsearch/conf)
+debug "Copying files from $DB_DEFAULT_CONF_DIR to $DB_CONF_DIR"
+cp -nr "$DB_DEFAULT_CONF_DIR"/. "$DB_CONF_DIR"
+
+if ! is_dir_empty "$DB_DEFAULT_PLUGINS_DIR"; then
+    debug "Copying plugins from $DB_DEFAULT_PLUGINS_DIR to $DB_PLUGINS_DIR"
+    # Copy the plugins installed by default to the plugins directory
+    # If there is already a plugin with the same name in the plugins folder do nothing
+    for plugin_path in "${DB_DEFAULT_PLUGINS_DIR}"/*; do
+        plugin_name="$(basename "$plugin_path")"
+        plugin_moved_path="${DB_PLUGINS_DIR}/${plugin_name}"
+        if ! [[ -d "$plugin_moved_path" ]]; then
+            cp -r "$plugin_path" "$plugin_moved_path" 
+        fi
+    done
+fi
+
 if [[ "$1" = "/opt/bitnami/scripts/opensearch/run.sh" ]]; then
     info "** Starting Opensearch setup **"
     /opt/bitnami/scripts/opensearch/setup.sh
--- bitnami/opensearch/1/debian-12/rootfs/opt/bitnami/scripts/opensearch/postunpack.sh
+++ bitnami/opensearch/1/debian-12/rootfs/opt/bitnami/scripts/opensearch/postunpack.sh
@@ -11,14 +11,14 @@
 # Load environment
 . /opt/bitnami/scripts/opensearch-env.sh
 
-for dir in "$DB_TMP_DIR" "$DB_DATA_DIR" "$DB_LOGS_DIR" "${DB_BASE_DIR}/plugins" "${DB_BASE_DIR}/modules" "${DB_BASE_DIR}/extensions" "$DB_CONF_DIR" "$DB_VOLUME_DIR" "$DB_INITSCRIPTS_DIR" "$DB_MOUNTED_PLUGINS_DIR"; do
+for dir in "$DB_TMP_DIR" "$DB_DATA_DIR" "$DB_LOGS_DIR" "${DB_BASE_DIR}/plugins" "${DB_BASE_DIR}/modules" "${DB_BASE_DIR}/extensions" "$DB_CONF_DIR" "$DB_VOLUME_DIR" "$DB_INITSCRIPTS_DIR" "$DB_MOUNTED_PLUGINS_DIR" "$DB_DEFAULT_CONF_DIR" "$DB_DEFAULT_PLUGINS_DIR"; do
     ensure_dir_exists "$dir"
     chmod -R ug+rwX "$dir"
 done
 
 elasticsearch_configure_logging
 
-for dir in "$DB_TMP_DIR" "$DB_DATA_DIR" "$DB_LOGS_DIR" "${DB_BASE_DIR}/plugins" "${DB_BASE_DIR}/modules" "$DB_CONF_DIR" "$DB_VOLUME_DIR" "$DB_INITSCRIPTS_DIR" "$DB_MOUNTED_PLUGINS_DIR"; do
+for dir in "$DB_TMP_DIR" "$DB_DATA_DIR" "$DB_LOGS_DIR" "${DB_BASE_DIR}/plugins" "${DB_BASE_DIR}/modules" "$DB_CONF_DIR" "$DB_VOLUME_DIR" "$DB_INITSCRIPTS_DIR" "$DB_MOUNTED_PLUGINS_DIR" "$DB_DEFAULT_CONF_DIR" "$DB_DEFAULT_PLUGINS_DIR"; do
     # `elasticsearch-plugin install` command complains about being unable to create the a plugin's directory
     # even when having the proper permissions.
     # The reason: the code is checking trying to check the permissions by consulting the parent directory owner,
@@ -33,3 +33,16 @@ for dir in "$DB_TMP_DIR" "$DB_DATA_DIR" "$DB_LOGS_DIR" "${DB_BASE_DIR}/plugins" 
 done
 
 elasticsearch_install_plugins
+
+# Copy all initially generated configuration files to the default directory
+# (this is to avoid breaking when entrypoint is being overridden)
+cp -r "${DB_CONF_DIR}/"* "$DB_DEFAULT_CONF_DIR"
+
+if ! is_dir_empty "$DB_PLUGINS_DIR"; then
+    # Move all initially installed plugins to the default plugins directory.
+    for plugin_path in "${DB_PLUGINS_DIR}"/*; do
+        plugin_name="$(basename "$plugin_path")"
+        plugin_moved_path="${DB_DEFAULT_PLUGINS_DIR}/${plugin_name}"
+        mv "$plugin_path" "$plugin_moved_path"
+    done
+fi
