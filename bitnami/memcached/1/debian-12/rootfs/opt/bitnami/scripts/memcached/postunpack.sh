29cf68e21b8e6e9a695cf1042862bdbced07a721
Bitnami Containers <bitnami-bot@vmware.com>
2024-02-22 09:47:12 +0000

    [bitnami/memcached] Release 1.6.23-debian-12-r8
    Signed-off-by: Bitnami Containers <bitnami-bot@vmware.com>

--- bitnami/memcached/1/debian-12/rootfs/opt/bitnami/scripts/memcached/postunpack.sh
+++ bitnami/memcached/1/debian-12/rootfs/opt/bitnami/scripts/memcached/postunpack.sh
@@ -17,7 +17,7 @@ set -o pipefail
 . /opt/bitnami/scripts/memcached-env.sh
 
 # Ensure directories used by Memcached exist and have proper ownership and permissions
-for dir in "$MEMCACHED_CONF_DIR" "$SASL_CONF_PATH"; do
+for dir in "$MEMCACHED_CONF_DIR" "$MEMCACHED_DEFAULT_CONF_DIR" "$SASL_CONF_PATH"; do
     ensure_dir_exists "$dir"
     chmod -R g+rwX "$dir"
 done
