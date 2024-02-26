c2502fb12a78b21ff047a34a1ba05b096ee88a7c
Bitnami Containers <bitnami-bot@vmware.com>
2024-02-26 13:42:10 +0000

    [bitnami/opensearch-dashboards] Release 2.12.0-debian-12-r1
    Signed-off-by: Bitnami Containers <bitnami-bot@vmware.com>

--- bitnami/opensearch-dashboards/2/debian-12/rootfs/opt/bitnami/scripts/opensearch-dashboards/postunpack.sh
+++ bitnami/opensearch-dashboards/2/debian-12/rootfs/opt/bitnami/scripts/opensearch-dashboards/postunpack.sh
@@ -16,7 +16,7 @@ set -o pipefail
 # Load environment
 . /opt/bitnami/scripts/opensearch-dashboards-env.sh
 
-for dir in "$SERVER_TMP_DIR" "$SERVER_LOGS_DIR" "$SERVER_CONF_DIR" "$SERVER_PLUGINS_DIR" "$SERVER_VOLUME_DIR" "$SERVER_DATA_DIR" "$SERVER_INITSCRIPTS_DIR"; do
+for dir in "$SERVER_TMP_DIR" "$SERVER_LOGS_DIR" "$SERVER_CONF_DIR" "$SERVER_DEFAULT_CONF_DIR" "$SERVER_PLUGINS_DIR" "$SERVER_DEFAULT_PLUGINS_DIR" "$SERVER_VOLUME_DIR" "$SERVER_DATA_DIR" "$SERVER_INITSCRIPTS_DIR"; do
     ensure_dir_exists "$dir"
     chmod -R ug+rwX "$dir"
 done
@@ -24,3 +24,18 @@ done
 kibana_conf_set "path.data" "$SERVER_DATA_DIR"
 # For backwards compatibility, create a symlink to the default path
 ! is_dir_empty "${SERVER_BASE_DIR}/data" || rm -rf "${SERVER_BASE_DIR}/data" && ln -s "$SERVER_DATA_DIR" "${SERVER_BASE_DIR}/data"
+
+# Copy all initially generated configuration files to the default directory
+# (this is to avoid breaking when entrypoint is being overridden)
+cp -r "${SERVER_CONF_DIR}/"* "$SERVER_DEFAULT_CONF_DIR"
+chmod o+rX -R "$SERVER_DEFAULT_CONF_DIR"
+
+if ! is_dir_empty "$SERVER_PLUGINS_DIR"; then
+    # Move all initially installed plugins to the default plugins directory.
+    for plugin_path in "${SERVER_PLUGINS_DIR}"/*; do
+        plugin_name="$(basename "$plugin_path")"
+        plugin_moved_path="${SERVER_DEFAULT_PLUGINS_DIR}/${plugin_name}"
+        mv "$plugin_path" "$plugin_moved_path"
+    done
+    chmod o+rX -R "$SERVER_DEFAULT_PLUGINS_DIR"
+fi
