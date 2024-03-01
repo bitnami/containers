4a27186a0b312ac5633f6426685b45e8d12d2f4e
Bitnami Containers <bitnami-bot@vmware.com>
2024-03-01 17:36:08 +0000

    [bitnami/airflow-worker] Release 2.8.2-debian-12-r1
    Signed-off-by: Bitnami Containers <bitnami-bot@vmware.com>

--- bitnami/airflow-worker/2/debian-12/rootfs/opt/bitnami/scripts/airflow-worker/postunpack.sh
+++ bitnami/airflow-worker/2/debian-12/rootfs/opt/bitnami/scripts/airflow-worker/postunpack.sh
@@ -19,7 +19,7 @@ set -o pipefail
 
 ensure_dir_exists "$AIRFLOW_BASE_DIR"
 # Ensure the needed directories exist with write permissions
-for dir in "$AIRFLOW_TMP_DIR" "$AIRFLOW_LOGS_DIR" "$AIRFLOW_DAGS_DIR"; do
+for dir in "$AIRFLOW_TMP_DIR" "$AIRFLOW_LOGS_DIR" "$AIRFLOW_DAGS_DIR" "${AIRFLOW_BASE_DIR}/nss-wrapper"; do
     ensure_dir_exists "$dir"
     configure_permissions_ownership "$dir" -d "775" -f "664" -g "root"
 done
