718d0ace5d96f633589c0b2846312dfb6a97c4ec
Bitnami Containers <bitnami-bot@vmware.com>
2024-04-04 22:05:11 +0000

    [bitnami/solr] Release 8.11.3-debian-12-r2
    Signed-off-by: Bitnami Containers <bitnami-bot@vmware.com>

--- bitnami/solr/8/debian-12/rootfs/opt/bitnami/scripts/solr-env.sh
+++ bitnami/solr/8/debian-12/rootfs/opt/bitnami/scripts/solr-env.sh
@@ -46,6 +46,7 @@ solr_env_vars=(
     SOLR_ZK_MAX_RETRIES
     SOLR_ZK_SLEEP_TIME
     SOLR_ZK_CHROOT
+    SOLR_ZK_HOSTS
     SOLR_COLLECTION
 )
 for env_var in "${solr_env_vars[@]}"; do
@@ -107,5 +108,6 @@ export SOLR_DAEMON_GROUP="solr"
 export SOLR_ZK_MAX_RETRIES="${SOLR_ZK_MAX_RETRIES:-5}"
 export SOLR_ZK_SLEEP_TIME="${SOLR_ZK_SLEEP_TIME:-5}"
 export SOLR_ZK_CHROOT="${SOLR_ZK_CHROOT:-/solr}"
+export SOLR_ZK_HOSTS="${SOLR_ZK_HOSTS:-}"
 
 # Custom environment variables may be defined below
