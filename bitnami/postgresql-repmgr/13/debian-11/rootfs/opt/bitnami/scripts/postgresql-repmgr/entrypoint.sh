5454ebb4f31e416bbe76d14cd4d2c1de46fa1ad0
dmejer <dmejer@consdata.com>
2024-05-06 11:11:29 +0200

    SHARKS-11548 feat nalozenie zmian bitnami debian 12

--- bitnami/postgresql-repmgr/13/debian-11/rootfs/opt/bitnami/scripts/postgresql-repmgr/entrypoint.sh
+++ bitnami/postgresql-repmgr/13/debian-11/rootfs/opt/bitnami/scripts/postgresql-repmgr/entrypoint.sh
@@ -24,6 +24,12 @@ print_welcome_page
 # Enable the nss_wrapper settings
 postgresql_enable_nss_wrapper
 
+# We add the copy from default config in the entrypoint to not break users
+# bypassing the setup.sh logic. If the file already exists do not overwrite (in
+# case someone mounts a configuration file in /opt/bitnami/postgresql/conf)
+debug "Copying files from $POSTGRESQL_DEFAULT_CONF_DIR to $POSTGRESQL_CONF_DIR"
+cp -nr "$POSTGRESQL_DEFAULT_CONF_DIR"/. "$POSTGRESQL_CONF_DIR"
+
 if [[ "$*" = *"/opt/bitnami/scripts/postgresql-repmgr/run.sh"* ]]; then
     info "** Starting PostgreSQL with Replication Manager setup **"
     /opt/bitnami/scripts/postgresql-repmgr/setup.sh
