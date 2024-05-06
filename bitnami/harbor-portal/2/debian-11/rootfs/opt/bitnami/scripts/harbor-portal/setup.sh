9982b9ad3ec2d14990cbcaf88f535ab70f2cbd1d
dmejer <dmejer@consdata.com>
2024-05-06 10:58:18 +0200

    Revert "[bitnami/*] Remove debian 11 distro (#62906)"
    This reverts commit d624c10ea7e23d8059dc62adb11e1414d8e39123.

---
+++ bitnami/harbor-portal/2/debian-11/rootfs/opt/bitnami/scripts/harbor-portal/setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Copyright VMware, Inc.
+# SPDX-License-Identifier: APACHE-2.0
+
+# shellcheck disable=SC1090,SC1091
+
+set -o errexit
+set -o nounset
+set -o pipefail
+# set -o xtrace # Uncomment this line for debugging purposes
+
+# Load libraries
+. /opt/bitnami/scripts/libharbor.sh
+
+# Load environment
+. /opt/bitnami/scripts/harbor-portal-env.sh
+
+install_custom_certs
