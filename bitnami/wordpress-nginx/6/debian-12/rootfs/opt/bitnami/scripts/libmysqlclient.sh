53a796a58fcb484af8fb5c7a24ca175ba26b0b41
Bitnami Containers <bitnami-bot@vmware.com>
2024-05-08 04:03:24 +0000

    [bitnami/wordpress-nginx] Release 6.5.3-debian-12-r0
    Signed-off-by: Bitnami Containers <bitnami-bot@vmware.com>

--- bitnami/wordpress-nginx/6/debian-12/rootfs/opt/bitnami/scripts/libmysqlclient.sh
+++ bitnami/wordpress-nginx/6/debian-12/rootfs/opt/bitnami/scripts/libmysqlclient.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright VMware, Inc.
+# Copyright Broadcom, Inc. All Rights Reserved.
 # SPDX-License-Identifier: APACHE-2.0
 #
 # Bitnami MySQL Client library
@@ -152,7 +152,7 @@ EOF
 }
 
 #!/bin/bash
-# Copyright VMware, Inc.
+# Copyright Broadcom, Inc. All Rights Reserved.
 # SPDX-License-Identifier: APACHE-2.0
 #
 # Library for mysql common
