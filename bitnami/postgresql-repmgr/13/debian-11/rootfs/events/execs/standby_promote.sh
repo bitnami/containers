79cd6a97d3648c4c5495eacb99f8e6eb688aafcf
Andrzej Dengusiak <adengusiak@consdata.pl>
2024-07-23 13:09:08 +0200

    SHARKS-12397 kill/resume pgbouncer on failover

--- bitnami/postgresql-repmgr/13/debian-11/rootfs/events/execs/standby_promote.sh
+++ bitnami/postgresql-repmgr/13/debian-11/rootfs/events/execs/standby_promote.sh
@@ -24,16 +24,36 @@ log_pure() {
   stderr_print "$(date "+%T.%2N ")${*}"
 }
 
+# param $1 - command
+pgbouncer_command() {
+  read -r -a nodes_psql <<<"$(tr ',;' ' ' <<<"${PGBOUNCER_NODES_PSQL}")"
+  for node_psql in "${nodes_psql[@]}"; do
+    [[ "${node_psql}" =~ ^(([^:/?#]+):)?// ]] || node_psql="tcp://${node_psql}"
+    local pgbouncer_psql_host="$(parse_uri "${node_psql}" "host")"
+    local pgbouncer_psql_port="$(parse_uri "${node_psql}" "port")"
+
+    log_pure "[notify pgbouncer] $1 node=${pgbouncer_psql_host}:${pgbouncer_psql_port}"
+    PGPASSWORD="${POSTGRESQL_PASSWORD}" psql -U "${POSTGRESQL_USERNAME}" -h "${pgbouncer_psql_host}" -p "${pgbouncer_psql_port}" -d pgbouncer -tc "$1"
+  done
+}
+
 notify_pgbouncer() {
   log_pure "[notify pgbouncer] start"
   log_pure "[notify pgbouncer] PGBOUNCER_NODES_PSQL=${PGBOUNCER_NODES_PSQL}"
   log_pure "[notify pgbouncer] PGBOUNCER_NODES_SSH=${PGBOUNCER_NODES_SSH}"
   log_pure "[notify pgbouncer] PGBOUNCER_DATABASE_INI=${PGBOUNCER_DATABASE_INI}"
+  log_pure "[notify pgbouncer] PGBOUNCER_DATABASES=${PGBOUNCER_DATABASES}"
 
   # select conninfo
   local -r query="SELECT conninfo FROM repmgr.nodes WHERE active = TRUE AND type='primary'"
   local -r conninfo=$(PGPASSWORD="${REPMGR_PASSWORD}" psql -U "${REPMGR_USERNAME}" -d "${REPMGR_DATABASE}" -A -t -c "${query}")
   log_pure "[notify pgbouncer] conninfo=${conninfo}"
+
+  #kill connections to old primary
+  read -r -a pgbouncer_databases <<<"$(tr ',;' ' ' <<<"${PGBOUNCER_DATABASES}")"
+  for pgbouncer_database in "${pgbouncer_databases[@]}"; do
+    pgbouncer_command "kill ${pgbouncer_database}"
+  done
 
   echo -en "[databases]\n" >${PGBOUNCER_DATABASE_INI_TEMP}
   local -r pg_primary_host=$(match_first_regex "(host=[.0-9]+)" "$conninfo")
@@ -54,15 +74,12 @@ notify_pgbouncer() {
       "${PGBOUNCER_CONTAINER_USERNAME}"@"${pgbouncer_ssh_host}":"${PGBOUNCER_DATABASE_INI}"
   done
 
-  # reload pgbouncer nodes
-  read -r -a nodes_psql <<<"$(tr ',;' ' ' <<<"${PGBOUNCER_NODES_PSQL}")"
-  for node_psql in "${nodes_psql[@]}"; do
-    [[ "${node_psql}" =~ ^(([^:/?#]+):)?// ]] || node_psql="tcp://${node_psql}"
-    local pgbouncer_psql_host="$(parse_uri "${node_psql}" "host")"
-    local pgbouncer_psql_port="$(parse_uri "${node_psql}" "port")"
+  # reload pgbouncer
+  pgbouncer_command reload
 
-    log_pure "[notify pgbouncer] reload node=${pgbouncer_psql_host}:${pgbouncer_psql_port}"
-    PGPASSWORD="${POSTGRESQL_PASSWORD}" psql -U "${POSTGRESQL_USERNAME}" -h "${pgbouncer_psql_host}" -p "${pgbouncer_psql_port}" -d pgbouncer -tc "reload"
+  # resume pgbouncer connections
+  for pgbouncer_database in "${pgbouncer_databases[@]}"; do
+    pgbouncer_command "resume ${pgbouncer_database}"
   done
 
   # clean up generated file
