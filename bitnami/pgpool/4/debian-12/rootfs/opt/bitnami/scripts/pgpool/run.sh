3cd3a916da2d920d95b5f6ca07c649874dd042dd
seatrain <seatrain@users.noreply.github.com>
2024-11-27 23:16:38 +0800

    [bitnami/pgpool] Enhance pgpool-II Recovery Mechanism (#75225)

--- bitnami/pgpool/4/debian-12/rootfs/opt/bitnami/scripts/pgpool/run.sh
+++ bitnami/pgpool/4/debian-12/rootfs/opt/bitnami/scripts/pgpool/run.sh
@@ -23,6 +23,7 @@ eval "$(ldap_env)"
 
 command="$(command -v pgpool)"
 flags=("-n" "--config-file=${PGPOOL_CONF_FILE}" "--hba-file=${PGPOOL_PGHBA_FILE}")
+is_boolean_yes "$PGPOOL_DISCARD_STATUS" && flags+=("-D")
 [[ -z "${PGPOOL_EXTRA_FLAGS:-}" ]] || flags=("${flags[@]}" "${PGPOOL_EXTRA_FLAGS[@]}")
 
 is_boolean_yes "$PGPOOL_ENABLE_LDAP" && ldap_start_nslcd_bg
