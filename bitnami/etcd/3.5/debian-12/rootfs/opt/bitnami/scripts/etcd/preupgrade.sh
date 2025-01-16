9d199a809805e3a863ec07de3d8f3934f11e393b
Khoi Pham <pckhoi@gmail.com>
2025-01-16 09:24:03 +0700

    [bitnami/etcd] Fail preupgrade hook if members cannot be listed
    Signed-off-by: Khoi Pham <pckhoi@gmail.com>

--- bitnami/etcd/3.5/debian-12/rootfs/opt/bitnami/scripts/etcd/preupgrade.sh
+++ bitnami/etcd/3.5/debian-12/rootfs/opt/bitnami/scripts/etcd/preupgrade.sh
@@ -39,13 +39,20 @@ read -r -a extra_flags <<<"$(etcdctl_auth_flags)"
 is_boolean_yes "$ETCD_ON_K8S" && extra_flags+=("--endpoints=$(endpoints_as_host_port)")
 debug "Listing members"
 if ! current="$(etcdctl member list ${extra_flags[@]} --write-out simple | awk -F ", " '{print $3 ":" $1}')"; then
-    debug "Error listing members, is this a new cluster?"
-    exit 0
+    error "Unable to list members, are all members healthy?"
+    exit 1
 fi
 info "Current cluster members are: $(echo "$current" | awk -F: '{print $1}' | tr -s '\n' ',' | sed 's/,$//g')"
+
 expected="$(echo $ETCD_INITIAL_CLUSTER | tr -s ',' '\n' | awk -F= '{print $1}')"
 info "Expected cluster members are: $(echo "$expected" | tr -s '\n' ',' | sed 's/,$//g')"
-for member in $(comm -23 <(echo "$current" | awk -F: '{print $1}' | sort) <(echo "$expected" | sort)); do
-    info "Removing obsolete member $member"
-    etcdctl member remove ${extra_flags[@]} "$(echo "$current" | grep "$member" | awk -F: '{print $2}')"
-done
+read -r -a obsolete_members <<<"$(comm -23 <(echo "$current" | awk -F: '{print $1}' | sort) <(echo "$expected" | sort))"
+if [ ${#obsolete_members[@]} -eq 0 ]; then
+    info "No obsolete members to remove."
+else
+    for member in $obsolete_members; do
+        info "Removing obsolete member $member"
+        etcdctl member remove ${extra_flags[@]} "$(echo "$current" | grep "$member" | awk -F: '{print $2}')"
+    done
+fi
+info "Pre-upgrade checks completed!"
