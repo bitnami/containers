4b2837844938a56cd8ba4820373b158d43dcdbf8
Bénédicte Martinez <benedicte.martinez@cea.fr>
2025-02-06 10:08:40 +0100

    [bitnami/matomo] Exclude matomo.js from being rsynced (issue #76460) Signed-off-by: Bénédicte Martinez <benedicte.martinez@cea.fr>

--- bitnami/matomo/5/debian-12/rootfs/opt/bitnami/scripts/libmatomo.sh
+++ bitnami/matomo/5/debian-12/rootfs/opt/bitnami/scripts/libmatomo.sh
@@ -218,7 +218,7 @@ EOF
     else
         info "Persisted Matomo installation detected"
         info "Updating Matomo files in persisted data"
-        rsync -a "$MATOMO_BASE_DIR"/* "$MATOMO_VOLUME_DIR" --exclude "$(realpath --relative-to="$MATOMO_BASE_DIR" "$MATOMO_CONF_FILE")"
+        rsync -a "$MATOMO_BASE_DIR"/* "$MATOMO_VOLUME_DIR" --exclude "$(realpath --relative-to="$MATOMO_BASE_DIR" "$MATOMO_CONF_FILE")" --exclude "$(realpath --relative-to="$MATOMO_BASE_DIR" "$MATOMO_TRACKER_FILE")"
         info "Restoring Matomo installation"
         restore_persisted_app "$app_name" "$MATOMO_DATA_TO_PERSIST"
         info "Trying to connect to the database server"
