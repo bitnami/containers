7acdd455194ee47ad0cdd828ca9b1a6ed9e25471
mueller-ma <mueller-ma@users.noreply.github.com>
2025-04-08 10:56:10 +0200

    Apply suggestion
    Signed-off-by: mueller-ma <mueller-ma@users.noreply.github.com>

--- bitnami/redis-sentinel/7.4/debian-12/rootfs/opt/bitnami/scripts/libredissentinel.sh
+++ bitnami/redis-sentinel/7.4/debian-12/rootfs/opt/bitnami/scripts/libredissentinel.sh
@@ -251,7 +251,11 @@ redis_initialize() {
             fi
             redis_conf_set tls-cert-file "$REDIS_SENTINEL_TLS_CERT_FILE"
             redis_conf_set tls-key-file "$REDIS_SENTINEL_TLS_KEY_FILE"
-            is_empty_value "$REDIS_SENTINEL_TLS_CA_FILE" && redis_conf_set tls-ca-cert-dir "$REDIS_SENTINEL_TLS_CA_DIR" || redis_conf_set tls-ca-cert-file "$REDIS_SENTINEL_TLS_CA_FILE"
+            if is_empty_value "$REDIS_SENTINEL_TLS_CA_FILE"; then
+                redis_conf_set tls-ca-cert-dir "$REDIS_SENTINEL_TLS_CA_DIR"
+            else
+                redis_conf_set tls-ca-cert-file "$REDIS_SENTINEL_TLS_CA_FILE"
+            fi
 
             [[ -n "$REDIS_SENTINEL_TLS_DH_PARAMS_FILE" ]] && redis_conf_set tls-dh-params-file "$REDIS_SENTINEL_TLS_DH_PARAMS_FILE"
             redis_conf_set tls-auth-clients "$REDIS_SENTINEL_TLS_AUTH_CLIENTS"
