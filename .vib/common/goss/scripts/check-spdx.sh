6d9eb990654bc4800bd116b3e4dd67c8a14cdf2f
Gonzalo Gomez Gracia <gonzalo.gomez@broadcom.com>
2025-06-06 10:44:09 +0200

    [bitnami/common] Update SPDX common test
    Signed-off-by: Gonzalo Gomez Gracia <gonzalo.gomez@broadcom.com>

--- .vib/common/goss/scripts/check-spdx.sh
+++ .vib/common/goss/scripts/check-spdx.sh
@@ -6,6 +6,6 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-mapfile -t files < <( find /bitnami "$BITNAMI_ROOT_DIR" -type f -name '.spdx-*.json' )
+mapfile -t files < <( find /bitnami "$BITNAMI_ROOT_DIR" -type f -name '.spdx-*.spdx' )
 
 [[ ${#files[@]} -gt 0 ]]
